6a238e08d88079b44a41cc7bbfa8c1b3b1d96c52
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-01-20 17:29:12 +0000

    added

---
+++ src/gate/TextualDocument.java
@@ -0,0 +1,27 @@
+/*
+ *	TextualDocument.java
+ *
+ *	Hamish Cunningham, 20/Jan/2000
+ *
+ *	$Id$
+ */
+
+package gate;
+import java.util.*;
+import gate.util.*;
+
+/** 
+  * Documents whose content is text
+  */
+public class TextualDocument implements Document
+{
+
+  /** The contents of the document */
+  public String getContents();
+
+  /** Main routine. */
+  public static void main(String[] args) {
+	  
+  } // main
+
+} // class TextualDocument
