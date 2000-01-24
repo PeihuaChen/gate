ec45112100c8ad11c63bb560d9866ee929144f7f
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-01-24 14:07:24 +0000

    *** empty log message ***

--- src/gate/Attic/impl/ag/Node.java
+++ src/gate/Attic/impl/ag/Node.java
@@ -1,7 +1,45 @@
 package gate.impl.ag;
 
 public class Node implements gate.Node{
-
+  /** Default constructor. The -1 Id indicates a non-valid Id*/
   public Node() {
+    id=new Integer(-1);
+    offset=null;
+  }//Node()
+
+  /** Contructor
+  @param id the Id of the new node
+  @param offset the (temporal) offset of the Node; Should be -1 for not anchored nodes*/
+  public Node(int id, long offset){
+    this.id=new Integer(id);
+    if(offset==-1)this.offset=null;
+    else this.offset=new Long(offset);
+  }//Node(int i)
+
+  /**returns the Id of the Node */
+  public Integer getId(){
+    return id;
   }
-} 
+
+  /** Offset (will be null when the node is not anchored)*/
+  public Long getOffset(){
+    return offset;
+  }
+
+  /** Does this node structurally precede n? */
+  public boolean sPrecedes(gate.Node n){
+    return false;
+  }
+
+  /** Does this node temporally (i.e. by offset) precede or is equal to n? */
+  public boolean tPrecedes(gate.Node n){
+    return (offset.compareTo(n.getId())<=0);
+  }
+
+  /** Does this node precede n? */
+  public boolean precedes(gate.Node n){
+    return sPrecedes(n)||tPrecedes(n);
+  }
+  private Integer id;
+  private Long offset;
+}
