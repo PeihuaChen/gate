5c5c7c48b6828fdd02914dd5738cf498c7331b2c
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-01-31 15:40:32 +0000

    sichronized Tools.gensym()

--- src/gate/util/Tools.java
+++ src/gate/util/Tools.java
@@ -6,7 +6,10 @@ public class Tools {
   }
   static long sym=0;
 
-  static public String gensym(String base){
-    return base+sym++;
+  /*Returns a Long wich is unique during the current run.
+  * Maybe we should use serializaton in order to save the state on System.exit...
+  */
+  static public synchronized Long gensym(){
+    return new Long(sym++);
   }
-} 
+}
