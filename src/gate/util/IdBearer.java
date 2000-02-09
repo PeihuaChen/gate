eced66c668fef8a8bf10c40c1320d1b523c7a483
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-02-09 16:40:32 +0000

    added

---
+++ src/gate/util/IdBearer.java
@@ -0,0 +1,19 @@
+/*
+ *	IdBearer.java
+ *
+ *	Hamish Cunningham, 7/Feb/2000
+ *
+ *	$Id$
+ */
+
+package gate.util;
+import java.util.*;
+import gate.*;
+
+/** Classes that have Ids. */
+public interface IdBearer 
+{
+  /** The ID. */
+  public Integer getId();
+
+} // interface IdBearer
