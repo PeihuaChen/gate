7dbbe28ba13f65bb043e7fc46728d8b5e2b5d03f
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-02-10 16:02:42 +0000

    added

---
+++ src/gate/util/SimpleFeatureMapImpl.java
@@ -0,0 +1,19 @@
+/*
+ *	SimpleFeatureMapImpl.java
+ *
+ *	Hamish Cunningham, 7/Feb/2000
+ *
+ *	$Id$
+ */
+
+package gate.util;
+
+import java.util.*;
+import gate.*;
+
+/** Simple case of features.
+  */
+public class SimpleFeatureMapImpl extends HashMap implements FeatureMap
+{
+
+} // class SimpleFeatureMapImpl
