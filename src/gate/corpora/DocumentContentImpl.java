05c4776bdec6abcb9e7b359ce93a4f75868f0c5d
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-02-16 15:40:34 +0000

    did getContent and edit

--- src/gate/corpora/DocumentContentImpl.java
+++ src/gate/corpora/DocumentContentImpl.java
@@ -31,28 +31,44 @@ public class DocumentContentImpl implements DocumentContent
     BufferedReader uReader = null;
     StringBuffer buf = new StringBuffer();
     char c;
+    long s = 0, e = Long.MAX_VALUE, counter = 0;
+    if(start != null && end != null) {
+      s = start.longValue();
+      e = end.longValue();
+    }
 
     uReader = new BufferedReader(new InputStreamReader(u.openStream()));
     while( true ) {
       int i = uReader.read();
       if(i == -1) break;
-      buf.append((char) i);
+
+      if(counter >= s && counter < e)
+        buf.append((char) i);
+      counter++;
     }
 
-      content = new String(buf);
+    content = new String(buf);
   } // Contruction from URL and offsets */
-
 
   /** Propagate changes to the document content. */
   void edit(Long start, Long end, DocumentContent replacement)
-  throws InvalidOffsetException {
-    throw new LazyProgrammerException();
+  {
+    int s = start.intValue(), e = end.intValue();
+    String repl = ((DocumentContentImpl) replacement).content;
+    StringBuffer newContent = new StringBuffer(content);
+    newContent.replace(s, e, repl);
   } // edit(start,end,replacement)
 
   /** The contents under a particular span. */
   public DocumentContent getContent(Long start, Long end)
-  throws InvalidOffsetException {
-    throw new LazyProgrammerException();
+    throws InvalidOffsetException
+  {
+    if(! isValidOffsetRange(start, end))
+      throw new InvalidOffsetException();
+
+    return new DocumentContentImpl(
+      content.substring(start.intValue(), end.intValue())
+    );
   } // getContent(start, end)
 
   /** The size of this content (e.g. character length for textual
@@ -62,6 +78,27 @@ public class DocumentContentImpl implements DocumentContent
     return new Long(content.length());
   } // size()
 
+  /** Check that an offset is valid */
+  boolean isValidOffset(Long offset) {
+    if(offset == null)
+      return false;
+
+    long o = offset.longValue();
+    long len = content.length();
+    if(o > len || o < 0)
+      return false;
+
+    return true;
+  } // isValidOffset
+
+  /** Check that both start and end are valid offsets and that
+    * they constitute a valid offset range
+    */
+  boolean isValidOffsetRange(Long start, Long end) {
+    return
+      isValidOffset(start) && isValidOffset(end) &&
+      start.longValue() <= end.longValue();
+  } // isValidOffsetRange(start,end)
 
   /** Just for now - later we have to cater for different types of
     * content.
