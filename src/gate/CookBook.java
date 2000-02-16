8ba42981b5846a28ff100620ef9482a98c1ee2b2
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-02-16 17:37:30 +0000

    added

---
+++ src/gate/CookBook.java
@@ -0,0 +1,23 @@
+/*
+ *	CookBook.java
+ *
+ *	Hamish Cunningham, 16/Feb/2000
+ *
+ *	$Id$
+ */
+
+package gate;
+
+import java.util.*;
+
+import gate.*;
+
+
+/**
+**************************************************************
+This class provides examples of using the GATE APIs.
+**************************************************************/
+public class CookBook
+{
+
+} // class CookBook
