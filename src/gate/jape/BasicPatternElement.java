ca6076496d439d8b3db7cf87b7d4378f743336f4
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-02-23 17:18:06 +0000

    changed match history caching to store annot sets (removes need to be able to delete n last added annots from AS)

--- src/gate/jape/BasicPatternElement.java
+++ src/gate/jape/BasicPatternElement.java
@@ -128,10 +128,7 @@ extends PatternElement implements JapeConstants, java.io.Serializable
     //Debug.nl(this);
 
     for(int i=0; i<arity; i++) {
-      int previousSize = ((Integer) matchHistory.pop()).intValue();
-      //for(int j = matchedAnnots.size() - 1; j >= previousSize; j--)
-      //  matchedAnnots.removeNth(j);
-      removeN(matchedAnnots, matchedAnnots.size() - previousSize);
+      matchedAnnots.removeAll((AnnotationSet) matchHistory.pop());
     }
   } // rollback
 
@@ -140,6 +137,8 @@ extends PatternElement implements JapeConstants, java.io.Serializable
     Document doc, int position, MutableInteger newPosition
   ) {
     final int startingCacheSize = matchedAnnots.size();
+    AnnotationSet addedAnnots = new AnnotationSetImpl((Document) null);
+
     //Debug.pr(this, "BPE.matches: trying at position " + position);
     //Debug.nl(this);
     int rightmostEnd = -1;
@@ -247,7 +246,8 @@ extends PatternElement implements JapeConstants, java.io.Serializable
         // purge the whole cache
         //for(int j = matchedAnnots.size() - 1; j >= startingCacheSize; j--)
         //  matchedAnnots.removeNth(j);
-        removeN(matchedAnnots, matchedAnnots.size() - startingCacheSize);
+        matchedAnnots.removeAll(addedAnnots);
+
         //Debug.pr(
         //  this, "BPE.matches: false, newPosition.value(" +
         //  newPosition.value + ")" + Debug.getNl()
@@ -256,17 +256,20 @@ extends PatternElement implements JapeConstants, java.io.Serializable
       } else {
         //Debug.pr(this,"BPE.matches: match= "+match.toString()+Debug.getNl());
         matchedAnnots.addAll(match);
+        addedAnnots.addAll(match);
         newPosition.value = Math.max(newPosition.value, matchEnd);
       }
 
     } // for each constraint
 
-    // success: store the size of the cache before this match
-    matchHistory.push(new Integer(startingCacheSize));
+    // success: store the annots added this time
+    matchHistory.push(addedAnnots);
+
     //Debug.pr(this, "BPE.matches: returning true" + Debug.getNl());
     // under negation we may not have advanced...
     if(newPosition.value == position)
       newPosition.value++;
+
     return true;
   } // matches
 
@@ -302,101 +305,6 @@ extends PatternElement implements JapeConstants, java.io.Serializable
     return buf.toString();
   } // toString
 
-  /** Utility method to replace JDM annotation set code. Removes the
-    * n most recently added elements of as. Relies on ID numbers of
-    * annotations (probably a BAD idea).
-    */
-  private void removeN(AnnotationSet as, int n) {
-    // get the next annotation ID number
-    // count down from it, removing annots until we did n
-    
-  } // removeN(as, n)
-
 } // class BasicPatternElement
 
 
-// $Log$
-// Revision 1.1  2000/02/23 13:46:03  hamish
-// added
-//
-// Revision 1.1.1.1  1999/02/03 16:23:01  hamish
-// added gate2
-//
-// Revision 1.26  1998/11/24 16:19:00  hamish
-// now feeds back next available of first constraint as newPosition
-//
-// Revision 1.25  1998/11/13 13:17:15  hamish
-// merged in the doc length bug fix
-//
-// Revision 1.24  1998/11/12 17:47:27  kalina
-// A bug fixed, wasn't restoring the document length
-//
-// Revision 1.23  1998/11/05 13:36:29  kalina
-// moved to use array of JdmAttributes for selectNextAnnotation
-// instead of a sequence
-//
-// Revision 1.22  1998/11/04 20:10:12  hamish
-// set lastFailurePoint from nextAvailable.value
-//
-// Revision 1.21  1998/11/01 23:18:43  hamish
-// use new instead of clear on containers
-//
-// Revision 1.20  1998/11/01 22:35:55  kalina
-// attribute seq hashtable mod
-//
-// Revision 1.19  1998/11/01 21:21:34  hamish
-// use Java arrays in transduction where possible
-//
-// Revision 1.18  1998/11/01 15:17:30  hamish
-// initial mod for feeding back next available; not currently used though
-//
-// Revision 1.17  1998/10/30 14:06:42  hamish
-// added getTransducer
-//
-// Revision 1.16  1998/10/29 12:00:34  hamish
-// debug code out
-//
-// Revision 1.15  1998/10/06 16:16:08  hamish
-// negation percolation during constrain add; position advance when none at end
-//
-// Revision 1.14  1998/10/01 16:06:27  hamish
-// new appelt transduction style, replacing buggy version
-//
-// Revision 1.13  1998/09/26 09:19:13  hamish
-// added cloning of PE macros
-//
-// Revision 1.12  1998/09/24 17:41:17  hamish
-// fixed multiple constraints on adjacent tokens bug
-//
-// Revision 1.11  1998/09/23 12:48:00  hamish
-// negation added; noncontiguous BPEs disallowed
-//
-// Revision 1.10  1998/09/15 16:36:11  hamish
-// debug printing bug
-//
-// Revision 1.9  1998/08/13 14:17:31  hamish
-// workaround class cast bug in jdm seq append
-//
-// Revision 1.8  1998/08/12 19:05:40  hamish
-// fixed multi-part CG bug; set reset to real reset and fixed multi-doc bug
-//
-// Revision 1.7  1998/08/12 15:39:30  hamish
-// added padding toString methods
-//
-// Revision 1.6  1998/08/07 16:18:44  hamish
-// parser pretty complete, with backend link done
-//
-// Revision 1.5  1998/08/05 21:58:03  hamish
-// backend works on simple test
-//
-// Revision 1.4  1998/08/03 19:51:18  hamish
-// rollback added
-//
-// Revision 1.3  1998/07/30 11:05:13  mks
-// more jape
-//
-// Revision 1.2  1998/07/29 11:06:52  hamish
-// first compiling version
-//
-// Revision 1.1.1.1  1998/07/28 16:37:46  hamish
-// gate2 lives
