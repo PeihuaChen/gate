577e1c6f5a06981f110c1da87e611a69ef6df2f4
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-05-24 14:25:35 +0000

    Added gate.gui

---
+++ src/gate/gui/ExtensionFileFilter.java
@@ -0,0 +1,49 @@
+/*
+*	ExtensionFileFilter.java
+*
+*	Valentin Tablan, 22/May/2000
+*
+*	$Id$
+*/
+package gate.gui;
+
+
+import javax.swing.filechooser.*;
+import java.io.*;
+import java.util.*;
+
+public class ExtensionFileFilter extends javax.swing.filechooser.FileFilter{
+
+  public ExtensionFileFilter() {
+  }
+
+  public boolean accept(File f){
+    String name = f.getName();
+    if(f.isDirectory()) return true;
+    boolean res = false;
+    if(name.indexOf('.') != -1){
+      String extension = name.substring(name.indexOf('.')+1,name.length());
+      Iterator extIter = acceptedExtensions.iterator();
+      while(extIter.hasNext()){
+        if(((String)extIter.next()).equalsIgnoreCase(extension)) res=true;
+      }
+    };
+    return res;
+  }
+
+  public String getDescription(){
+    return description;
+  }
+
+  public void addExtension(String ext){
+    acceptedExtensions.add(ext);
+  }
+
+  public void setDescription(String desc){
+    description = desc;
+  }
+
+  private Set acceptedExtensions = new HashSet();
+  private String description;
+
+}
