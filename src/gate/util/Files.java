c11b0660934b7500fb746e5c18af648049b84ec3
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-06-09 15:37:14 +0000

    added getResource... methods

--- src/gate/util/Files.java
+++ src/gate/util/Files.java
@@ -33,6 +33,53 @@ public class Files {
     return bytes;
   } // getByteArray(File)
 
+  /** Get a resource from the GATE resources directory as a String.
+    * The resource name should be relative to <TT>gate/resources</TT>; e.g.
+    * for a resource stored as <TT>gate/resources/jape/Test11.jape</TT>,
+    * this method should be passed the name <TT>jape/Test11.jape</TT>.
+    */
+  public static String getResourceAsString(String resourceName)
+  throws IOException {
+    InputStream resourceStream = getResourceAsStream(resourceName);
+    BufferedReader resourceReader =
+      new BufferedReader(new InputStreamReader(resourceStream));
+    StringBuffer resourceBuffer = new StringBuffer();
+    int i;
+
+    while( (i = resourceReader.read()) != -1 )
+      resourceBuffer.append((char) i);
+
+    resourceReader.close();
+    return resourceBuffer.toString();
+  } // getResourceAsString(String)
+
+  /** Get a resource from the GATE resources directory as a byte array.
+    * The resource name should be relative to <TT>gate/resources</TT>; e.g.
+    * for a resource stored as <TT>gate/resources/jape/Test11.jape</TT>,
+    * this method should be passed the name <TT>jape/Test11.jape</TT>.
+    */
+  public static byte[] getResourceAsByteArray(String resourceName)
+  throws IOException {
+    InputStream resourceInputStream = getResourceAsStream(resourceName);
+    BufferedInputStream resourceStream =
+      new BufferedInputStream(resourceInputStream);
+    int len = resourceStream.available();
+    byte[] bytes = new byte[len];
+    resourceStream.read(bytes, 0, len);
+    resourceStream.close();
+
+    return bytes;
+  } // getResourceAsByteArray(String)
+
+  /** Get a resource from the GATE resources directory as an InputStream.
+    * The resource name should be relative to <TT>gate/resources</TT>; e.g.
+    * for a resource stored as <TT>gate/resources/jape/Test11.jape</TT>,
+    * this method should be passed the name <TT>jape/Test11.jape</TT>.
+    */
+  public static InputStream getResourceAsStream(String resourceName)
+  throws IOException {
+    return Class.class.getResourceAsStream("/gate/resources/" + resourceName);
+  } // getResourceAsStream(String)
 
 } // class Files
 
---
+++ src/gate/util/TestFiles.java
@@ -0,0 +1,54 @@
+/*
+ *	TestFiles.java
+ *
+ *	Hamish Cunningham, 10/June/00
+ *
+ *	$Id$
+ */
+
+package gate.util;
+
+import java.util.*;
+import java.io.*;
+import junit.framework.*;
+
+/** Files test class.
+  */
+public class TestFiles extends TestCase
+{
+  /** Construction */
+  public TestFiles(String name) { super(name); }
+
+  /** Fixture set up */
+  public void setUp() {
+  } // setUp
+
+  /** Test the getResourceAs... methods. */
+  public void testGetResources() throws Exception {
+    assert(true);
+    String japeResName = "jape/combined/testloc.jape";
+    String firstLine = "// testloc.jape";
+
+    InputStreamReader resReader =
+      new InputStreamReader(Files.getResourceAsStream(japeResName));
+    BufferedReader bufResReader = new BufferedReader(resReader);
+    assert(bufResReader.readLine().equals(firstLine));
+    resReader.close();
+
+    String resString = Files.getResourceAsString(japeResName);
+    assert(resString.startsWith(firstLine));
+
+    byte[] resBytes = Files.getResourceAsByteArray(japeResName);
+    char resChars[] = new char[firstLine.length()];
+    for(int i=0; i<resChars.length; i++) resChars[i] = (char)resBytes[i];
+    resString = new String(resChars);
+    assert(resString, resString.equals(firstLine));
+
+  } // testGetResources()
+
+  /** Test suite routine for the test runner */
+  public static Test suite() {
+    return new TestSuite(TestFiles.class);
+  } // suite
+
+} // class TestFiles
