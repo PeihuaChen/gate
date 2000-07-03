b67c9f39df8c210e2f1cd4e58753fa87e10a2266
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-07-03 12:59:27 +0000

    added UnicodeType.java

---
+++ src/gate/creole/tokeniser/UnicodeType.java
@@ -0,0 +1,9 @@
+package gate.creole.tokeniser;
+
+/** Used as an object wrapper that holds an Unicode type (the byte value of
+  * the static member of java.lang.Character).
+  */
+class UnicodeType{
+  byte type;
+  UnicodeType(byte type){ this.type = type;}
+}//class UnicodeType
