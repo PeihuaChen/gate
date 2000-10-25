78e210f93e647e4a7858eaaca328bf584b6c6221
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-10-25 15:42:33 +0000

    Added headers to the gazetteer and tokeniser test classes

--- src/gate/creole/gazetteer/TestGazetteer.java
+++ src/gate/creole/gazetteer/TestGazetteer.java
@@ -1,3 +1,20 @@
+/*
+ * TestGazetteer.java
+ *
+ * Copyright (c) 2000-2001, The University of Sheffield.
+ *
+ * This file is part of GATE (see http://gate.ac.uk/), and is free
+ * software, licenced under the GNU Library General Public License,
+ * Version 2, June1991.
+ *
+ * A copy of this licence is included in the distribution in the file
+ * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
+ *
+ * Valentin Tablan, 25/10/2000
+ *
+ * $Id$
+ */
+
 package gate.creole.gazetteer;
 
 /**
