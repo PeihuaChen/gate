b7cbe0c85c72877538ef3a651b9f340ddd6c2d16
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-11-09 15:54:51 +0000

    fixed formatting

--- src/gate/annotation/AnnotationDiffTableModel.java
+++ src/gate/annotation/AnnotationDiffTableModel.java
@@ -1,18 +1,18 @@
 /*
- * AnnotationDiffTableModel.java
+ *  AnnotationDiffTableModel.java
  *
- * Copyright (c) 2000-2001, The University of Sheffield.
+ *  Copyright (c) 1998-2001, The University of Sheffield.
  *
- * This file is part of GATE (see http://gate.ac.uk/), and is free
- * software, licenced under the GNU Library General Public License,
- * Version 2, June1991.
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
  *
- * A copy of this licence is included in the distribution in the file
- * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
+ *  Hamish Cunningham, 11/Feb/2000
  *
- * Cristian URSU, 07/Nov/2000
+ *  Cristian URSU, 07/Nov/2000
  *
- * $Id$
+ *  $Id$
  */
 
 package gate.annotation;
