3c78dcc64e67ed6e9f581bd6cb7656e01dbfa6a8
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2000-11-24 12:03:33 +0000

    Added AbstractVisualResource

---
+++ src/gate/creole/AbstractVisualResource.java
@@ -0,0 +1,23 @@
+package gate.creole;
+
+import javax.swing.JPanel;
+import gate.*;
+
+public abstract class AbstractVisualResource extends JPanel
+                                             implements VisualResource{
+  //no doc required: javadoc will copy it from the interface
+  public FeatureMap getFeatures(){
+    return features;
+  }
+
+  public void setFeatures(FeatureMap features){
+    this.features = features;
+  }
+
+  public Resource init(){
+    return this;
+  }
+  //properties
+  protected FeatureMap features;
+
+}
