c7e54d2b072c8c2972142273871bb62603bcd6c5
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-03-09 11:30:36 +0000

    gate.bat from kali and valy

--- build/gate.bat
+++ build/gate.bat
@@ -1,4 +1,28 @@
-rem gate.bat - DOS front-end for GATE. Hamish, March 2001
+@echo off
+rem A batch file to run GATE
+rem Kali and Valy, 9/3/1
+rem This batch file was written and tested under Windows NT
 rem $Id$
 
-w:\jdk\jdk1.3\bin\java -Djava.ext.dirs=..\lib\ext -classpath gate.jar;..\lib\ext\guk.jar -Xmx200m gate.Main
+rem if the correct version of java isn't in your path, or in the
+rem JAVA_HOME variable, then set JAVA_HOME to the correct location here, e.g.
+rem SET JAVA_HOME=w:\jdk\jdk1.3
+
+rem Guess if JAVA_HOME is defined
+if not "%JAVA_HOME%" == "" goto gothome
+
+SET JAVA=java
+
+goto execute
+
+:gothome
+
+SET JAVA=%JAVA_HOME%\bin\java.exe 
+
+:execute
+
+%JAVA% -Djava.ext.dirs=..\lib\ext -classpath gate.jar;..\lib\ext\guk.jar -Xmx200m gate.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
+
+
