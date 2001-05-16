4138e9c21facbd84db99991310743ead81759ef9
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-05-16 15:37:02 +0000

    Hamish will hate me but I'm doing it anyway: added a new type of URL handler for URLs of type classpath:/mypackage/myresources/AResourceFile.txt They are intended to allow users that know about them to point to their resources from within their own jar.

---
+++ src/gate/util/protocols/classpath/Handler.java
@@ -0,0 +1,20 @@
+package gate.util.protocols.classpath;
+
+import java.net.*;
+import java.io.*;
+
+import gate.util.*;
+
+/**
+ * The handler for the "classpath://" URLs.
+ * All this class does is to transparently transform a "classpath://" URL into
+ * an URL of the according type and forward all requests through it.
+ */
+public class Handler extends URLStreamHandler {
+
+  protected URLConnection openConnection(URL u) throws java.io.IOException {
+    URL actualURL = Handler.class.getResource(u.getPath());
+    if(actualURL == null) throw new FileNotFoundException(u.toExternalForm());
+    return actualURL.openConnection();
+  }
+}
