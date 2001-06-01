c8680aa98a1be61e747e8f8504e262a3180e21ea
hamish <hamish@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-06-01 12:37:20 +0000

    gate.sh now working

--- bin/gate.sh
+++ bin/gate.sh
@@ -2,15 +2,18 @@
 # gate.sh - UNIX front-end for GATE binary distributions. Hamish, May 2001
 # $Id$
 
-LOCAT=`basename $0`
+E=""
+[ x$1 = x-debug ] && E=echo
+
+LOCAT=`dirname $0`
 GATEJAR=${LOCAT}/gate.jar
-GUKJAR=${LOCAT}/ext/guk.jar
+GUK=${LOCAT}/ext
 [ ! -f $GATEJAR ] && GATEJAR=${LOCAT}/../build/gate.jar
-[ ! -f $GUKJAR ] && GUKJAR=${LOCAT}/../lib/ext/guk.jar
+[ ! -f $GUK ] && GUK=${LOCAT}/../lib/ext
 
 JAVA=$JAVA_HOME/bin/java
 [ ! -f $JAVA ] && JAVA=java
 
-${JAVA} -Xmx200m \
-  -Djava.ext.dirs=${GUKJAR} \
-  -classpath ${GATEJAR}/gate.jar:$CLASSPATH gate.Main $*
+$E ${JAVA} -Xmx200m \
+  -Djava.ext.dirs=${GUK} \
+  -classpath ${GATEJAR}:$CLASSPATH gate.Main $*
