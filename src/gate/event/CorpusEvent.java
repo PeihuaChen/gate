934e6230f3db5cd42a90666ba274458b000fdc9f
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-07-13 17:55:17 +0000

    corpus events

---
+++ src/gate/event/CorpusEvent.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Valentin Tablan 13/07/2001
+ *
+ *  $Id$
+ */
+
+package gate.event;
+
+import gate.*;
+
+/**
+ * Models events fired by corpora when documents are added or removed.
+ */
+public class CorpusEvent extends GateEvent {
+
+  /**
+   * Event type that is fired when a new document is added to a corpus
+   */
+  public static int DOCUMENT_ADDED = 401;
+
+  /**
+   * Event type that is fired when a document is removed from a corpus
+   */
+  public static int DOCUMENT_REMOVED = 402;
+
+  /**
+   * Creates a new CorpusEvent.
+   * @param source the corpus that fires the event
+   * @param doc the document this event refers to
+   * @param type the type of event ({@link DOCUMENT_ADDED} or
+   * {@link DOCUMENT_REMOVED}).
+   */
+  public CorpusEvent(Corpus source, Document doc, int type){
+    super(source, type);
+    this.document = doc;
+  }
+
+  /**
+   * Gets the dcument this event refers to
+   */
+  public gate.Document getDocument() {
+    return document;
+  }
+
+  /**
+   * The document that has been added/removed.
+   */
+  private gate.Document document;
+}
+
---
+++ src/gate/event/CorpusListener.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Valentin Tablan 13/07/2001
+ *
+ *  $Id$
+ */
+
+package gate.event;
+
+import java.util.EventListener;
+
+/**
+ * A listener for events fired by {@link gate.Corpus}
+ */
+public interface CorpusListener extends EventListener {
+
+  /**
+   * Called when a document has been added
+   */
+  public void documentAdded(CorpusEvent e);
+
+  /**
+   * Called when a document has been removed
+   */
+  public void documentRemoved(CorpusEvent e);
+}
