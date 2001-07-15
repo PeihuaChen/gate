8e9a0c03682321214a7af0021267ebc6a0f1f167
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-07-15 15:38:20 +0000

    restored the code for getVREnabledAnnTypes()

--- src/gate/creole/CreoleRegisterImpl.java
+++ src/gate/creole/CreoleRegisterImpl.java
@@ -623,26 +623,25 @@ public class CreoleRegisterImpl extends HashMap
    * there are custom viewers/editor registered.
    */
   public List getVREnabledAnnotationTypes(){
-//    LinkedList responseList = new LinkedList();
-//    Iterator vrIterator = vrTypes.iterator();
-//    while (vrIterator.hasNext()){
-//      String vrClassName = (String) vrIterator.next();
-//      ResourceData vrResourceData = (ResourceData) this.get(vrClassName);
-//      if (vrResourceData == null)
-//        throw new GateRuntimeException(
-//          "Couldn't get resource data for VR called " + vrClassName
-//        );
-//      // Test if VR can display all types of annotations
-//      if ( vrResourceData.getGuiType() == ResourceData.NULL_GUI &&
-//           vrResourceData.getAnnotationTypeDisplayed() != null ){
-//
-//          String annotationTypeDisplayed =
-//                                  vrResourceData.getAnnotationTypeDisplayed();
-//          responseList.add(annotationTypeDisplayed);
-//      }// End if
-//    }// End while
-//    return Collections.unmodifiableList(responseList);
-return Arrays.asList(new String[]{"Tree"});
+    LinkedList responseList = new LinkedList();
+    Iterator vrIterator = vrTypes.iterator();
+    while (vrIterator.hasNext()){
+      String vrClassName = (String) vrIterator.next();
+      ResourceData vrResourceData = (ResourceData) this.get(vrClassName);
+      if (vrResourceData == null)
+        throw new GateRuntimeException(
+          "Couldn't get resource data for VR called " + vrClassName
+        );
+      // Test if VR can display all types of annotations
+      if ( vrResourceData.getGuiType() == ResourceData.NULL_GUI &&
+           vrResourceData.getAnnotationTypeDisplayed() != null ){
+
+          String annotationTypeDisplayed =
+                                  vrResourceData.getAnnotationTypeDisplayed();
+          responseList.add(annotationTypeDisplayed);
+      }// End if
+    }// End while
+    return Collections.unmodifiableList(responseList);
   }// getVREnabledAnnotationTypes()
 
 
