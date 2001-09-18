c594ca34f2c32e92c43202216da747593789cdcb
kalina <kalina@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-09-18 09:53:50 +0000

    Added NameBearer interface for names

---
+++ src/gate/util/NameBearer.java
@@ -0,0 +1,30 @@
+/*
+ *  NameBearer.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Kalina Bontcheva, 17/Sept/2001
+ *
+ *  $Id$
+ */
+
+package gate.util;
+import java.util.*;
+import gate.*;
+
+/** Classes that have features.
+  */
+public interface NameBearer
+{
+  /** Sets the name of this resource*/
+  public void setName(String name);
+
+  /** Returns the name of this resource*/
+  public String getName();
+
+} // interface NameBearer
