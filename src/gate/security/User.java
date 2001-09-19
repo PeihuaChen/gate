a2ecb70f42f682bd6b0bda9fd0ff19e07df6cfa1
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-09-19 15:28:28 +0000

    [marin] security related methods

---
+++ src/gate/security/User.java
@@ -0,0 +1,37 @@
+/*
+ *  OracleDataStore.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 19/Sep/2001
+ *
+ */
+
+package gate.security;
+
+import java.util.*;
+
+import gate.persist.PersistenceException;
+
+
+public interface User {
+
+  /** --- */
+  public String getName();
+
+  /** --- */
+  public List getGroups();
+
+  /** --- */
+  public void setName(String newName, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void setPassword(String newPass, Long sessionID)
+    throws PersistenceException,SecurityException;
+}
