b9453724646d5747a9f7e7c73d6c8d29bd5801f1
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-09-19 15:39:32 +0000

    [marin] security related methods

---
+++ src/gate/security/Group.java
@@ -0,0 +1,50 @@
+/*
+ *  Group.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 19/Sep/2001
+ *
+ */
+
+package gate.security;
+
+import java.util.*;
+
+import gate.persist.PersistenceException;
+
+
+public interface Group {
+
+  /** --- */
+  public String getName();
+
+  /** --- */
+  public List getUsers();
+
+  /** --- */
+  public void setName(String newName, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void addUser(Long userID, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void addUser(User usr, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void removeUser(Long userID, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void removeUser(User usr, Long sessionID)
+    throws PersistenceException,SecurityException;
+
+}
---
+++ src/gate/security/Session.java
@@ -0,0 +1,30 @@
+/*
+ *  Group.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 19/Sep/2001
+ *
+ */
+
+package gate.security;
+
+import gate.persist.PersistenceException;
+
+public interface Session {
+
+  /** --- */
+  public Long getID();
+
+  /** --- */
+  public User getUser();
+
+  /** --- */
+  public Group getGroup();
+
+}
