3e2c3cb104000ba98f50ad2a27880f4379b5779a
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-09-20 15:05:29 +0000

    [marin] securty factory (interface and implementation)

---
+++ src/gate/security/AccessController.java
@@ -0,0 +1,81 @@
+/*
+ *  AccessController.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 19/Sep/2001
+ *
+ */
+
+package gate.security;
+
+import gate.persist.PersistenceException;
+
+
+public interface AccessController {
+
+  /** --- */
+  public Group findGroup(String name)
+    throws PersistenceException,SecurityException;;
+
+  /** --- */
+  public Group findGroup(Long id)
+    throws PersistenceException,SecurityException;;
+
+  /** --- */
+  public User findUser(String name)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public User findUser(Long id)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public Session findSession(Long id)
+    throws SecurityException;
+
+  /** --- */
+  public Group createGroup(String name)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void deleteGroup(Long id, Session s)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void deleteGroup(Group grp, Session s)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public User createUser(String name, String passwd)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public User deleteUser(User usr, Session s)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public User deleteUser(Long id, Session s)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public Session login(String usr_name, String passwd,Long prefGroupID)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void logout(Session s)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public void setSessionTimeout(Session s, int timeoutMins)
+    throws PersistenceException,SecurityException;
+
+  /** --- */
+  public boolean isValidSession(Session s);
+
+}
---
+++ src/gate/security/AccessControllerImpl.java
@@ -0,0 +1,115 @@
+/*
+ *  AccessControllerImpl.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 19/Sep/2001
+ *
+ */
+
+package gate.security;
+
+import gate.persist.PersistenceException;
+import gate.util.MethodNotImplementedException;
+
+
+public class AccessControllerImpl implements AccessController {
+
+  public AccessControllerImpl() {
+  }
+
+  /** --- */
+  public Group findGroup(String name){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public Group findGroup(Long id){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public User findUser(String name){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public User findUser(Long id){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public Session findSession(Long id){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public Group createGroup(String name){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public void deleteGroup(Long id, Session s){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public void deleteGroup(Group grp, Session s){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public User createUser(String name, String passwd){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public User deleteUser(User usr, Session s){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public User deleteUser(Long id, Session s){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public Session login(String usr_name, String passwd,Long prefGroupID){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public void logout(Session s){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public void setSessionTimeout(Session s, int timeoutMins){
+
+    throw new MethodNotImplementedException();
+  }
+
+  /** --- */
+  public boolean isValidSession(Session s){
+
+    throw new MethodNotImplementedException();
+  }
+}
