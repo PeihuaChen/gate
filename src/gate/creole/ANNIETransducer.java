969da2d742e3ba9d9211e5b3771fbd7c453670fe
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-09-20 11:20:34 +0000

    New classes for Jape Transducer, ANNIE NE Transducer and VP Chunker

---
+++ src/gate/creole/ANNIETransducer.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Valentin Tablan, 20 Sep 2001
+ *
+ *  $Id$
+ */
+package gate.creole;
+
+/**
+ * The ANNIE named entity transducer.
+ * This is a JAPE transducer and this class is here to allow the specification
+ * in creole.xml of a default grammar to be used in .
+ */
+public class ANNIETransducer extends Transducer {
+}
---
+++ src/gate/creole/VPChunker.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Valentin Tablan, 20 Sep 2001
+ *
+ *  $Id$
+ */
+package gate.creole;
+
+/**
+ * ANNIE VP Chunker module. It is actually a JAPE grammar; this class is here
+ * so we can have a separate entry in creol.xml inorder to point to the default
+ * VP chunking grammar.
+ */
+public class VPChunker extends Transducer {
+}
