6ed0e42d1b97f5688b71b0ee399e77fc5023ea1f
kalina <kalina@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-10-04 15:10:46 +0000

    Added the actions for adding/deleting/etc but not all are implemented yet

--- src/gate/gui/UserGroupEditor.java
+++ src/gate/gui/UserGroupEditor.java
@@ -40,6 +40,8 @@ public class UserGroupEditor extends JComponent {
 
   protected boolean usersFirst = true;
   protected JButton exitButton = new JButton();
+  protected JPopupMenu userMenu = new JPopupMenu();
+  protected JPopupMenu groupMenu = new JPopupMenu();
 
   /** JDBC URL */
   private static final String JDBC_URL =
@@ -57,7 +59,7 @@ public class UserGroupEditor extends JComponent {
     this.session = theSession;
     this.controller = ac;
 
-    readSecurityData();
+    showUsersFirst();
 
   }
 
@@ -91,7 +93,9 @@ public class UserGroupEditor extends JComponent {
 
   private void jbInit() throws Exception {
     this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-    jPanel2.setLayout(new BorderLayout(40, 40));
+//    jPanel2.setLayout(new BorderLayout(40, 40));
+    jPanel2.setLayout(new BoxLayout(jPanel2,BoxLayout.X_AXIS));
+
 
 //    jPanel1.setSize(800, 100);
 //    jPanel2.setSize(800, 500);
@@ -118,9 +122,16 @@ public class UserGroupEditor extends JComponent {
     jPanel1.add(displayGroupsFirst);
 
     this.add(jPanel2, null);
-    jPanel2.add(firstList, BorderLayout.WEST);
-    jPanel2.add(secondList, BorderLayout.EAST);
+    jPanel2.add(new JScrollPane(firstList), BorderLayout.WEST);
+    jPanel2.add(Box.createHorizontalStrut(50));
+    jPanel2.add(new JScrollPane(secondList), BorderLayout.EAST);
     firstList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    firstList.setModel(new DefaultListModel());
+    firstList.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent e) {
+        listRightMouseClick(e);
+      }//mouse clicked
+    });
     firstList.getSelectionModel().addListSelectionListener(
       new ListSelectionListener() {
         public void valueChanged(ListSelectionEvent e) {
@@ -128,6 +139,15 @@ public class UserGroupEditor extends JComponent {
         }//
       }//the selection listener
     );
+    secondList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    secondList.setModel(new DefaultListModel());
+    secondList.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent e) {
+        listRightMouseClick(e);
+      }//mouse clicked
+    });
+
+    this.add(Box.createVerticalGlue());
 
     this.add(exitButton);
     exitButton.setText("Exit");
@@ -144,14 +164,52 @@ public class UserGroupEditor extends JComponent {
       } //actionPerformed
     });
     this.add(Box.createVerticalStrut(50));
+
   }
 
   private void showUsersFirst() {
+    DefaultListModel firstListData = (DefaultListModel) firstList.getModel();
+    DefaultListModel secondListData = (DefaultListModel) secondList.getModel();
+    firstListData.clear();
+    secondListData.clear();
+
+    readUsers(firstListData, firstList);
   }
 
+  private void readUsers(DefaultListModel listModel, JList list) {
+    //get the names of all users
+    try {
+      java.util.List users = controller.listUsers();
+      for (int i = 0; i < users.size(); i++)
+        listModel.addElement(users.get(i));
+      list.setModel(listModel);
+    } catch (gate.persist.PersistenceException ex) {
+      throw new gate.util.GateRuntimeException("Cannot read users!");
+    }
+
+  }//readUsers
+
   private void showGroupsFirst() {
-    Out.prln("Groups go first");
+    DefaultListModel firstListData = (DefaultListModel) firstList.getModel();
+    DefaultListModel secondListData = (DefaultListModel) secondList.getModel();
+    firstListData.clear();
+    secondListData.clear();
+
+    readGroups(firstListData, firstList);
   }
+
+  private void readGroups(DefaultListModel listModel, JList list) {
+    //get the names of all groups
+    try {
+      java.util.List groups = controller.listGroups();
+      for (int i = 0; i < groups.size(); i++)
+        listModel.addElement(groups.get(i));
+      list.setModel(listModel);
+    } catch (gate.persist.PersistenceException ex) {
+      throw new gate.util.GateRuntimeException("Cannot read groups!");
+    }
+
+  }//readGroups
 
   void displayUsersFirst_itemStateChanged(ItemEvent e) {
     if (e.getStateChange() == e.DESELECTED) {
@@ -171,10 +229,94 @@ public class UserGroupEditor extends JComponent {
     }
   } //display users first (de-)selected
 
+  void listRightMouseClick(MouseEvent e) {
+        //if it's not a right click, then return
+        //coz we're not interested
+        if (! SwingUtilities.isRightMouseButton(e))
+          return;
+
+        JList theList = (JList) e.getSource();
+        //check if we have a selection and if not, try to force it
+        if (theList.getSelectedIndex() == -1) {
+          int index = theList.locationToIndex(e.getPoint());
+          if (index == -1)
+            return;
+          else
+            theList.setSelectedIndex(index);
+        } else
+            //if the right click is outside the currently selected item return
+            if ( theList.locationToIndex(e.getPoint())
+                 !=  theList.getSelectedIndex())
+              return;
+
+
+        if (theList.equals(firstList)) {
+          if (usersFirst)
+            showUsersMenu(theList,
+                          (int) e.getPoint().getX(),
+                          (int) e.getPoint().getY());
+          else
+            showGroupsMenu(theList,
+                          (int) e.getPoint().getX(),
+                          (int) e.getPoint().getY());
+
+        } else {
+          if (usersFirst)
+            showGroupsMenu(theList,
+                          (int) e.getPoint().getX(),
+                          (int) e.getPoint().getY());
+          else
+            showUsersMenu(theList,
+                          (int) e.getPoint().getX(),
+                          (int) e.getPoint().getY());
+
+        }
+
+  }
+
+  private void showUsersMenu(JList source, int x, int y) {
+    //create the menu items first
+    userMenu.removeAll();
+    userMenu.add(new CreateUserAction(source));
+    userMenu.add(new DeleteUserAction(source));
+    userMenu.addSeparator();
+    userMenu.add(new Add2GroupAction(source));
+    userMenu.add(new RemoveFromGroupAction(source));
+    userMenu.addSeparator();
+    userMenu.add(new ChangePasswordAction(source));
+    userMenu.add(new RenameUserAction(source));
+
+    userMenu.show(source, x, y);
+
+  }//create and show the menu for user manipulation
+
+  private void showGroupsMenu(JList source, int x, int y) {
+    //create the menu items first
+    groupMenu.removeAll();
+    groupMenu.add(new AddGroupAction(source));
+    groupMenu.add(new DeleteGroupAction(source));
+    groupMenu.addSeparator();
+    groupMenu.add(new AddUserAction(source));
+    groupMenu.add(new RemoveUserAction(source));
+    groupMenu.addSeparator();
+    groupMenu.add(new RenameGroupAction(source));
+
+    groupMenu.show(source, x, y);
+
+  }
+
   //called when the selection changes in the first list
   void firstListItemSelected(ListSelectionEvent e) {
-    int i = e.getFirstIndex();
+    int i = firstList.getSelectedIndex();
     String name = (String) firstList.getModel().getElementAt(i);
+
+    if (usersFirst)
+      showGroupsForUser(name);
+    else
+      showUsersForGroup(name);
+  } //firstListItemSelected
+
+  protected void showGroupsForUser(String name) {
     User user = null;
     try {
       user = controller.findUser(name);
@@ -207,22 +349,216 @@ public class UserGroupEditor extends JComponent {
       }//for loop
       secondList.setModel(secondListData);
 
+  }//showGroupsForUser
 
-  }//firstListItemSelected
 
-
-  private void readSecurityData() {
-    //get the names of all users
+  protected void showUsersForGroup(String name) {
+    Group group = null;
     try {
-      java.util.List users = controller.listUsers();
-      DefaultListModel firstListData = new DefaultListModel();
-      for (int i = 0; i < users.size(); i++)
-        firstListData.addElement(users.get(i));
-      firstList.setModel(firstListData);
+      group = controller.findGroup(name);
     } catch (gate.persist.PersistenceException ex) {
-      throw new gate.util.GateRuntimeException("Cannot read users!");
+      throw new gate.util.GateRuntimeException(
+                  "Cannot locate the group with name: " + name
+                );
+    } catch (gate.security.SecurityException ex1) {
+      throw new gate.util.GateRuntimeException(
+                  ex1.getMessage()
+                );
+    }
+    if (group == null)
+      return;
+    java.util.List myUsers = group.getUsers();
+    if (myUsers == null)
+      return;
+
+      DefaultListModel secondListData = new DefaultListModel();
+
+      for (int j = 0; j< myUsers.size(); j++) {
+        try {
+          User myUser = controller.findUser((Long) myUsers.get(j));
+          secondListData.addElement(myUser.getName());
+        } catch (Exception ex) {
+          throw new gate.util.GateRuntimeException(
+                  ex.getMessage()
+                );
+        }//catch
+      }//for loop
+      secondList.setModel(secondListData);
+
+  }//showGroupsForUser
+
+
+  protected class CreateUserAction extends AbstractAction{
+    private JList source;
+
+    public CreateUserAction(JList source){
+      super("Create new user");
+      this.source = source;
     }
 
-  }//readSecurityData
+    public void actionPerformed(ActionEvent e){
+      try {
+        controller.createUser("myUser", "myPassword");
+      } catch (gate.persist.PersistenceException ex) {
+        throw new gate.util.GateRuntimeException(ex.getMessage());
+      } catch (gate.security.SecurityException ex1) {
+        throw new gate.util.GateRuntimeException(ex1.getMessage());
+      }
+      DefaultListModel model = (DefaultListModel) source.getModel();
+      model.clear();
+      readUsers(model, source);
+    }//public void actionPerformed(ActionEvent e)
+  } //CreateUserAction
 
-}
+  protected class DeleteUserAction extends AbstractAction{
+    private JList source;
+
+    public DeleteUserAction(JList source){
+      super("Delete user");
+      this.source = source;
+    }
+
+    public void actionPerformed(ActionEvent e){
+      //first get the index of the selection
+      int index = source.getSelectedIndex();
+      if (index == -1) //return if no selection
+        return;
+      DefaultListModel model = (DefaultListModel) source.getModel();
+      try {
+      User user = controller.findUser((String) model.get(index) );
+      controller.deleteUser(user, session);
+      model.remove(index);
+      } catch (gate.persist.PersistenceException ex) {
+        throw new gate.util.GateRuntimeException(ex.getMessage());
+      } catch (gate.security.SecurityException ex1) {
+        throw new gate.util.GateRuntimeException(ex1.getMessage());
+      }
+    }//public void actionPerformed(ActionEvent e)
+  } //DeleteUserAction
+
+  protected class Add2GroupAction extends AbstractAction{
+    private JList source;
+
+    public Add2GroupAction(JList source){
+      super("Add to group");
+      this.source = source;
+    }
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to add the user to a group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //Add2GroupAction
+
+  protected class RemoveFromGroupAction extends AbstractAction{
+    private JList source;
+
+    public RemoveFromGroupAction(JList source){
+      super("Remove from group");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to remove the user from a group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //RemoveFromGroupAction
+
+
+  protected class ChangePasswordAction extends AbstractAction{
+    private JList source;
+
+    public ChangePasswordAction(JList source){
+      super("Change password");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to change the password!");
+    }//public void actionPerformed(ActionEvent e)
+  } //ChangePasswordAction
+
+
+  protected class RenameUserAction extends AbstractAction{
+    private JList source;
+
+    public RenameUserAction(JList source){
+      super("Rename user");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to change the user's name!");
+    }//public void actionPerformed(ActionEvent e)
+  } //RenameUserAction
+
+
+  protected class AddGroupAction extends AbstractAction{
+    private JList source;
+
+    public AddGroupAction(JList source){
+      super("Create new group");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to create a new group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //AddGroupAction
+
+
+  protected class DeleteGroupAction extends AbstractAction{
+    private JList source;
+
+    public DeleteGroupAction(JList source){
+      super("Delete group");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to delete the group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //DeleteGroupAction
+
+
+  protected class AddUserAction extends AbstractAction{
+    private JList source;
+
+    public AddUserAction(JList source){
+      super("Add user");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to add a user to the group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //AddUserAction
+
+
+  protected class RemoveUserAction extends AbstractAction{
+    private JList source;
+
+    public RemoveUserAction(JList source){
+      super("Remove user");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to remove a user from the group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //RemoveUserAction
+
+
+  protected class RenameGroupAction extends AbstractAction{
+    private JList source;
+
+    public RenameGroupAction(JList source){
+      super("Rename group");
+      this.source = source;
+    }//
+
+    public void actionPerformed(ActionEvent e){
+      Out.prln("I need to rename the group!");
+    }//public void actionPerformed(ActionEvent e)
+  } //RenameGroupAction
+
+} //UserGroupEditor
+
