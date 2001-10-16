7548c833ea62f623f6289054b50a69ad29325abd
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-10-16 18:05:50 +0000

    [marin] Document implementation that will load content/anotations on demand

---
+++ src/gate/corpora/DatabaseDocumentImpl.java
@@ -0,0 +1,22 @@
+/*
+ *  DatabaseDocumentImpl.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 16/Oct/2001
+ *
+ *  $Id$
+ */
+
+package gate.corpora;
+
+public class DatabaseDocumentImpl extends DocumentImpl {
+
+  public DatabaseDocumentImpl() {
+  }
+}
