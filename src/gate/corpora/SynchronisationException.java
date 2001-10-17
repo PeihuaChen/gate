5bbeb39645869ff597c72805def5522cac2541ed
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-10-17 11:47:48 +0000

    [marin]

---
+++ src/gate/corpora/SynchronisationException.java
@@ -0,0 +1,25 @@
+/*
+ *  SynchronisationException.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 17/Oct/2001
+ *
+ *  $Id$
+ */
+
+package gate.corpora;
+
+import gate.util.GateRuntimeException;
+
+
+public class SynchronisationException extends GateRuntimeException {
+
+  public SynchronisationException() {
+  }
+}
