dbd054f5b6f2294d020396196949e21cde9fa5df
kalina <kalina@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-10-19 10:59:57 +0000

    The new document reset PR has been added. It implements document reset for all transient, XML-stored and serialisation documents (don't forget to say save for the latter).

---
+++ src/gate/creole/annotdelete/AnnotationDeletePR.java
@@ -0,0 +1,86 @@
+/*
+ *  AnnotationDeletePR.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Kalina Bontcheva, 19/10/2001
+ *
+ *  $Id$
+ */
+
+package gate.creole.annotdelete;
+
+import java.util.*;
+import gate.*;
+import gate.creole.*;
+import gate.util.*;
+
+/**
+ * This class is the implementation of a processing resource which
+ * deletes all annotations and sets other than 'original markups'.
+ * If put at the start of an application, it'll ensure that the
+ * document is restored to its clean state before being processed.
+ */
+public class AnnotationDeletePR extends AbstractLanguageAnalyser
+  implements ProcessingResource {
+
+  protected String                markupSetName = "Original markups";
+
+  /** Initialise this resource, and return it. */
+  public Resource init() throws ResourceInstantiationException
+  {
+    return super.init();
+  } // init()
+
+  /**
+  * Reinitialises the processing resource. After calling this method the
+  * resource should be in the state it is after calling init.
+  * If the resource depends on external resources (such as rules files) then
+  * the resource will re-read those resources. If the data used to create
+  * the resource has changed since the resource has been created then the
+  * resource will change too after calling reInit().
+  */
+  public void reInit() throws ResourceInstantiationException
+  {
+    init();
+  } // reInit()
+
+  /** Run the resource. */
+  public void execute() throws ExecutionException {
+
+    if(document == null)
+      throw new GateRuntimeException("No document to process!");
+
+    //first clear the default set, which cannot be removed
+    document.getAnnotations().clear();
+
+    //get the names of all sets
+    Map namedSets = document.getNamedAnnotationSets();
+    //nothing left to do if there are no named sets
+    if (namedSets.isEmpty())
+      return;
+
+    //loop through the sets and delete them all unless they're original markups
+    Iterator iter = namedSets.keySet().iterator();
+    while (iter.hasNext()) {
+      String setName = (String) iter.next();
+      if (! setName.equals(markupSetName))
+        document.removeAnnotationSet(setName);
+    }
+
+  } // execute()
+
+  public void setMarkupASName(String newMarkupASName) {
+    markupSetName = newMarkupASName;
+  }
+
+  public String  getMarkupASName() {
+    return markupSetName;
+  }
+
+} // class AnnotationSetTransfer
