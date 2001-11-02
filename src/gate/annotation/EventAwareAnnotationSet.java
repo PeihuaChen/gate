c0152513225800c143b9e3a1a2dc4b1f26453454
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-02 16:53:34 +0000

    [marin] extends AnnotationSet, gives acces to the IDs of the annotations added/ deleted/updated since the last sync

---
+++ src/gate/annotation/EventAwareAnnotationSet.java
@@ -0,0 +1,34 @@
+/*
+ *  EventAwareAnnotationSet.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 02/Nov/2001
+ *
+ *  NOTE: the interface is not public
+ *
+ *  $Id$
+ */
+
+package gate.annotation;
+
+import java.util.*;
+
+import gate.AnnotationSet;
+
+
+
+interface EventAwareAnnotationSet extends AnnotationSet {
+
+  public Collection getAddedAnnotationIDs();
+
+  public Collection getChangedAnnotationIDs();
+
+  public Collection getRemovedAnnotationIDs();
+
+}
