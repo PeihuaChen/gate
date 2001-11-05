fd217c1b705590fcd838babe44d22afd303ab7ef
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-05 16:36:30 +0000

    renamed

---
+++ src/gate/creole/ANNIEConstants.java
@@ -0,0 +1,35 @@
+/*
+ *  AnnieConstants.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Cristian URSU, 16/Oct/2001
+ *
+ *  $Id$
+ */
+
+package gate.creole;
+
+/** This interface defines ANNIE constants */
+public interface ANNIEConstants{
+
+  /**
+   * This is an array of strings containing all class names for all ANNIE PRs
+   */
+  public static final String[] PR_NAMES = {
+                                      "gate.creole.tokeniser.DefaultTokeniser",
+                                      "gate.creole.gazetteer.DefaultGazetteer",
+                                      "gate.creole.splitter.SentenceSplitter",
+                                      "gate.creole.POSTagger",
+                                      "gate.creole.ANNIETransducer",
+                                      "gate.creole.orthomatcher.OrthoMatcher"};
+
+
+  public static final String DOCUMENT_COREF_FEATURE_NAME = "MatchesAnnots";
+  public static final String ANNOTATION_COREF_FEATURE_NAME = "matches";
+}// AnnieConstants
