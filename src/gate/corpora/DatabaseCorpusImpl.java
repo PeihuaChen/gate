58c05684febb0b229d35635bfc2a74f9ab753cf8
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-05 19:43:28 +0000

    [marin] partial implementation

--- src/gate/corpora/DatabaseCorpusImpl.java
+++ src/gate/corpora/DatabaseCorpusImpl.java
@@ -15,8 +15,63 @@
 
 package gate.corpora;
 
+import java.util.*;
+
+import gate.*;
+import gate.persist.*;
+import gate.annotation.*;
+import gate.creole.*;
+import gate.event.*;
+import gate.util.*;
+
+
 public class DatabaseCorpusImpl extends CorpusImpl {
 
   public DatabaseCorpusImpl() {
+
+    super();
   }
+
+  public boolean add(Object o){
+
+    //accept only documents
+    if (false == o instanceof Document) {
+      throw new IllegalArgumentException("");
+    }
+
+    LanguageResource lr = (LanguageResource)o;
+    Long lrID = (Long)lr.getLRPersistenceId();
+
+    //assert docs are either transient or from the same datastore
+    if (null == this.getDataStore()) {
+      //accept only transient doc
+      if (null == lrID) {
+        //ok, accept it
+        return super.add(o);
+      }
+    }
+    else {
+      //accept transient docs + these from our DS
+
+      if (null == lrID || lr.getDataStore().equals(this.getDataStore())) {
+        //ok, accept it
+        return super.add(o);
+      }
+    }
+
+    return false;
+  }
+
+
+  public boolean addAll(Collection c){
+
+    throw new MethodNotImplementedException();
+//    return supportList.addAll(c);
+  }
+
+  public boolean addAll(int index, Collection c){
+    throw new MethodNotImplementedException();
+//    return supportList.addAll(index, c);
+  }
+
 }
