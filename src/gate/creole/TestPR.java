dcbdbaf55707233c9c3aada23a7c93fc6d4bd462
oana <oana@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-06 11:15:21 +0000

    change the hard-coded names of the documents

--- src/gate/creole/TestPR.java
+++ src/gate/creole/TestPR.java
@@ -90,14 +90,15 @@ public class TestPR extends TestCase
     //run the tokeniser for doc1
     tokeniser.setDocument(doc1);
     tokeniser.execute();
-    assertTrue("Found in ft-bt-03-aug-2001.html "+ doc1.getAnnotations().size() +
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+
+      doc1.getAnnotations().size() +
       " Token annotations, instead of the expected 1286.",
       doc1.getAnnotations().size()== 1286);
 
     //run the tokeniser for doc2
     tokeniser.setDocument(doc2);
     tokeniser.execute();
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+ doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().size() +
       " Token annotations, instead of the expected 2144.",
       doc2.getAnnotations().size()== 2144);
@@ -105,7 +106,7 @@ public class TestPR extends TestCase
     //run the tokeniser for doc3
     tokeniser.setDocument(doc3);
     tokeniser.execute();
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+ doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().size() +
       " Token annotations, instead of the expected 2812.",
       doc3.getAnnotations().size()== 2812);
@@ -121,7 +122,7 @@ public class TestPR extends TestCase
     //run gazetteer for doc1
     gaz.setDocument(doc1);
     gaz.execute();
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+ doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Lookup").size() +
       " Lookup annotations, instead of the expected 47.",
       doc1.getAnnotations().get("Lookup").size()== 47);
@@ -129,7 +130,7 @@ public class TestPR extends TestCase
     //run gazetteer for doc2
     gaz.setDocument(doc2);
     gaz.execute();
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+ doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Lookup").size() +
       " Lookup annotations, instead of the expected 99.",
       doc2.getAnnotations().get("Lookup").size()== 99);
@@ -137,7 +138,7 @@ public class TestPR extends TestCase
     //run gazetteer for doc3
     gaz.setDocument(doc3);
     gaz.execute();
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+ doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Lookup").size() +
       " Lookup annotations, instead of the expected 112.",
       doc3.getAnnotations().get("Lookup").size()== 112);
@@ -152,12 +153,12 @@ public class TestPR extends TestCase
     //run splitter for doc1
     splitter.setDocument(doc1);
     splitter.execute();
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+ doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Sentence").size() +
       " Sentence annotations, instead of the expected 22.",
       doc1.getAnnotations().get("Sentence").size()== 22);
 
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+ doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Split").size() +
       " Split annotations, instead of the expected 36.",
       doc1.getAnnotations().get("Split").size()== 36);
@@ -166,12 +167,12 @@ public class TestPR extends TestCase
     //run splitter for doc2
     splitter.setDocument(doc2);
     splitter.execute();
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+ doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Sentence").size() +
       " Sentence annotations, instead of the expected 53.",
       doc2.getAnnotations().get("Sentence").size()== 53);
 
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+ doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Split").size() +
       " Split annotations, instead of the expected 71.",
       doc2.getAnnotations().get("Split").size()== 71);
@@ -180,12 +181,12 @@ public class TestPR extends TestCase
     splitter.setDocument(doc3);
     splitter.execute();
 
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+ doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Sentence").size() +
       " Sentence annotations, instead of the expected 65.",
       doc3.getAnnotations().get("Sentence").size()== 65);
 
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+ doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Split").size() +
       " Split annotations, instead of the expected 85.",
       doc3.getAnnotations().get("Split").size()== 85);
@@ -207,7 +208,7 @@ public class TestPR extends TestCase
     AnnotationSet annots =
                   doc1.getAnnotations().get("Token", fType);
 
-    assertTrue("Found in ft-bt-03-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+ doc1.getSourceUrl().getFile()+ " "+ annots.size() +
       " Token annotations with category feature, instead of the expected 675.",
       annots.size() == 675);
 
@@ -215,7 +216,7 @@ public class TestPR extends TestCase
     tagger.setDocument(doc2);
     tagger.execute();
     annots = doc2.getAnnotations().get("Token", fType);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+  doc2.getSourceUrl().getFile()+ " "+annots.size() +
       " Token annotations with category feature, instead of the expected 1131.",
       annots.size() == 1131);
 
@@ -223,7 +224,7 @@ public class TestPR extends TestCase
     tagger.setDocument(doc3);
     tagger.execute();
     annots = doc3.getAnnotations().get("Token", fType);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+ doc3.getSourceUrl().getFile()+ " "+ annots.size() +
       " Token annotations with category feature, instead of the expected 1426.",
       annots.size() == 1426);
     Factory.deleteResource(tagger);
@@ -237,23 +238,23 @@ public class TestPR extends TestCase
     //run the transducer for doc1
     transducer.setDocument(doc1);
     transducer.execute();
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+ doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Organization").size() +
       " Organization annotations, instead of the expected 16",
       doc1.getAnnotations().get("Organization").size()== 16);
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Location").size() +
       " Location annotations, instead of the expected 3",
       doc1.getAnnotations().get("Location").size()== 3);
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Person").size() +
       " Person annotations, instead of the expected 3",
       doc1.getAnnotations().get("Person").size()== 3);
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Date").size() +
       " Date annotations, instead of the expected 6",
       doc1.getAnnotations().get("Date").size()== 6);
-    assertTrue("Found in ft-bt-03-aug-2001.html "+
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+
       doc1.getAnnotations().get("Money").size() +
       " Money annotations, instead of the expected 1",
       doc1.getAnnotations().get("Money").size()== 1);
@@ -261,23 +262,23 @@ public class TestPR extends TestCase
     //run the transducer for doc2
     transducer.setDocument(doc2);
     transducer.execute();
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Organization").size() +
       " Organization annotations, instead of the expected 18",
       doc2.getAnnotations().get("Organization").size()== 18);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Location").size() +
       " Location annotations, instead of the expected 9",
       doc2.getAnnotations().get("Location").size()== 9);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Person").size() +
       " Person annotations, instead of the expected 1",
       doc2.getAnnotations().get("Person").size()== 1);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Date").size() +
       " Date annotations, instead of the expected 6",
       doc2.getAnnotations().get("Date").size()== 6);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+
       doc2.getAnnotations().get("Money").size() +
       " Money annotations, instead of the expected 3",
       doc2.getAnnotations().get("Money").size()== 3);
@@ -285,23 +286,23 @@ public class TestPR extends TestCase
     //run the transducer for doc3
     transducer.setDocument(doc3);
     transducer.execute();
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Organization").size() +
       " Organization annotations, instead of the expected 9",
       doc3.getAnnotations().get("Organization").size()== 9);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Location").size() +
       " Location annotations, instead of the expected 12",
       doc3.getAnnotations().get("Location").size()== 12);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Person").size() +
       " Person annotations, instead of the expected 8",
       doc3.getAnnotations().get("Person").size()== 8);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Date").size() +
       " Date annotations, instead of the expected 7",
       doc3.getAnnotations().get("Date").size()== 7);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+
       doc3.getAnnotations().get("Money").size() +
       " Money annotations, instead of the expected 4",
       doc3.getAnnotations().get("Money").size()== 4);
@@ -325,7 +326,7 @@ public class TestPR extends TestCase
     AnnotationSet annots =
                   doc1.getAnnotations().get(null,fType);
 
-    assertTrue("Found in ft-bt-03-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+doc1.getSourceUrl().getFile()+ " "+ annots.size() +
       " annotations with matches feature, instead of the expected 29.",
       annots.size() == 29);
 
@@ -333,7 +334,7 @@ public class TestPR extends TestCase
     orthomatcher.setDocument(doc2);
     orthomatcher.execute();
     annots = doc2.getAnnotations().get(null,fType);
-    assertTrue("Found in gu-Am-Brit-4-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+doc2.getSourceUrl().getFile()+ " "+ annots.size() +
       " annotations with matches feature, instead of the expected 35.",
       annots.size() == 33);
 
@@ -342,7 +343,7 @@ public class TestPR extends TestCase
     orthomatcher.execute();
 
     annots = doc3.getAnnotations().get(null,fType);
-    assertTrue("Found in in-outlook-09-aug-2001.html "+ annots.size() +
+    assertTrue("Found in "+doc3.getSourceUrl().getFile()+ " "+ annots.size() +
       " annotations with matches feature, instead of the expected 20.",
       annots.size() == 20);
     Factory.deleteResource(orthomatcher);
@@ -493,6 +494,7 @@ public class TestPR extends TestCase
 
   public static void main(String[] args) {
     try{
+
       Gate.init();
       TestPR testPR = new TestPR("");
       testPR.setUp();
