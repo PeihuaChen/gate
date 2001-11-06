e1bd341a8a8b492ae92477a3f210809c52b78a78
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-06 12:36:28 +0000

    [marin] partial implementation

--- src/gate/corpora/DatabaseCorpusImpl.java
+++ src/gate/corpora/DatabaseCorpusImpl.java
@@ -17,6 +17,8 @@ package gate.corpora;
 
 import java.util.*;
 
+import junit.framework.*;
+
 import gate.*;
 import gate.persist.*;
 import gate.annotation.*;
@@ -25,53 +27,142 @@ import gate.event.*;
 import gate.util.*;
 
 
-public class DatabaseCorpusImpl extends CorpusImpl {
+public class DatabaseCorpusImpl extends CorpusImpl
+                                implements DatastoreListener {
 
   public DatabaseCorpusImpl() {
 
     super();
+
+    //4. add self as listener for the data store, so that we'll know when the DS is
+    //synced and we'll clear the isXXXChanged flags
+    this.dataStore.addDatastoreListener(this);
   }
 
   public boolean add(Object o){
 
+    Assert.assertNotNull(o);
+    boolean result = false;
+
     //accept only documents
     if (false == o instanceof Document) {
-      throw new IllegalArgumentException("");
+      throw new IllegalArgumentException();
     }
 
-    LanguageResource lr = (LanguageResource)o;
-    Long lrID = (Long)lr.getLRPersistenceId();
+    Document doc = (Document)o;
 
     //assert docs are either transient or from the same datastore
-    if (null == this.getDataStore()) {
-      //accept only transient doc
-      if (null == lrID) {
-        //ok, accept it
-        return super.add(o);
-      }
-    }
-    else {
-      //accept transient docs + these from our DS
-
-      if (null == lrID || lr.getDataStore().equals(this.getDataStore())) {
-        //ok, accept it
-        return super.add(o);
-      }
+    if (isValidForAdoption(doc)) {
+      result = super.add(doc);
     }
 
-    return false;
+    if (result) {
+      fireDocumentAdded(new CorpusEvent(this,
+                                        doc,
+                                        this.documentsList.size()-1,
+                                        CorpusEvent.DOCUMENT_ADDED));
+    }
+
+    return result;
   }
+
+
+  public void add(int index, Object element){
+
+    Assert.assertNotNull(element);
+    Assert.assertTrue(index >= 0);
+
+    long    collInitialSize = this.documentsList.size();
+
+    //accept only documents
+    if (false == element instanceof Document) {
+      throw new IllegalArgumentException();
+    }
+
+    Document doc = (Document)element;
+
+    //assert docs are either transient or from the same datastore
+    if (isValidForAdoption(doc)) {
+      super.add(index,doc);
+
+      //if added then fire event
+      if (this.documentsList.size() > collInitialSize) {
+        fireDocumentAdded(new CorpusEvent(this,
+                                          doc,
+                                          index,
+                                          CorpusEvent.DOCUMENT_ADDED));
+      }
+    }
+  }
+
 
 
   public boolean addAll(Collection c){
 
-    throw new MethodNotImplementedException();
-//    return supportList.addAll(c);
+    boolean collectionChanged = false;
+
+    Iterator it = c.iterator();
+    while (it.hasNext()) {
+      Document doc = (Document)it.next();
+      if (isValidForAdoption(doc)) {
+        collectionChanged |= add(doc);
+      }
+    }
+
+    return collectionChanged;
   }
+
 
   public boolean addAll(int index, Collection c){
-    throw new MethodNotImplementedException();
-//    return supportList.addAll(index, c);
+
+    Assert.assertTrue(index >=0);
+
+    //funny enough add(index,element) returns void and not boolean
+    //so we can't use it
+    boolean collectionChanged = false;
+    int collInitialSize = this.documentsList.size();
+    int currIndex = index;
+
+    Iterator it = c.iterator();
+    while (it.hasNext()) {
+      Document doc = (Document)it.next();
+      if (isValidForAdoption(doc)) {
+        add(currIndex++,doc);
+      }
+    }
+
+    return (this.documentsList.size() > collInitialSize);
   }
 
+
+  private boolean isValidForAdoption(LanguageResource lr) {
+
+    Long lrID = (Long)lr.getLRPersistenceId();
+
+    if (null == lrID ||
+        (this.getDataStore() != null && lr.getDataStore().equals(this.getDataStore()))) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public void resourceAdopted(DatastoreEvent evt){
+  }
+
+  public void resourceDeleted(DatastoreEvent evt){
+
+    Assert.assertNotNull(evt);
+    Assert.assertNotNull(evt.getResourceID());
+
+    //unregister self as listener from the DataStore
+    if (evt.getResourceID().equals(this.getLRPersistenceId())) {
+      //someone deleted this document
+      getDataStore().removeDatastoreListener(this);
+    }
+  }
+
+  public void resourceWritten(DatastoreEvent evt){
+  }
 }
