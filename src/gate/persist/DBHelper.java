696c274ba05a572090f9d14dcdcdc96856ca9ef8
kalina <kalina@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-09 13:52:42 +0000

    changed the name of the CorpusImpl to be DatabaseCorpusImpl as is correct

--- src/gate/persist/DBHelper.java
+++ src/gate/persist/DBHelper.java
@@ -62,7 +62,7 @@ public class DBHelper {
   public static final int EMPTY_CONTENT = 3;
 
   public static final String DOCUMENT_CLASS = "gate.corpora.DatabaseDocumentImpl";
-  public static final String CORPUS_CLASS =  "gate.corpora.CorpusImpl";
+  public static final String CORPUS_CLASS =  "gate.corpora.DatabaseCorpusImpl";
 
   public static final String  DB_PARAMETER_GUID = "DB_GUID";
 
