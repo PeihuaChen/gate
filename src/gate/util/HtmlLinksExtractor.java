7164bb76f7547e06febbd46972b89a9f51b48a53
cursu <cursu@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-11-20 20:07:57 +0000

    upgraded to include H1s and to avoid other folders

--- src/gate/util/HtmlLinksExtractor.java
+++ src/gate/util/HtmlLinksExtractor.java
@@ -22,7 +22,7 @@ import javax.swing.text.*;
 import java.util.*;
 import java.io.*;
 
-/** 
+/**
  * This class extracts links from HTML files.
  * Implements the behaviour of the HTML reader.
  * Methods of an object of this class are called by the HTML parser when
@@ -41,66 +41,79 @@ public class HtmlLinksExtractor extends ParserCallback {
     * not an empty one.
     */
   public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+
     currentTag = t;
-
     if (HTML.Tag.A == t){
-      Out.prln("<"+t);
-      // Construct a feature map from the attributes list
-      Map fm = new HashMap();
-      // Take all the attributes an put them into the feature map
-      if (0 != a.getAttributeCount()){
-        Enumeration enum = a.getAttributeNames();
-        while (enum.hasMoreElements()){
-          Object attribute = enum.nextElement();
-          fm.put(attribute.toString(),(a.getAttribute(attribute)).toString());
-          Out.prln(" "+ attribute.toString() + "=\"" +
-                                    a.getAttribute(attribute).toString()+"\"");
+      Out.pr("<" + t);
+      printAttributes(a);
+      Out.pr(">");
+    }// End if
 
-        }// while
-        Out.prln(">");
-    }// if
-  }// End if
+    if (HTML.Tag.H1 == t){
+      Out.pr("<" + t);
+      printAttributes(a);
+      Out.pr(">");
+    }// End if
+
   }//handleStartTag
+
+  private void printAttributes(MutableAttributeSet a){
+    if (a == null) return;
+    // Take all the attributes an put them into the feature map
+    if (0 != a.getAttributeCount()){
+      Enumeration enum = a.getAttributeNames();
+      while (enum.hasMoreElements()){
+        Object attribute = enum.nextElement();
+        Out.pr(" "+ attribute.toString() + "=\"" +
+                                  a.getAttribute(attribute).toString()+"\"");
+      }// End while
+    }// End if
+  }// printAttributes();
 
    /** This method is called when the HTML parser encounts the end of a tag
      * that means that the tag is paired by a beginning tag
      */
   public void handleEndTag(HTML.Tag t, int pos){
     currentTag = null;
+
     if (HTML.Tag.A == t)
-      Out.prln("</"+t+">\n");
+      Out.pr("</"+t+">\n");
+    if (HTML.Tag.H1 == t)
+      Out.pr("</"+t+">\n");
+
   }//handleEndTag
 
   /** This method is called when the HTML parser encounts an empty tag
     */
   public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos){
     if (HTML.Tag.A == t){
-      Out.prln("<"+t);
-      // construct a feature map from the attributes list
-      // these are empty elements
-      Map fm = new HashMap();
-      // take all the attributes an put them into the feature map
-      if (0 != a.getAttributeCount ()){
-          Enumeration enum = a.getAttributeNames();
-          while (enum.hasMoreElements ()){
-            Object attribute = enum.nextElement ();
-            fm.put ( attribute.toString(),(a.getAttribute(attribute)).toString());
-            Out.prln(" "+ attribute.toString() + "=\"" +
-                                    a.getAttribute(attribute).toString()+"\"");
-          }//while
-      }
-      Out.prln("/>\n");
+      Out.pr("<"+t);
+      printAttributes(a);
+      Out.pr("/>\n");
+    }// End if
+
+    if (HTML.Tag.H1 == t){
+      Out.pr("<"+t);
+      printAttributes(a);
+      Out.pr("/>\n");
     }// End if
   } // handleSimpleTag
 
-  /** This method is called when the HTML parser encounts text (PCDATA)
-    */
+  /** This method is called when the HTML parser encounts text (PCDATA)*/
   public void handleText(char[] text, int pos){
+
     if(HTML.Tag.A == currentTag){
       //text of tag A
       String tagText = new String(text);
-      Out.prln(tagText);
+      Out.pr(tagText);
     }// End if
+
+    if(HTML.Tag.H1 == currentTag){
+      //text of tag A
+      String tagText = new String(text);
+      Out.pr(tagText);
+    }// End if
+
   }// end handleText();
 
   /**
@@ -128,7 +141,7 @@ public class HtmlLinksExtractor extends ParserCallback {
    * in that folder. It returns a list of strings representing the file
    * names
    */
-  private static List listAllFiles(File aFile){
+  private static List listAllFiles(File aFile, Set foldersToIgnore){
     java.util.List sgmlFileNames = new ArrayList();
     java.util.List foldersToExplore = new ArrayList();
     if (!aFile.isDirectory()){
@@ -136,27 +149,36 @@ public class HtmlLinksExtractor extends ParserCallback {
       sgmlFileNames.add(aFile.getPath());
       return sgmlFileNames;
     }// End if
-    listFilesRec(aFile,sgmlFileNames,foldersToExplore);
+    listFilesRec(aFile,sgmlFileNames,foldersToExplore, foldersToIgnore);
     return sgmlFileNames;
   } // listAllFiles();
 
   /** Helper method for listAllFiles */
-  private static void listFilesRec(File aFile, java.util.List fileNames,
-                                             java.util.List foldersToExplore){
+  private static void listFilesRec(File aFile,
+                                  java.util.List fileNames,
+                                  java.util.List foldersToExplore,
+                                  Set foldersToIgnore){
 
     String[] fileList = aFile.list();
     for (int i=0; i< fileList.length; i++){
       File tmpFile = new File(aFile.getPath()+"\\"+fileList[i]);
-      if (tmpFile.isDirectory())
-        foldersToExplore.add(tmpFile);
-      else
-        fileNames.add(tmpFile.getPath());
+      if (tmpFile.isDirectory()){
+        // If the file is not included
+        if (!foldersToIgnore.contains(fileList[i]))
+          foldersToExplore.add(tmpFile);
+      }else{
+        // only process .html files
+        if(
+          ( fileList[i].toLowerCase().endsWith(".html") ) ||
+          ( fileList[i].toLowerCase().endsWith(".htm") )
+        ) fileNames.add(tmpFile.getPath());
+      }// End if
     }// End for
 
     while(!foldersToExplore.isEmpty()){
       File folder = (File)foldersToExplore.get(0);
       foldersToExplore.remove(0);
-      listFilesRec(folder,fileNames,foldersToExplore);
+      listFilesRec(folder,fileNames,foldersToExplore,foldersToIgnore);
     }//End while
 
   } // listFilesRec();
@@ -167,26 +189,23 @@ public class HtmlLinksExtractor extends ParserCallback {
     // create a new Htmldocument handler
     HtmlLinksExtractor htmlDocHandler = new HtmlLinksExtractor();
 
-    if (args.length != 1){
+    if (args.length == 0){
       Out.prln(
-        "Eg: java HtmlLinksExtractor g:\\tmp\\relative > results.txt"
+        "Eg: java HtmlLinksExtractor g:\\tmp\\relative javadoc img > results.txt"
       );
       return;
     }
     // Create a folder file File
     File htmlFolder = new File(args[0]);
-    List htmlFileNames = listAllFiles(htmlFolder);
+    Set foldersToIgnore = new HashSet();
+    for(int i = 1; i<args.length; i++)
+      foldersToIgnore.add(args[i]);
+
+    List htmlFileNames = listAllFiles(htmlFolder,foldersToIgnore);
     while (!htmlFileNames.isEmpty()){
       try{
         String htmlFileName = (String) htmlFileNames.get(0);
         htmlFileNames.remove(0);
-
-	// only process .html files
-	if(
-	  (! htmlFileName.toLowerCase().endsWith(".html") ) &&
-	  (! htmlFileName.toLowerCase().endsWith(".htm") )
-	)
-	  continue;
 
         Reader reader = new FileReader(htmlFileName);
         // parse the HTML document
