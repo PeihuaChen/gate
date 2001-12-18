ffbaf1a321cfa927c8950e95d9cbbaa9e12adc63
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-12-18 14:56:07 +0000

    [marin] main coref class

---
+++ src/gate/creole/coref/Coreferencer.java
@@ -0,0 +1,25 @@
+/*
+ *  Coreferencer.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 18/Dec/2001
+ *
+ *  $Id$
+ */
+
+package gate.creole.coref;
+
+import gate.creole.*;
+
+
+public class Coreferencer extends AbstractProcessingResource {
+
+  public Coreferencer() {
+  }
+}
