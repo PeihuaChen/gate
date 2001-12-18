2d9a9ce42ad6165bfb142997883e9564e5482e34
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-12-18 14:34:46 +0000

    [marin] beautified

--- src/gate/util/Restriction.java
+++ src/gate/util/Restriction.java
@@ -1,6 +1,21 @@
+/*
+ *  Restriction.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Rosen Marinov, 10/Dec/2001
+ *
+ *  $Id$
+ */
+
 package gate.util;
 
-public class Restriction implements java.io.Serializable{
+public class Restriction {
 
   /* Type of operator for cmarision in query*/
   public static final int OPERATOR_EQUATION = 100;
@@ -9,30 +24,29 @@ public class Restriction implements java.io.Serializable{
   public static final int OPERATOR_EQUATION_OR_BIGGER = 103;
   public static final int OPERATOR_EQUATION_OR_LESS = 104;
 
-  private Object m_value;
-  private String m_key;
-  private int m_operator;
+  private Object value;
+  private String key;
+  private int    operator_;
 
-  public Restriction(String key, Object value, int _operator){
-    m_key = key;
-    m_value = value;
-    m_operator = _operator;
-
+  public Restriction(String key, Object value, int operator_){
+    this.key = key;
+    this.value = value;
+    this.operator_ = operator_;
   }
 
   public Object getValue(){
-    return m_value;
+    return value;
   }
 
   public String getStringValue(){
-    return m_value.toString();
+    return value.toString();
   }
 
   public String getKey(){
-    return m_key;
+    return key;
   }
 
   public int getOperator(){
-    return m_operator;
+    return operator_;
   }
 }
