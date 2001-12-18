dae35ca049128e4c272d3c868d13931cbf7f4e39
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2001-12-18 13:15:03 +0000

    new constants for feature names

--- src/gate/GateConstants.java
+++ src/gate/GateConstants.java
@@ -56,4 +56,13 @@ public interface GateConstants {
   /** The key for the save session on exit value*/
   public static final String SAVE_SESSION_ON_EXIT = "Save_session_on_exit";
 
+  /** The key for the feature keeping the original content of the document */
+  public static final String
+   ORIGINAL_DOCUMENT_CONTENT_FEATURE_NAME = "Original_document_content_on_load";
+
+  /** The key for the feature keeping the repositioning information
+   *  between original and displayed content of the document*/
+  public static final String
+    DOCUMENT_REPOSITIONING_INFO_FEATURE_NAME = "Document_repositioning_info";
+
 } // GateConstants
