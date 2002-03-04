66d6f8eb829f4469bc7d98500b65fa6a1439491f
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-03-04 15:20:30 +0000

    [marin] tools14,jar added to classpath

--- build/deploy/setEnv.bat
+++ build/deploy/setEnv.bat
@@ -3,10 +3,13 @@ REM ##############################################
 REM $Id$
 REM ##############################################
 
-cd %1\bin /d
+set GATE_HOME=%1
+set JAVA_HOME=%2
 
+cd %1\bin /d
 echo set GATE_HOME=%1> tempEnv.txt
 echo set JAVA_HOME=%2>> tempEnv.txt
+echo if not exist %JAVA_HOME%\lib\tools.jar set CLASSPATH="%CLASSPATH%";%GATE_HOME%\bin\tools14.jar
 
 move gate.bat gate1.bat
 copy tempEnv.txt+gate1.bat gate.bat
