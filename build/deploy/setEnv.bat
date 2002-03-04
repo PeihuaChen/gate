e721cfd0fd3f2e02fdd9cc34e2e9538bde8e744a
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-03-04 14:36:54 +0000

    [marin] helper sript that set env variables executed from the ZeroG installer

---
+++ build/deploy/setEnv.bat
@@ -0,0 +1,14 @@
+@echo off
+REM ##############################################
+REM $Id$
+REM ##############################################
+
+cd %1\bin /d
+
+echo set GATE_HOME=%1> tempEnv.txt
+echo set JAVA_HOME=%2>> tempEnv.txt
+
+move gate.bat gate1.bat
+copy tempEnv.txt+gate1.bat gate.bat
+del gate1.bat
+del tempEnv.txt
