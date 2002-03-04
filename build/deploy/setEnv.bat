99f934c93bcf8d3d080e1ee11a13d3db30aa8a14
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-03-04 17:05:36 +0000

    [marin] modified

--- build/deploy/setEnv.bat
+++ build/deploy/setEnv.bat
@@ -9,7 +9,7 @@ set JAVA_HOME=%2
 cd %1\bin /d
 echo set GATE_HOME=%1> tempEnv.txt
 echo set JAVA_HOME=%2>> tempEnv.txt
-echo if not exist %JAVA_HOME%\lib\tools.jar set CLASSPATH="%CLASSPATH%";%GATE_HOME%\bin\tools14.jar
+echo if not exist %JAVA_HOME%\lib\tools.jar set CLASSPATH="%CLASSPATH%";%GATE_HOME%\bin\tools14.jar >> tempEnv.txt
 
 move gate.bat gate1.bat
 copy tempEnv.txt+gate1.bat gate.bat
