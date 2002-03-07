c86998e1bfde1627d7717c16b9dabba0bc9e4f90
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-03-07 17:44:23 +0000

    custom installer code

--- build/deploy/setEnv.bat
+++ build/deploy/setEnv.bat
@@ -3,13 +3,9 @@ REM ##############################################
 REM $Id$
 REM ##############################################
 
-set GATE_HOME=%1
-set JAVA_HOME=%2
-
 cd  /d %1\bin
 echo set GATE_HOME=%1> tempEnv.txt
 echo set JAVA_HOME=%2>> tempEnv.txt
-echo if not exist %JAVA_HOME%\lib\tools.jar set CLASSPATH="%CLASSPATH%";%GATE_HOME%\bin\tools14.jar >> tempEnv.txt
 
 move gate.bat gate1.bat
 copy tempEnv.txt+gate1.bat gate.bat
