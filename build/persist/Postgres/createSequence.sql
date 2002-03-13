4389420ac5ad944e3b0ab6068ef3267faf63e344
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-03-13 12:06:22 +0000

    [marin]

---
+++ build/persist/Postgres/createSequence.sql
@@ -0,0 +1,25 @@
+/*
+ *  DDL script for PostgreSQL 7.2
+ *
+ *  Copyright (c) 1998-2002, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 12/Mar/2002
+ *
+ *
+ *  $Id$
+ *
+ */
+
+CREATE SEQUENCE "SEQ_FT_ID"
+       start 1
+       increment 1
+       maxvalue 2147483647
+       minvalue 1
+       cache 20;
+
+
