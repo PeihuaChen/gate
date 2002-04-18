5f4b49b9698c5e564c693dcd18eb1eab7f5c0fce
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-04-18 12:25:15 +0000

    [angel] Dummy Protege LR interface.

---
+++ src/gate/ProtegeProject.java
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Angel Kirilov 18/04/2002
+ *
+ *  $Id$
+ *
+ */
+package gate;
+
+/** Just a dummy interface for dummy Protege LR. */
+public interface ProtegeProject extends LanguageResource {
+} // interface ProtegeProject extends LanguageResource
