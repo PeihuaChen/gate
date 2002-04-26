5ac3ea4f7b7cc643f03e4dd2a1294467282920f9
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-04-26 08:52:02 +0000

    [rosen] Lucene Search Implementation

--- src/gate/creole/ir/lucene/LuceneSearch.java
+++ src/gate/creole/ir/lucene/LuceneSearch.java
@@ -17,6 +17,13 @@ package gate.creole.ir.lucene;
 import gate.creole.ir.*;
 import java.util.*;
 
+import org.apache.lucene.index.*;
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.document.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.queryParser.*;
+import org.apache.lucene.store.*;
+
 public class LuceneSearch implements Search {
 
   private IndexedCorpus indexedCorpus;
@@ -25,24 +32,54 @@ public class LuceneSearch implements Search {
     this.indexedCorpus = ic;
   }
 
-  public QueryResultList search(String query){
-    //NOT IMPLEMENTED YET
-    return null;
+  public QueryResultList search(String query) throws IndexException, SearchException{
+    return search(query, -1);
   }
 
-  public QueryResultList search(String query, int limit){
-    //NOT IMPLEMENTED YET
-    return null;
+  public QueryResultList search(String query, int limit) throws IndexException, SearchException{
+    return search(query, limit, null);
   }
 
-  public QueryResultList search(String query, List fieldNames){
-    //NOT IMPLEMENTED YET
-    return null;
+  public QueryResultList search(String query, List fieldNames) throws IndexException, SearchException{
+    return search(query, -1, fieldNames);
   }
 
-  public QueryResultList search(String query, int limit, List fieldNames){
-    //NOT IMPLEMENTED YET
-    return null;
+  public QueryResultList search(String query, int limit, List fieldNames) throws IndexException, SearchException{
+    Vector result = new Vector();
+
+    try {
+      IndexSearcher searcher = new IndexSearcher(indexedCorpus.getIndexDefinition().getIndexLocation());
+      Query luceneQuery = QueryParser.parse(query, null, (Analyzer) indexedCorpus.getIndexDefinition().getAnalyzer());
+
+      Hits hits = searcher.search(luceneQuery);
+      int resultlength = hits.length();
+      if (limit>-1) {
+        resultlength = Math.min(limit,resultlength);
+      }
+
+      Vector fieldValues = null;
+      for (int i=0; i<resultlength; i++) {
+
+        if (fieldNames != null){
+          fieldValues = new Vector();
+          for (int j=0; j<fieldNames.size(); j++){
+            fieldValues.add(hits.doc(i).get(fieldNames.get(j).toString()));
+          }
+        }
+
+        result.add(new QueryResult(hits.doc(i).get(LuceneIndexManager.DOCUMENT_ID),hits.score(i),fieldValues));
+      }// for (all search hints)
+
+      searcher.close();
+
+      return new QueryResultList(query, indexedCorpus, result);
+    }
+    catch (java.io.IOException ioe) {
+      throw new IndexException(ioe.getMessage());
+    }
+    catch (org.apache.lucene.queryParser.ParseException pe) {
+      throw new SearchException(pe.getMessage());
+    }
   }
 
 }
