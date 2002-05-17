04e76783ffbed89299d35d9618600f093c8c5ab0
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-05-17 09:39:30 +0000

    [rosen] ?

---
+++ src/gate/creole/ir/DefaultPropertyReader.java
@@ -0,0 +1,26 @@
+/*
+ *  DefaultPropertyReader.java
+ *
+ *  Copyright (c) 1998-2001, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Rosen Marinov, 19/Apr/2002
+ *
+ */
+
+package gate.creole.ir;
+
+import gate.Document;
+
+public class DefaultPropertyReader implements PropertyReader{
+
+  /** @return String value of the requested field. */
+  public String getRpopertyValue(Document doc){
+    return doc.getContent().toString();
+  }
+
+}
