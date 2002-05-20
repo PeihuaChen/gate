0682e6071e7dc2eb96e25f501d8b453e0be72107
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-05-20 16:09:51 +0000

    [marin] implemented

---
+++ src/gate/wordnet/VerbFrameImpl.java
@@ -0,0 +1,34 @@
+/*
+ *  VerbFrameImpl.java
+ *
+ *  Copyright (c) 1998-2002, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 16/May/2002
+ *
+ *  $Id$
+ */
+
+package gate.wordnet;
+
+import junit.framework.*;
+
+public class VerbFrameImpl implements VerbFrame {
+
+  private String frame;
+
+  public VerbFrameImpl(String _frame) {
+
+    Assert.assertNotNull(_frame);
+    this.frame = _frame;
+  }
+
+  public String getFrame() {
+    return this.frame;
+  }
+
+}
---
+++ src/gate/wordnet/VerbImpl.java
@@ -0,0 +1,49 @@
+/*
+ *  VerbImpl.java
+ *
+ *  Copyright (c) 1998-2002, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Marin Dimitrov, 20/May/2002
+ *
+ *  $Id$
+ */
+
+package gate.wordnet;
+
+import java.util.*;
+
+import junit.framework.*;
+
+public class VerbImpl extends WordSenseImpl
+                      implements Verb {
+
+  private ArrayList verbFrames;
+
+  public VerbImpl(Word _word,
+                  Synset _synset,
+                  int _senseNumber,
+                  int _orderInSynset,
+                  boolean _isSemcor,
+                  net.didion.jwnl.data.Verb _jwVerb) {
+
+    super(_word,_synset,_senseNumber,_orderInSynset,_isSemcor);
+
+    Assert.assertNull(_jwVerb);
+
+    String[] jwFrames = _jwVerb.getVerbFrames();
+    this.verbFrames = new ArrayList(jwFrames.length);
+
+    for (int i= 0; i< jwFrames.length; i++) {
+      this.verbFrames.add(new VerbFrameImpl(jwFrames[i]));
+    }
+  }
+
+  public List getVerbFrames() {
+    return this.verbFrames;
+  }
+}
