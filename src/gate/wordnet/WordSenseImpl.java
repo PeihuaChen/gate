5d9a52a0fd43609831d58234f8561f8f2156e4f8
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-05-21 16:49:46 +0000

    [marin] 1. typos fixed 2. exceptions added 3. constructor changed

--- src/gate/wordnet/WordSenseImpl.java
+++ src/gate/wordnet/WordSenseImpl.java
@@ -15,13 +15,17 @@
 
 package gate.wordnet;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 import junit.framework.*;
-import net.didion.jwnl.dictionary.*;
-//import net.didion.jwnl.data.*;
-import net.didion.jwnl.*;
+import net.didion.jwnl.dictionary.Dictionary;
+import net.didion.jwnl.data.POS;
+import net.didion.jwnl.data.Pointer;
+import net.didion.jwnl.data.PointerType;
+import net.didion.jwnl.data.PointerTarget;
+import net.didion.jwnl.data.IndexWord;
+import net.didion.jwnl.JWNLException;
+
 import gate.util.*;
 
 
@@ -33,18 +37,26 @@ public class WordSenseImpl implements WordSense {
   private int orderInSynset;
   private boolean isSemcor;
   private List lexRelations;
+  private Dictionary wnDictionary;
 
   public WordSenseImpl(Word _word,
                       Synset _synset,
                       int _senseNumber,
                       int _orderInSynset,
-                      boolean _isSemcor) {
+                      boolean _isSemcor,
+                      Dictionary _wnDict) {
+
+    //0.
+    Assert.assertNotNull(_word);
+    Assert.assertNotNull(_synset);
+    Assert.assertNotNull(_wnDict);
+
     this.word = _word;
     this.synset = _synset;
     this.senseNumber = _senseNumber;
     this.orderInSynset = _orderInSynset;
     this.isSemcor = _isSemcor;
-
+    this.wnDictionary = _wnDict;
   }
 
   public Word getWord() {
@@ -71,12 +83,92 @@ public class WordSenseImpl implements WordSense {
     return this.isSemcor;
   }
 
-  public List getLexicalRealtions() {
-    throw new MethodNotImplementedException();
+  public List getLexicalRelations() throws WordNetException {
+
+    if (null == this.lexRelations) {
+      _loadLexicalRelations();
+    }
+
+    return this.lexRelations;
   }
 
-  public List getLexicalRealtions(int type) {
-    throw new MethodNotImplementedException();
+  public List getLexicalRelations(int type) throws WordNetException {
+
+    List result = new ArrayList(1);
+
+    if (null == this.lexRelations) {
+      _loadLexicalRelations();
+    }
+
+    Iterator it = this.lexRelations.iterator();
+    while (it.hasNext()) {
+      LexicalRelation lRel = (LexicalRelation)it.next();
+      Assert.assertNotNull(lRel);
+      if (type == lRel.getType()) {
+        result.add(lRel);
+      }
+    }
+
+    return result;
   }
 
+
+  private void _loadLexicalRelations() throws WordNetException{
+
+    POS jwPOS = null;
+    jwPOS = WNHelper.int2POS(this.getPOS());
+
+    try {
+      net.didion.jwnl.data.Synset jwSynset = this.wnDictionary.getSynsetAt(jwPOS,this.synset.getOffset());
+      Assert.assertNotNull(jwSynset);
+
+      Pointer[] jwPointers = null;
+
+      net.didion.jwnl.data.Word[] jwWords = jwSynset.getWords();
+      for (int i=0; i< jwWords.length; i++) {
+        net.didion.jwnl.data.Word currJwWord = jwWords[i];
+        if (currJwWord.getLemma().equalsIgnoreCase(this.getWord().getLemma())) {
+          jwPointers = currJwWord.getPointers();
+          break;
+        }
+      }
+
+      this.lexRelations = new ArrayList(jwPointers.length);
+
+      for (int i= 0; i< jwPointers.length; i++) {
+        Pointer currPointer = jwPointers[i];
+        PointerType currType = currPointer.getType();
+//        PointerTarget ptrSource = currPointer.getSource();
+        PointerTarget ptrTarget = currPointer.getTarget();
+        Assert.assertTrue(ptrTarget instanceof net.didion.jwnl.data.Word);
+
+        net.didion.jwnl.data.Word jwTargetWord = (net.didion.jwnl.data.Word)ptrTarget;
+        net.didion.jwnl.data.Synset jwTargetSynset = jwTargetWord.getSynset();
+        IndexWord jwTargetIndexWord = this.wnDictionary.lookupIndexWord(jwTargetWord.getPOS(),
+                                                                      jwTargetWord.getLemma());
+
+        Synset gateSynset = new SynsetImpl(jwTargetSynset,this.wnDictionary);
+
+        Word gateWord = new WordImpl(jwTargetWord.getLemma(),
+                                      jwTargetIndexWord.getSenseCount(),
+                                      this.wnDictionary);
+
+        WordSense gateTargetWordSense = new WordSenseImpl(gateWord,
+                                                          gateSynset,
+                                                          0,
+                                                          jwTargetWord.getIndex(),
+                                                          false,
+                                                          this.wnDictionary);
+
+        LexicalRelation gateLexRel = new LexicalRelationImpl(WNHelper.PointerType2int(currType),
+                                                            this,
+                                                            gateTargetWordSense);
+        //add to list of sem relations for this synset
+        this.lexRelations.add(gateLexRel);
+      }
+    }
+    catch(JWNLException e) {
+      throw new WordNetException(e);
+    }
+  }
 }
