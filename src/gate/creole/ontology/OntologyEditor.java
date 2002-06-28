6e1ebcfa1b5fb3a322d3733a430aed7af2f91eb8
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-06-28 14:24:15 +0000

    [boby] javadoc comments

--- src/gate/creole/ontology/OntologyEditor.java
+++ src/gate/creole/ontology/OntologyEditor.java
@@ -26,95 +26,102 @@ import java.util.*;
 import javax.swing.*;
 
 
+/**
+ * An interface defining the methods of an Ontology Editor.
+ */
 public interface OntologyEditor extends VisualResource {
 
-/**create a new ontology
+/**Creates a new ontology
  * @param name the name of the ontology
- * @param sourceURI
- * @param theURL
- * @param comment */
+ * @param sourceURI the URI of the ontology
+ * @param theURL the URL of the ontology
+ * @param comment ontology's comment */
 public void createOntology
   (String name, String sourceURI, String theURL, String comment)
   throws ResourceInstantiationException;
 
-/** set ontology to be loaded in the editor
+/**Sets the ontology to be loaded in the editor
  * @param o the ontology to be loaded */
 public void setOntology(Ontology o);
 
-/** get the loaded ontology
+/** Gets the loaded ontology
  * @return the current ontology in the editor*/
 public Ontology getOntology();
 
-/**set the list of ontologies to be loaded in the editor
+/**Sets the list of ontologies to be loaded in the editor
  * @param list the list of ontologies */
 public void setOntologyList(Vector list);
 
-/**get the list of ontologies currently in the editor
+/**Gets the list of ontologies currently in the editor
  * @return the list of ontologies */
 public Vector getOntologyList();
 
-/** visualize the editor */
+/** Visualizes the editor */
 public void visualize();
 
-/**invokes an add sub class dialog in position x,y
+/**Invokes an add sub class dialog in position x,y
  * @param x the x coordinate of the dialog
  * @param y the y coordinate of the dialog*/
 public void addSubClass(int x,int y) ;
 
-/**addSubClass given a ClassNode and the resulting info from the dialog
+/**Adds Sub Class given a ClassNode and the resulting info
+ * from the Add Sub Class dialog.
  * @param root the node which is root to the sub class being added
  * @param className the name from the dialog
  * @param classComment the comment from the dialog */
 public void addSubClass(ClassNode root, String className, String classComment);
 
-/**removes the node/class
+/**Removes the node/class
  * @param node the node to be removed*/
 public void removeClass(ClassNode node);
 
-/**rename a class
+/**Renames a class
  * @param c the class to be renamed
  * @param n the class node associated with the class
  * @param x coords
  * @param y coords */
 public void renameClass(OClass c, ClassNode n, int x, int y);
 
-/**is called when an ontology has been selecte from the ontology list
+/**Selects an ontology.
+ * Is called when an ontology has been selecte
+ * from the ontology list.
  * @param o the selected ontology */
 public void ontologySelected(Ontology o) ;
 
-/**Save a list of ontologies.
+/**Saves a list of ontologies.
  * @param list a list of ontologies to be saved*/
 public void saveOntologies(Vector list);
 
-/**close list of ontologies
+/**Closes list of ontologies
  * @param list a list of ontologies to be saved*/
  public void closeOntologies(Vector list) throws ResourceInstantiationException;
 
-/**Get Modified Ontologies
+/**Gets all modified ontologies.
  * @return list of the modified ontologies */
 public Vector getModifiedOntologies() ;
 
 /*----------ontologies list popup menu item listeners------------
 note: these methods could be invoked from within a listener or explicitly*/
 
-/**save this ontology
- * @param o the ontology to be saved */
+/**Saves this ontology
+ * @param o the ontology to be saved
+ * @throws  */
 public void saveOntology(Ontology o) throws ResourceInstantiationException ;
 
-/** invoke a saveas dialog for this ontology and save it
- *  to the location specified
+/**Invokes a Save As dialog for this ontology and saves it
+ *  to the specified location.
  *  @param o the ontology to be saved
  *  @param x the x coordinate of the save as dialog
  *  @param y the y coordinate of the save as dialog*/
 public void saveAsOntology(Ontology o, int x, int y) throws ResourceInstantiationException;
 
-/**rename an ontology
+/**Renames an ontology
  * @param o the ontology to be renamed
  * @param x the x coordinate of the rename dialog
  * @param y the y coordinate of the rename dialog*/
 public void renameOntology(Ontology o, int x, int y);
 
-/**delete an ontology. invoke AreYouSureDialog if the
+/**Deletes an ontology. Invokes AreYouSureDialog if the
  * ontology has been changed.
  * @param o the ontology to be deleted
  * @param x x coordinate of the option pane to be invoked
@@ -122,31 +129,32 @@ public void renameOntology(Ontology o, int x, int y);
 public void deleteOntology(Ontology o, int x, int y)
   throws ResourceInstantiationException;
 
-/** edit the URI of an ontology
+/** Edits the URI of an ontology.
  * @param o the ontology to be edited
  * @param x  coords of the dialog
  * @param y  coords of the dialog
  */
 public void editURI(Ontology o, int x, int y);
 
-/** edit the URI of an ontology class
+/** Edit the URI of an ontology class
  * @param c class to be edited
  * @param x  coords of the dialog
  * @param y  coords of the dialog */
 public void editClassURI(OClass c, int x, int y);
 
 /**
+ * Gets all URIs that are present at the moment as ontology URIs.
  * @return all the uris that are available in the editor
  */
 public Set getAllURIs() ;
 
-/**retrieve a set of all the URIs in an ontology
+/**Retrieve a set of all the class URIs in an ontology
  * @param o the ontology
  * @return set of all the URIs in the ontology
  */
 public Set getAllURIs(Ontology o) ;
 
-/**close an ontology. invoke AreYouSureDialog if the
+/**Closes an ontology. Invokes AreYouSureDialog if the
  * ontology has been changed.
  * @param o the ontology to be closed
  * @param x x coordinate of the option pane to be invoked
@@ -156,7 +164,7 @@ public void closeOntology(Ontology o, int x, int y)
 
 /*End-------ontologies list popup menu item listeners------------*/
 
-/**Wanna Save Dialog invocation
+/**Wanna Save Dialog invocation.
  * @param o the ontology to be saved or not
  * @param x x coordinate of the option pane to be invoked
  * @param y y coordinate of the option pane to be invoked
@@ -165,12 +173,21 @@ public int AskWannaSave(Ontology o, int x, int y);
 
 /*------------- menu bar methods --------------*/
 
+/**
+ * Acts on choosing Exit from the File menu.
+ */
 public void fileExit();
 
+/**
+ * Acts on choosing Open from the File menu.
+ * @param x the x coordinate of the invocation
+ * @param y the y coordinate of the invocation
+ * @throws ResourceInstantiationException if something goes wrong with the loading.
+ */
 public void fileOpen(int x,int y)throws ResourceInstantiationException;
 
 /**
- * invoke a mutiple selection save dialog with a list of ontologies.
+ * Invoke a mutiple selection save dialog with a list of ontologies to be saved.
  * @param x  coords of the dialog
  * @param y  coords of the dialog
  * @param ontologies the list of ontologies to be optionally saved
@@ -178,14 +195,14 @@ public void fileOpen(int x,int y)throws ResourceInstantiationException;
 public void fileSave(int x, int y,Vector ontologies) ;
 
 /**
- * invoke a mutiple selection close dialog with a list of ontologies.
+ * Invokes a mutiple selection close dialog with a list of ontologies to be closed.
  * @param x  coords of the dialog
  * @param y  coords of the dialog
  * @param ontologies the list of ontologies to be optionally closed
  */
 public void fileClose(int x, int y,Vector ontologies) ;
 
-/**inovke a 'new ontology dialog'
+/**Inovkes a 'new ontology dialog'.
  * @param x  coords of the dialog
  * @param y  coords of the dialog */
 public void fileNew(int x, int y);
