ce02a2840a4f66eedaf2d0544afa7f77ecbd591e
nasso <nasso@aa4978b9-1eff-4426-90b4-889a60d71948>
2002-06-28 14:34:03 +0000

    [boby] javadoc comments

--- src/gate/creole/ontology/Ontology.java
+++ src/gate/creole/ontology/Ontology.java
@@ -21,16 +21,18 @@ import java.util.*;
 import gate.*;
 import gate.creole.ResourceInstantiationException;
 
-/**defines the interface of an ontology*/
+/**Defines the interface of an ontology*/
 public interface Ontology extends LanguageResource{
 
-  /** Get ontology by URL. The ontology will be searched first among the LRs and
+  /** Gets ontology by URL. The ontology will be searched first among the LRs and
    *  afterwards loaded by the URL if not found
    *  @param someUrl the url of the ontology
-   *  @return the retrieved or loaded ontology*/
+   *  @return the retrieved or loaded ontology
+   *  @throws ResourceInstantiationException if something gets wrong with the loading*/
   public Ontology getOntology(URL someUrl) throws ResourceInstantiationException;
 
   /**
+   * Gets the label.
    * @return the label of the ontology
    */
   public String getLabel();
@@ -41,106 +43,116 @@ public interface Ontology extends LanguageResource{
    */
   public void setLabel(String theLabel);
 
-  /** get the url of this ontology
+  /** Gets the url of this ontology
    * @return the url of this ontology */
   public URL getURL();
 
-  /**set the url of this ontology
+  /**Set the url of this ontology
    * @param aUrl the url to be set   */
   public void setURL(URL aUrl);
 
-  /**load this ontology. according to different storages - different implementations
+  /**Loads this ontology. According to different storages - different implementations
    * are expected.
-   * should take care of the modifiedAfterLoading member */
+   * Should take care of the modifiedAfterLoading member */
   public void load() throws ResourceInstantiationException ;
 
-  /**store this ontology. according to different storages - different implementations
+  /**Stores this ontology. According to different storages - different implementations
    * are expected.
-   * should take care of the modifiedAfterLoading member */
+   * Should take care of the modifiedAfterLoading member */
   public void store() throws ResourceInstantiationException;
 
-  /**set the URI of the ontology
+  /**Sets the URI of the ontology
    * @param theURI  the URI to be set */
   public void setSourceURI(String theURI);
 
-  /**@return the URI of this ontology*/
+  /**Gets the source URI.
+   * @return the URI of this ontology*/
   public String getSourceURI();
 
-  /**set version of this ontology
+  /**Sets version to this ontology.
    * @param theVersion the version to be set   */
   public void setVersion(String theVersion);
 
-  /**get the version of this ontology
+  /**Gets the version of this ontology.
    * @return  the version of this ontology*/
   public String getVersion();
 
-  /**get the id of this ontology
+  /**Gets the id of this ontology.
    * @return the id of this ontology  */
   public String getId();
 
-  /**set the id of this ontology
+  /**Sets the id of this ontology.
    * @param theId the id to be set */
   public void setId(String theId);
 
-  /**get the comment of this ontology
+  /**Gets the comment of this ontology.
    * @return the comment of this ontology  */
   public String getComment();
 
-  /**sets the comment of this ontology
+  /**Sets the comment of this ontology.
    * @param theComment the comment to be set  */
   public void setComment(String theComment);
 
-  /**creates a new OClass and returns it
+  /**Creates a new OClass and returns it.
    * @param aName the name of this class
    * @param aComment the comment of this class
    * @return the newly created class  */
   public OClass createClass(String aName, String aComment);
 
-  /**remove a class from this ontology
+  /**Removes a class from this ontology.
    * @param theClass the class to be removed */
   public void removeClass(OClass theClass);
 
-  /**add class to the ontology
+  /**Adds a class to the ontology.
    * @param theClass the class to be added */
   public void addClass(OClass theClass);
 
-  /**retrieve class by its name
+  /**Retrieves a class by its name.
    * @param theName the name of the class
    * @return the class matching the name or null if no matches.
    */
   public OClass getClassByName(String theName);
 
-  /**@param theName name of a class
+  /**Checks if the ontology contains a class with the given name.
+   * @param theName name of a class
    * @return true if the ontology contains a class with the name specified  */
   public boolean containsClassByName(String theName);
 
-  /**retrieve all classes as a set
+  /**Retrieves all classes as a set.
    * @return set of all the classes in this ontology  */
   public Set getClasses();
 
-  /**retireve an iterator over the classes. ordered according to the comparator.
+  /**Retireves an iterator over the classes, ordered according to the comparator.
    * @param comp a comparator defining the order of iterating the classes
    * @return an iterator over the classes
    */
   public Iterator getClasses(Comparator comp);
 
-  /**@return the top classes of this ontology */
+  /**Gets the top classes.
+   * @return set of the top classes of this ontology */
   public Set getTopClasses();
 
-  /** get the taxonomic distance between 2 classes
+  /** Gets the taxonomic distance between 2 classes.
     * @param class1 the first class
     * @param class2 the second class
     * @return the taxonomic distance between the 2 classes
     */
   public int getTaxonomicDistance(OClass class1,OClass class2);
 
+  /**
+   * Checks the equality of two ontologies.
+   * @param o the other ontology
+   * @return true if the ontologies are considered equal, otherwise - false.
+   */
   public boolean equals(Object o);
 
-  /**@param isModified sets this param as a value of
+  /**Sets the modified flag.
+   * @param isModified sets this param as a value of
    * the modified property of the ontology */
   public void setModified(boolean isModified);
 
-  /**@return whether the ontology has been modified after the loading*/
+  /**Checks the modified flag.
+   * @return whether the ontology has been modified after the loading*/
   public boolean isModified();
 
 }//interface Ontology
