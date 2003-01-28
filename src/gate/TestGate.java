d476f79f8aaf0da4ed60ff1536642dab2e950a7c
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2003-01-28 17:40:08 +0000

    *** empty log message ***

--- src/gate/TestGate.java
+++ src/gate/TestGate.java
@@ -209,7 +209,7 @@ public class TestGate {
       // set this true to run all tests; false to run the just one below
       boolean allTests = true;
       if(! allTests){
-        suite.addTest(TestWordNet.suite());
+        suite.addTest(TestPR.suite());
       } else {
         suite.addTest(TestWordNet.suite());
         suite.addTest(TestIndex.suite());
