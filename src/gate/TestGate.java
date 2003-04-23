d3a02b17b42bc89ff052bb79e712b376cf62dc3f
marin <marin@aa4978b9-1eff-4426-90b4-889a60d71948>
2003-04-23 17:41:24 +0000

    [marin] removed TestJavac

--- src/gate/TestGate.java
+++ src/gate/TestGate.java
@@ -229,7 +229,7 @@ public class TestGate {
         suite.addTest(TestCreole.suite());
         suite.addTest(CookBook.suite());
         suite.addTest(TestFiles.suite());
-        suite.addTest(TestJavac.suite());
+  //-      suite.addTest(TestJavac.suite());
         suite.addTest(TestJdk.suite());
         suite.addTest(TestJape.suite());
         suite.addTest(TestTemplate.suite());
