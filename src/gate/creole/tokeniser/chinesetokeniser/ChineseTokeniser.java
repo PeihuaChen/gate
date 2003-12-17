6a05f280dcca5ff9809d3bc604524c68c89d16fa
diana <diana@aa4978b9-1eff-4426-90b4-889a60d71948>
2003-12-17 15:29:14 +0000

    chinesetokeniser resources

---
+++ src/gate/creole/tokeniser/chinesetokeniser/ChineseTokeniser.java
@@ -0,0 +1,333 @@
+package gate.creole.tokeniser.chinesetokeniser;
+
+/*
+ *  ChineseTokeniser.java
+ *
+ * Copyright (c) 2000-2001, The University of Sheffield.
+ *
+ * This file is part of GATE (see http://gate.ac.uk/), and is free
+ * software, licenced under the GNU Library General Public License,
+ * Version 2, June1991.
+ *
+ * A copy of this licence is included in the distribution in the file
+ * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
+ *
+ *  m2na2, 13/10/2003
+ *
+ *  $Id$
+ */
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import gate.creole.AbstractLanguageAnalyser;
+import gate.ProcessingResource;
+import gate.creole.tokeniser.SimpleTokeniser;
+import gate.Resource;
+import gate.creole.ResourceInstantiationException;
+import gate.creole.ExecutionException;
+import gate.util.GateRuntimeException;
+import gate.FeatureMap;
+import gate.Factory;
+import gate.Gate;
+import gate.Document;
+import gate.AnnotationSet;
+import gate.util.OffsetComparator;
+import gate.Annotation;
+import gate.util.InvalidOffsetException;
+
+/**
+ * <p>Title: ChineseTokeniser.java </p>
+ * <p>Description: This class is a wrapper for segmenter.</p>
+ * <p> Tokenises a Chinese document using the Chinesse segmenter</p>
+ * @author Niraj Aswani
+ * @version 1.0
+ */
+public class ChineseTokeniser
+    extends AbstractLanguageAnalyser
+    implements ProcessingResource {
+
+  /** Instance of segmenter */
+  private Segmenter segmenter;
+
+  /** The name of the encoding used */
+  private String encoding;
+
+  /** The name of the sourceFile */
+  private gate.Document sourceDoc;
+
+  /** Temporary document */
+  private gate.Document tempDoc;
+
+  /** Instance of Simple Tokenizer */
+  private SimpleTokeniser tokeniser;
+
+  /** Boolean value which says if tokeniser has to generate the spack tokens */
+  private Boolean generateSpaceTokens;
+
+  /** Rules for the simple tokeniser */
+  private java.net.URL rulesURL;
+
+  private String annotationSetName;
+
+  private int charform;
+
+  /** Default Constructor */
+  public ChineseTokeniser() {
+
+  }
+
+  public Resource init() throws ResourceInstantiationException {
+    fireProgressChanged(0);
+    fireStatusChanged("Loading Data Files...");
+    // check the encoging parameters
+    if (encoding == null) {
+      // setting the default parameter for encoding
+      encoding = "UTF8";
+    }
+    else {
+      if (encoding.equals("BIG5")) {
+        charform = Segmenter.TRAD;
+      }
+      else if (encoding.equals("GBK")) {
+        charform = Segmenter.SIMP;
+      }
+      else if (encoding.equals("UTF8")) {
+        charform = Segmenter.BOTH;
+      }
+      else {
+        // setting the default parameter for encoding
+        encoding = "UTF8";
+        charform = Segmenter.BOTH;
+      }
+    }
+
+    if (rulesURL == null) {
+      throw new ResourceInstantiationException(
+          "No URL provided for the tokeniser rules");
+    }
+    // creating instance of segmenter
+    segmenter = new Segmenter(charform, true);
+    fireProcessFinished();
+
+    // returning the current resource
+    return this;
+  }
+
+  /**
+   * This method gets executed whenever user clicks on the Run button
+   * available in the GATE gui.  It runs the segmenter on the given document
+   * and segments the text by addting spaces or space tokens with 0-length
+   * character (depends on the value of generateSpaceTokens selected by the
+   * user at run time).
+   * @throws ExecutionException
+   */
+  public void execute() throws ExecutionException {
+    // lets start the progress and initialize the progress counter
+    fireProgressChanged(0);
+
+    // If no document provided to process throw an exception
+    if (sourceDoc == null) {
+      throw new GateRuntimeException("No document to process!");
+    }
+
+    // run the segmenter on this text
+    String segmentedData = segmenter.segmentData(
+        sourceDoc.getContent().toString(),
+        encoding);
+
+    // now we need to create a temporary document
+    // and copy all the contents of sourceDoc to this temporary document
+    // so that we'll provide this document to the segmenter
+    try {
+      FeatureMap params = Factory.newFeatureMap();
+      params.put("stringContent", segmentedData);
+      FeatureMap features = Factory.newFeatureMap();
+
+      // we need to hide the creation of new document on the GUI screen
+      Gate.setHiddenAttribute(features, true);
+
+      tempDoc = (Document) Factory.createResource("gate.corpora.DocumentImpl",
+                                                  params, features);
+    }
+    catch (ResourceInstantiationException rie) {
+      throw new ExecutionException("Temporary document cannot be created");
+    }
+
+    // and the get the marks where the spaces in the original document
+    // were added
+    ArrayList marks = segmenter.getMarks();
+
+    // we need to run the Simple Tokenizer on this
+    FeatureMap features = Factory.newFeatureMap();
+    Gate.setHiddenAttribute(features, true);
+
+    // set the parameters
+    FeatureMap params = Factory.newFeatureMap();
+    params.put("rulesURL", rulesURL);
+    params.put("encoding", encoding);
+    params.put("document", tempDoc);
+    params.put("annotationSetName", annotationSetName);
+
+    try {
+      tokeniser = (gate.creole.tokeniser.SimpleTokeniser) Factory.
+          createResource(
+          "gate.creole.tokeniser.SimpleTokeniser", params, features);
+    }
+    catch (ResourceInstantiationException rie) {
+      throw new ExecutionException(
+          "Instance of SimpleTokeniser cannot be created");
+    }
+
+    // so now run the tokeniser
+    tokeniser.execute();
+
+    // so space tokens have been added, now we need to map all the newly added
+    // features from tokeniser to the new document to the original one
+    AnnotationSet anns = tempDoc.getAnnotations();
+    List tokens = new ArrayList(anns.get());
+    Comparator offsetComparator = new OffsetComparator();
+    Collections.sort(tokens, offsetComparator);
+    Iterator tokenIter = tokens.iterator();
+    AnnotationSet original = sourceDoc.getAnnotations();
+
+    // to make the process faster, lets copy all the marks into the long array
+    long[] markValues = new long[marks.size()];
+    for (int i = 0; i < marks.size(); i++) {
+      markValues[i] = ( (Long) marks.get(i)).longValue();
+    }
+    Arrays.sort(markValues);
+
+    // and finally transfer the annotations
+    while (tokenIter.hasNext()) {
+      Annotation currentToken = ( (Annotation) tokenIter.next());
+      long startOffset =
+          currentToken.getStartNode().getOffset().longValue();
+      long endOffset =
+          currentToken.getEndNode().getOffset().longValue();
+
+      // search how many chinese splits are before the current annotation
+      int index = Arrays.binarySearch(markValues, startOffset);
+      if (index >= 0) {
+        // it is a chinese split
+        if (generateSpaceTokens.booleanValue()) {
+          try {
+            FeatureMap newFeatures = Factory.newFeatureMap();
+            newFeatures.put("kind", "ChineseSplit");
+            original.add(new Long(startOffset - index),
+                         new Long(startOffset - index),
+                         SPACE_TOKEN_ANNOTATION_TYPE, newFeatures);
+          }
+          catch (InvalidOffsetException ioe) {
+            throw new ExecutionException("Offset Error");
+          }
+        }
+
+      }
+      else {
+        index = Math.abs(index) - 1;
+
+        // it is not a chinese split but some other token
+        // lets add this annotation to the original document
+        String annotSetName = currentToken.getType();
+        FeatureMap newFeatureMap = currentToken.getFeatures();
+        try {
+          original.add(new Long(startOffset - index),
+                       new Long(endOffset - index), annotSetName,
+                       newFeatureMap);
+        }
+        catch (InvalidOffsetException ioe) {
+          throw new ExecutionException(
+              "Problem with the invalid offset while adding annotations" +
+              "to the original document");
+        }
+      }
+    }
+    // and finally remove the temporary Document
+    Factory.deleteResource(tempDoc);
+
+    // process finished, acknowledge user about this.
+    fireProcessFinished();
+  }
+
+  // getter and setter method
+  /**
+   * Sets the boolean parameter which states if segmenter should produce
+   * the space tokens
+   * @param inputFile
+   */
+  public void setGenerateSpaceTokens(Boolean value) {
+    this.generateSpaceTokens = value;
+  }
+
+  /**
+   * Gets the boolean parameter which states if segmenter should produce
+   * the space tokens
+   * @param inputFile
+   */
+  public Boolean getGenerateSpaceTokens() {
+    return this.generateSpaceTokens;
+  }
+
+  /**
+   * Sets the document to be processed
+   * @param document - document to be processed
+   */
+  public void setSourceDoc(gate.Document inputFile) {
+    this.sourceDoc = inputFile;
+  }
+
+  /**
+   * Returns the document under process
+   */
+  public gate.Document getSourceDoc() {
+    return this.sourceDoc;
+  }
+
+  /**
+   * Sets the rule file to be processed
+   * @param String - rule File name to be processed
+   */
+  public void setEncoding(String encoding) {
+    this.encoding = encoding;
+  }
+
+  /**
+   * Returns the document under process
+   */
+  public String getEncoding() {
+    return this.encoding;
+  }
+
+  /**
+   * URL for the file, which contains rules to be given to the tokeniser
+   * @param rules
+   */
+  public void setRulesURL(java.net.URL rules) {
+    this.rulesURL = rules;
+  }
+
+  /**
+   * Returns the URL of the file, which contains rules for the tokeniser
+   * @return
+   */
+  public java.net.URL getRulesURL() {
+    return rulesURL;
+  }
+
+  /**
+   * AnnotationSet name
+   * @param name Name of the annotation
+   */
+  public void setAnnotationSetName(String name) {
+    this.annotationSetName = name;
+  }
+
+  /**
+   * Returns the provided annotationset name
+   * @return
+   */
+  public String getAnnotationSetName() {
+    return this.annotationSetName;
+  }
+}
---
+++ src/gate/creole/tokeniser/chinesetokeniser/Segmenter.java
@@ -0,0 +1,419 @@
+package gate.creole.tokeniser.chinesetokeniser;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * <p>Title: Segmenter.java</p>
+ * <p>Description: This class segments the Chinese Text by adding extra spaces
+ * </p>
+ * <p>Company: University Of Sheffield</p>
+ * @author     - modified by Niraj Aswani
+ * @version 1.0
+ */
+public class Segmenter {
+  //private Hashtable zhwords;
+  private TreeMap zhwords;
+  private TreeSet csurname, cforeign, cnumbers, cnotname;
+
+  private boolean debug;
+
+  // Char form
+  public final static int TRAD = 0;
+  public final static int SIMP = 1;
+  public final static int BOTH = 2;
+
+  // by niraj
+  private ArrayList marks;
+
+  // Charform is TRAD, SIMP or BOTH
+  public Segmenter(int charform, boolean loadwordfile) {
+    debug = false;
+
+    int count = 0;
+
+    int treelevel;
+
+    csurname = new TreeSet();
+    cforeign = new TreeSet();
+    cnumbers = new TreeSet();
+    cnotname = new TreeSet();
+
+    if (charform == SIMP) {
+      loadset(cnumbers,
+              "gate:/creole/tokeniser/chinesetokeniser/snumbers_u8.txt");
+      loadset(cforeign,
+              "gate:/creole/tokeniser/chinesetokeniser/sforeign_u8.txt");
+      loadset(csurname,
+              "gate:/creole/tokeniser/chinesetokeniser/ssurname_u8.txt");
+      loadset(cnotname,
+              "gate:/creole/tokeniser/chinesetokeniser/snotname_u8.txt");
+    }
+    else if (charform == TRAD) {
+      loadset(cnumbers,
+              "gate:/creole/tokeniser/chinesetokeniser/tnumbers_u8.txt");
+      loadset(cforeign,
+              "gate:/creole/tokeniser/chinesetokeniser/tforeign_u8.txt");
+      loadset(csurname,
+              "gate:/creole/tokeniser/chinesetokeniser/tsurname_u8.txt");
+      loadset(cnotname,
+              "gate:/creole/tokeniser/chinesetokeniser/tnotname_u8.txt");
+    }
+    else { // BOTH
+      loadset(cnumbers,
+              "gate:/creole/tokeniser/chinesetokeniser/snumbers_u8.txt");
+      loadset(cforeign,
+              "gate:/creole/tokeniser/chinesetokeniser/sforeign_u8.txt");
+      loadset(csurname,
+              "gate:/creole/tokeniser/chinesetokeniser/ssurname_u8.txt");
+      loadset(cnotname,
+              "gate:/creole/tokeniser/chinesetokeniser/snotname_u8.txt");
+      loadset(cnumbers,
+              "gate:/creole/tokeniser/chinesetokeniser/tnumbers_u8.txt");
+      loadset(cforeign,
+              "gate:/creole/tokeniser/chinesetokeniser/tforeign_u8.txt");
+      loadset(csurname,
+              "gate:/creole/tokeniser/chinesetokeniser/tsurname_u8.txt");
+      loadset(cnotname,
+              "gate:/creole/tokeniser/chinesetokeniser/tnotname_u8.txt");
+    }
+
+    zhwords = new TreeMap();
+
+    if (!loadwordfile) {
+      return;
+    }
+
+    String newword = null;
+    try {
+      InputStream worddata = null;
+      if (charform == SIMP) {
+        worddata = new java.net.URL(
+            "gate:/creole/tokeniser/chinesetokeniser/simplexu8.txt").openStream();
+      }
+      else if (charform == TRAD) {
+        worddata = new java.net.URL(
+            "gate:/creole/tokeniser/chinesetokeniser/tradlexu8.txt").openStream();
+      }
+      else if (charform == BOTH) {
+        worddata = new java.net.URL(
+            "gate:/creole/tokeniser/chinesetokeniser/bothlexu8.txt").openStream();
+      }
+      BufferedReader in = new BufferedReader(new InputStreamReader(worddata,
+          "UTF8"));
+      while ( (newword = in.readLine()) != null) {
+        if ( (newword.indexOf("#") == -1) && (newword.length() < 5)) {
+
+          zhwords.put(newword.intern(), "1");
+
+          if (newword.length() == 3) {
+            if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+              zhwords.put(newword.substring(0, 2).intern(), "2");
+            }
+          }
+
+          if (newword.length() == 4) {
+            if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+              zhwords.put(newword.substring(0, 2).intern(), "2");
+            }
+            if (zhwords.containsKey(newword.substring(0, 3).intern()) == false) {
+              zhwords.put(newword.substring(0, 3).intern(), "2");
+            }
+
+          }
+
+          //if (count++ % 20000 == 0) { System.err.println(count); }
+        }
+      }
+      in.close();
+
+    }
+    catch (IOException e) {
+      //System.err.println("IOException: "+e);
+    }
+
+  }
+
+  /** Load a set of character data */
+  private void loadset(TreeSet targetset, String sourcefile) {
+    String dataline;
+    try {
+      InputStream setdata = new java.net.URL(sourcefile).openStream();
+      BufferedReader in = new BufferedReader(new InputStreamReader(setdata,
+          "UTF-8"));
+      while ( (dataline = in.readLine()) != null) {
+        if ( (dataline.indexOf("#") > -1) || (dataline.length() == 0)) {
+          continue;
+        }
+        targetset.add(dataline.intern());
+      }
+      in.close();
+    }
+    catch (Exception e) {
+      //System.err.println("Exception loading data file" + sourcefile + " " + e);
+    }
+
+  }
+
+  public boolean isNumber(String testword) {
+    boolean result = true;
+    for (int i = 0; i < testword.length(); i++) {
+      if (cnumbers.contains(testword.substring(i, i + 1).intern()) == false) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  public boolean isAllForeign(String testword) {
+    boolean result = true;
+    for (int i = 0; i < testword.length(); i++) {
+      if (cforeign.contains(testword.substring(i, i + 1).intern()) == false) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  public boolean isNotCJK(String testword) {
+    boolean result = true;
+    for (int i = 0; i < testword.length(); i++) {
+      if (Character.UnicodeBlock.of(testword.charAt(i)) ==
+          Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  public String stemWord(String word) {
+    String[] prefix = new String[] {
+        "\u7b2c", "\u526f", "\u4e0d"};
+    String[] suffix = new String[] {
+        "\u4e86", "\u7684", "\u5730", "\u4e0b", "\u4e0a", "\u4e2d", "\u91cc",
+        "\u5230", "\u5185", "\u5916", "\u4eec"};
+    String[] infix = new String[] {
+        "\u5f97", "\u4e0d"};
+    int i;
+
+    StringBuffer unstemmed = new StringBuffer(word);
+
+    for (i = 0; i < prefix.length; i++) {
+      if (unstemmed.substring(0, 1).equals(prefix[i]) == true &&
+          (zhwords.get(unstemmed.substring(1, unstemmed.length()).intern()) != null ||
+           unstemmed.length() == 2)) {
+        unstemmed.deleteCharAt(0);
+        return unstemmed.toString();
+      }
+    }
+
+    for (i = 0; i < suffix.length; i++) {
+      if (unstemmed.substring(unstemmed.length() - 1, unstemmed.length()).
+          equals(suffix[i]) == true &&
+          (zhwords.get(unstemmed.substring(0, unstemmed.length() - 1).intern()) != null ||
+           unstemmed.length() == 2)) {
+        unstemmed.deleteCharAt(unstemmed.length() - 1);
+        return unstemmed.toString();
+      }
+    }
+
+    for (i = 0; i < infix.length; i++) {
+      if (unstemmed.length() == 3 && unstemmed.substring(1, 2).equals(infix[i]) == true &&
+          zhwords.get(new String(unstemmed.substring(0, 1) +
+                                 unstemmed.substring(2, 3)).intern()) != null) {
+        unstemmed.deleteCharAt(1);
+        return unstemmed.toString();
+      }
+    }
+
+    return unstemmed.toString();
+  }
+
+  // here we will keep track of where we add the spaces in the original
+  // document and we will store all these marks in the array called *marks*
+  public String segmentLine(String cline, String separator) {
+    StringBuffer currentword = new StringBuffer();
+    StringBuffer outline = new StringBuffer();
+    int i, clength;
+    char currentchar;
+    separator = " ";
+
+    clength = cline.length();
+    int[][] offsets = new int[clength][2];
+    marks = new ArrayList(); // addition by Niraj
+
+    for (i = 0; i < clength; i++) {
+      currentchar = cline.charAt(i);
+      if (Character.UnicodeBlock.of(currentchar) ==
+          Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS ||
+          isNumber(cline.substring(i, i + 1)) == true) {
+        // Character in CJK block
+        if (currentword.length() == 0) { // start looking for next word
+          //System.err.println("current word length 0");
+          if (i > 0 && (Character.isWhitespace(cline.charAt(i - 1)) == false)) {
+
+            marks.add(new Long(i + marks.size())); //addition by Niraj
+            outline.append(separator);
+          }
+          currentword.append(currentchar);
+
+        }
+        else {
+          if (zhwords.containsKey(new String(currentword.toString() +
+                                             currentchar).intern()) == true &&
+              ( (String) (zhwords.get(new String(currentword.toString() +
+                                                 currentchar).intern()))).
+              equals("1") == true) {
+            // word is in lexicon
+            currentword.append(currentchar);
+
+          }
+          else if (isAllForeign(currentword.toString()) &&
+                   cforeign.contains(new String(new char[] {currentchar}).
+                                     intern()) &&
+                   i + 2 < clength &&
+                   (zhwords.containsKey(cline.substring(i, i + 2).intern()) == false)) {
+            // Possible a transliteration of a foreign name
+            currentword.append(currentchar);
+
+          }
+          else if (isNumber(currentword.toString()) &&
+                   cnumbers.contains(new String(new char[] {currentchar}).
+                                     intern())
+                   /* && (i + 2 < clength) &&
+               (zhwords.containsKey(cline.substring(i, i+2).intern()) == false) */) {
+            // Put all consecutive number characters together
+            currentword.append(currentchar);
+
+          }
+          else if ( (zhwords.containsKey(new String(currentword.toString() +
+              currentchar).intern())) &&
+                   ( ( (String) (zhwords.get(new String(currentword.toString() +
+              currentchar).intern()))).equals("2") == true) &&
+                   i + 1 < clength &&
+                   (zhwords.containsKey(new String(currentword.toString() +
+              currentchar +
+              cline.charAt(i + 1)).intern()) == true)) {
+
+            // Starts a word in the lexicon
+            currentword.append(currentchar);
+
+          }
+          else { // Start anew
+
+            outline.append(currentword.toString());
+            if (Character.isWhitespace(currentchar) == false) {
+              // addition by Niraj
+              marks.add(new Long(i + marks.size()));
+              // end of addition
+              outline.append(separator);
+            }
+            currentword.setLength(0);
+            currentword.append(currentchar);
+          }
+        }
+
+      }
+      else { // Not chinese character
+        //System.err.println("not cjk");
+        if (currentword.length() > 0) {
+          outline.append(currentword.toString());
+          if (Character.isWhitespace(currentchar) == false) {
+            // addition by Niraj
+            marks.add(new Long(i + marks.size()));
+            // end of addition
+            outline.append(separator);
+          }
+          currentword.setLength(0);
+        }
+        outline.append(currentchar);
+      }
+    }
+
+    outline.append(currentword.toString());
+
+    return outline.toString();
+    //return offsets;
+  }
+
+  public void addword(String newword) {
+    zhwords.put(newword.intern(), "1");
+
+    if (newword.length() == 3) {
+      if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+        zhwords.put(newword.substring(0, 2).intern(), "2");
+      }
+    }
+
+    if (newword.length() == 4) {
+      if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+        zhwords.put(newword.substring(0, 2).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 3).intern()) == false) {
+        zhwords.put(newword.substring(0, 3).intern(), "2");
+      }
+
+    }
+
+    if (newword.length() == 5) {
+      if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+        zhwords.put(newword.substring(0, 2).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 3).intern()) == false) {
+        zhwords.put(newword.substring(0, 3).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 4).intern()) == false) {
+        zhwords.put(newword.substring(0, 4).intern(), "2");
+      }
+    }
+
+    if (newword.length() == 6) {
+      if (zhwords.containsKey(newword.substring(0, 2).intern()) == false) {
+        zhwords.put(newword.substring(0, 2).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 3).intern()) == false) {
+        zhwords.put(newword.substring(0, 3).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 4).intern()) == false) {
+        zhwords.put(newword.substring(0, 4).intern(), "2");
+      }
+      if (zhwords.containsKey(newword.substring(0, 5).intern()) == false) {
+        zhwords.put(newword.substring(0, 5).intern(), "2");
+      }
+    }
+
+  }
+
+  /**
+   * This method returns the marks where the spaces were added by the segmenter
+   */
+  // addition by Niraj
+  public ArrayList getMarks() {
+    return marks;
+  }
+
+  public String segmentData(String fileContents, String encoding) {
+    byte[] gbbytes;
+    String segstring = "";
+    boolean debug = false;
+
+    try {
+      segstring = segmentLine(fileContents, " ");
+      if (debug) {
+        gbbytes = segstring.getBytes(encoding);
+      }
+    }
+    catch (Exception e) {
+      //System.err.println("Exception " + e.toString());
+    }
+
+    return segstring;
+  }
+}
