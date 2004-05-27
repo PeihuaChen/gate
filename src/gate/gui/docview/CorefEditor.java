bdb3acaa2f085326c7bed11bbad7a0e2c3aca476
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2004-05-27 14:46:51 +0000

    Coref editor

---
+++ src/gate/gui/docview/CorefEditor.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 1998-2004, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  CorefEditor.java
+ *
+ *  Valentin Tablan, 27-May-2004
+ *
+ *  $Id$
+ */
+
+package gate.gui.docview;
+
+import java.awt.Component;
+
+/**
+ */
+public class CorefEditor extends AbstractDocumentView{
+  /* (non-Javadoc)
+   * @see gate.gui.docview.AbstractDocumentView#initGUI()
+   */
+  protected void initGUI(){
+    // TODO Auto-generated method stub
+  }
+  /* (non-Javadoc)
+   * @see gate.gui.docview.AbstractDocumentView#registerHooks()
+   */
+  protected void registerHooks(){
+    // TODO Auto-generated method stub
+  }
+  /* (non-Javadoc)
+   * @see gate.gui.docview.AbstractDocumentView#unregisterHooks()
+   */
+  protected void unregisterHooks(){
+    // TODO Auto-generated method stub
+  }
+  /* (non-Javadoc)
+   * @see gate.gui.docview.DocumentView#getGUI()
+   */
+  public Component getGUI(){
+    // TODO Auto-generated method stub
+    return null;
+  }
+  /* (non-Javadoc)
+   * @see gate.gui.docview.DocumentView#getType()
+   */
+  public int getType(){
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
