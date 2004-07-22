a97ab00bb206e488e7be01b13e0a21797b7c5b09
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2004-07-22 11:50:50 +0000

    A bit of guesswork so that we can automatically convert old gate:// URLs (which are now invalid) into new corect URLs. This should keep most old applications working.

--- src/gate/util/protocols/gate/Handler.java
+++ src/gate/util/protocols/gate/Handler.java
@@ -1,9 +1,15 @@
 package gate.util.protocols.gate;
 
+import java.io.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.*;
-
+import java.util.*;
+import java.util.Iterator;
+import java.util.Map;
+import gate.GateConstants;
 import gate.util.Files;
+
 
 /**
  * The handler for the "gate://" URLs.
@@ -16,7 +22,61 @@ public class Handler extends URLStreamHandler {
     URL actualURL = Handler.class.getResource(
                       Files.getResourcePath() + u.getPath()
                     );
+    if(actualURL == null){
+      System.out.println(
+        "WARNING: Starting with the GATE v.3 release the gate:// URLs have " +
+        "been deprecated. All resources used by processing resources are now " +
+        "external to the GATE distribution.\n" +
+        "You should rebuild your application!\n" + 
+        "The system will try to guess the location but " +
+        "there are no guarantees:");
+      //try to fix the problem if caused by the externalisation of GATE 
+      //resources
+      File pluginHome = new File(
+              System.getProperty(GateConstants.GATE_HOME_SYSPROP_KEY), 
+              "plugins");
+      Iterator pathIter = conversionMap.keySet().iterator();
+      while(pathIter.hasNext()){
+        String aPath = (String)pathIter.next();
+        if(u.getPath().startsWith(aPath)){
+          String oldPath = u.getPath();
+          String newPath = oldPath.replaceFirst(aPath, 
+                  (String)conversionMap.get(aPath));
+          actualURL = new File(pluginHome, newPath).toURL();
+          System.out.println(u.toExternalForm() + " ---> " + 
+                  actualURL.toExternalForm() + "\n");
+          return actualURL.openConnection();
+        }
+      }
+//      
+//      Iterator pluginIter = Gate.getCreoleRegister().getDirectories().iterator();
+//      while(pluginIter.hasNext()){
+//        actualURL = new URL((URL)pluginIter.next(), "/resources/" + u.getPath());
+//        try{
+//          URLConnection conn = actualURL.openConnection();
+//          return conn;
+//        }catch(IOException ioe){
+//          //it didn't work this time.
+//          actualURL = null;
+//        }
+//      }
+    }
     if(actualURL == null) throw new FileNotFoundException(u.toExternalForm());
     return actualURL.openConnection();
+  }
+  
+  static Map conversionMap;
+  static{
+    conversionMap = new HashMap();
+    conversionMap.put("/creole/BengaliNE", "ANNIE/resources/BengaliNE");
+    conversionMap.put("/creole/chunker/VP", "ANNIE/resources/VP/");
+    conversionMap.put("/creole/gazeteer", "ANNIE/resources/gazetteer/");
+    conversionMap.put("/creole/heptag", "ANNIE/resources/heptag/");
+    conversionMap.put("/creole/morph", "Tools/resources/morph/");
+    conversionMap.put("/creole/namematcher", "ANNIE/resources/othomatcher/");
+    conversionMap.put("/creole/ontology", "Ontology_Tools/resources/");
+    conversionMap.put("/creole/splitter", "ANNIE/resources/sentenceSplitter/");
+    conversionMap.put("/creole/tokeniser", "ANNIE/resources/tokeniser/");
+    conversionMap.put("/creole/transducer/NE", "ANNIE/resources/NE/");
   }
 }
