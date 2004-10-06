c9bbd2907eb6d3bd9f9d7b92260cbb6d5c944a03
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2004-10-06 12:09:17 +0000

    Cross-platform encoding

--- src/gate/creole/TestPR.java
+++ src/gate/creole/TestPR.java
@@ -55,21 +55,21 @@ public class TestPR extends TestCase
         doc1 = Factory.newDocument(
             new URL(TestDocument.getTestServerName() +
                     "tests/ft-bt-03-aug-2001.html"),
-            "UTF-8"
+            "ISO-8859-1"
             );
 
       if (doc2 == null)
         doc2 = Factory.newDocument(
             new URL(TestDocument.getTestServerName() +
                     "tests/gu-Am-Brit-4-aug-2001.html"),
-            "UTF-8"
+            "ISO-8859-1"
             );
 
       if (doc3 == null)
         doc3 = Factory.newDocument(
             new URL(TestDocument.getTestServerName() +
                     "tests/in-outlook-09-aug-2001.html"),
-            "UTF-8"
+            "ISO-8859-1"
             );
     }catch(Exception e){
       e.printStackTrace();
