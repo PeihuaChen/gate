abd2adff1742ab252e6047b700b2d5df72bc9889
cursu <cursu@aa4978b9-1eff-4426-90b4-889a60d71948>
2005-10-26 14:40:07 +0000

    added a new test for the patch applied on the GateXmlFormat reader and writer (Annotation ID is now saved and restored)

--- src/gate/xml/TestXml.java
+++ src/gate/xml/TestXml.java
@@ -16,13 +16,21 @@
 package gate.xml;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URL;
 import java.util.*;
+import java.text.NumberFormat;
 
 import junit.framework.*;
 
 import gate.*;
+import gate.creole.SerialAnalyserController;
 import gate.util.Files;
+import gate.util.Err;
+import gate.creole.ANNIEConstants;
+
 //import org.w3c.www.mime.*;
 
 
@@ -195,6 +203,228 @@ public class TestXml extends TestCase
 
     gate.corpora.TestDocument.verifyNodeIdConsistency(doc);
   } // testUnpackMarkup()
+
+  /*
+   * This method runs ANNIE with defaults on a document, then saves
+   * it as a GATE XML document and loads it back. All the annotations on the
+   * loaded document should be the same as the original ones.
+   *
+   * It also verifies if the matches feature still holds after an export/import to XML
+   */
+  public void testAnnotationConsistencyForSaveAsXml()throws Exception{
+    // Load a document from the test repository
+    //Document origDoc = gate.Factory.newDocument(Gate.getUrl("tests/xml/gateTestSaveAsXML.xml"));
+    String testDoc = gate.util.Files.getGateResourceAsString("gate.ac.uk/tests/xml/gateTestSaveAsXML.xml");
+    Document origDoc = gate.Factory.newDocument(testDoc);
+
+
+    // Load ANNIE with defaults and run it on the document
+    SerialAnalyserController annie = loadANNIEWithDefaults();
+    assertTrue("ANNIE not loaded!", annie != null);
+    Corpus c = Factory.newCorpus("test");
+    c.add(origDoc);
+    annie.setCorpus(c);
+    annie.execute();
+
+    // SaveAS XML and reload the document into another GATE doc
+    // Export the Gate document called origDoc as XML, into a temp file,
+    // using UTF-8 encoding
+    File xmlFile = Files.writeTempFile(origDoc.toXml(),"UTF-8");
+
+    Document reloadedDoc = gate.Factory.newDocument(xmlFile.toURL());
+    System.out.println("Saving to temp file :" + xmlFile.toURL());
+
+    // Verify if the annotations are identical in the two docs.
+    Map origAnnotMap = buildID2AnnotMap(origDoc);
+    Map reloadedAnnMap = buildID2AnnotMap(reloadedDoc);
+
+    //Verifies if the reloaded annotations are the same as the original ones
+    verifyIDConsistency(origAnnotMap, reloadedAnnMap);
+
+    // Build the original Matches map
+    // ID  -> List of IDs
+    Map origMatchesMap = buildMatchesMap(origDoc);
+    // Verify the consistency of matches
+    // Compare every orig annotation pointed by the MatchesMap with the reloadedAnnot
+    // extracted from the reloadedMAp
+    for(Iterator it = origMatchesMap.keySet().iterator(); it.hasNext();){
+      Integer id = (Integer)it.next();
+      Annotation origAnnot = (Annotation) origAnnotMap.get(id);
+      assertTrue("Couldn't find an original annot with ID=" + id, origAnnot != null);
+      Annotation reloadedAnnot = (Annotation) reloadedAnnMap.get(id);
+      assertTrue("Couldn't find a reloaded annot with ID=" + id, reloadedAnnot != null);
+      compareAnnot(origAnnot,reloadedAnnot);
+      // Iterate through the matches list and repeat the comparison
+      List matchesList = (List) origMatchesMap.get(id);
+      for (Iterator itList = matchesList.iterator(); itList.hasNext();){
+        Integer matchId = (Integer) itList.next();
+        Annotation origA = (Annotation) origAnnotMap.get(matchId);
+        assertTrue("Couldn't find an original annot with ID=" + matchId, origA != null);
+        Annotation reloadedA = (Annotation) reloadedAnnMap.get(matchId);
+        assertTrue("Couldn't find a reloaded annot with ID=" + matchId, reloadedA != null);
+        compareAnnot(origA, reloadedA);
+      }// End for
+    }// End for
+
+  }// End testAnnotationIDConsistencyForSaveAsXml
+
+  /**
+   * Builds a Map based on the matches feature of some annotations. The goal is to
+   * use this map to validate the annotations from the reloaded document.
+   * In case no Annot has the matches feat, will return an Empty MAP
+   * @param doc The document of which annotations will be used to construct the map
+   * @return A Map from Annot ID -> Lists of Annot IDs
+   */
+  private Map buildMatchesMap(Document doc){
+    Map matchesMap = new HashMap();
+    // Scan the default annotation set
+    AnnotationSet annotSet = doc.getAnnotations();
+
+    helperBuildMatchesMap(annotSet, matchesMap);
+    // Scan all named annotation sets
+    if (doc.getNamedAnnotationSets() != null){
+      for ( Iterator namedAnnotSetsIter = doc.getNamedAnnotationSets().values().iterator();
+                                                                namedAnnotSetsIter.hasNext(); ){
+        helperBuildMatchesMap((gate.AnnotationSet) namedAnnotSetsIter.next(), matchesMap);
+      }// End while
+    }// End if
+    return matchesMap;
+  }// End of buildMatchesMap()
+
+  /**
+   * This is a helper metod. It scans an annotation set and adds the ID of the annotations
+   * which have the matches feature to the map.
+   * @param sourceAnnotSet  The annotation set investigated
+   * @param aMap
+   */
+  private void helperBuildMatchesMap(AnnotationSet sourceAnnotSet, Map aMap ){
+
+    for (Iterator it = sourceAnnotSet.iterator(); it.hasNext();){
+      Annotation a = (Annotation) it.next();
+      FeatureMap aFeatMap = a.getFeatures();
+      // Skip those annotations who don't have features
+      if (aFeatMap == null) continue;
+      // Extract the matches feat
+      List matchesVal = (List) aFeatMap.get("matches");
+      if (matchesVal == null) continue;
+      Integer id = a.getId();
+      aMap.put(id,matchesVal);
+    }//End for
+
+  }// End of helperBuildMatchesMap()
+
+
+  /**
+   * Verifies if the two maps hold annotations with the same ID. The only thing not checked
+   * are the features, as some of them could be lost in the serialization/deserialization process
+   * @param origAnnotMap A map by ID, containing the original annotations
+   * @param reloadedAnnMap A map by ID, containing the recreated annotations
+   */
+  private void verifyIDConsistency(Map origAnnotMap, Map reloadedAnnMap) {
+    assertEquals("Found a different number of annot in both documents.",
+            origAnnotMap.keySet().size(), reloadedAnnMap.keySet().size());
+
+//    List orig = new ArrayList(origAnnotMap.keySet());
+//    Collections.sort(orig);
+//    System.out.println("ORIG SET =" + orig);
+//
+//    List rel = new ArrayList(reloadedAnnMap.keySet());
+//    Collections.sort(rel);
+//    System.out.println("REL  SET =" + rel);
+//
+
+    for (Iterator it = origAnnotMap.keySet().iterator(); it.hasNext();){
+      Integer id = (Integer) it.next();
+      Annotation origAnn = (Annotation) origAnnotMap.get(id);
+      Annotation reloadedAnnot = (Annotation) reloadedAnnMap.get(id);
+
+      assertTrue("Annotation with ID="+ id +" was not found in the reloaded document.", reloadedAnnot != null);
+      compareAnnot(origAnn, reloadedAnnot);
+
+    }// End for
+  }// End of verifyIDConsistency()
+
+  /**
+   * Thes if two annotatiosn are the same, except their features.
+   * @param origAnn
+   * @param reloadedAnnot
+   */
+  private void compareAnnot(Annotation origAnn, Annotation reloadedAnnot) {
+    assertTrue("Found original and reloaded annot without the same ID!",
+            origAnn.getId().equals(reloadedAnnot.getId()));
+    assertTrue("Found original and reloaded annot without the same TYPE!\n"+
+               "Original was ["+origAnn.getType()+"] and reloaded was ["+reloadedAnnot.getType()+"].",
+            origAnn.getType().equals(reloadedAnnot.getType()));
+    assertTrue("Found original and reloaded annot without the same START offset!",
+            origAnn.getStartNode().getOffset().equals(reloadedAnnot.getStartNode().getOffset()));
+    assertTrue("Found original and reloaded annot without the same END offset!",
+            origAnn.getEndNode().getOffset().equals(reloadedAnnot.getEndNode().getOffset()));
+  }// End of compareAnnot()
+
+
+  private Map addAnnotSet2Map(AnnotationSet annotSet, Map id2AnnMap){
+    for (Iterator it = annotSet.iterator(); it.hasNext();){
+      Annotation a = (Annotation) it.next();
+      Integer id = a.getId();
+
+      assertTrue("Found two annotations(one with type = " + a.getType() +
+              ")with the same ID=" + id, !id2AnnMap.keySet().contains(id));
+
+      id2AnnMap.put(id, a);
+    }// End for
+    return id2AnnMap;
+  }
+
+  /**
+   * Scans a target Doc for all Annotations and builds a map (from anot ID to annot) in the process
+   * I also checks to see if there are two annotations with the same ID.
+   * @param aDoc The GATE doc to be scaned
+   * @return a Map ID2Annot
+   */
+  private Map buildID2AnnotMap(Document aDoc){
+    Map id2AnnMap = new HashMap();
+    // Scan the default annotation set
+    AnnotationSet annotSet = aDoc.getAnnotations();
+    addAnnotSet2Map(annotSet, id2AnnMap);
+    // Scan all named annotation sets
+    if (aDoc.getNamedAnnotationSets() != null){
+      for ( Iterator namedAnnotSetsIter = aDoc.getNamedAnnotationSets().values().iterator();
+                                                                namedAnnotSetsIter.hasNext(); ){
+
+        addAnnotSet2Map((gate.AnnotationSet) namedAnnotSetsIter.next(), id2AnnMap);
+      }// End while
+    }// End if
+    return id2AnnMap;
+  }// End of buildID2AnnotMap()
+
+  /**
+   * Load ANNIE with defaults
+   * @return
+   */
+  private SerialAnalyserController loadANNIEWithDefaults(){
+    FeatureMap params = Factory.newFeatureMap();
+    SerialAnalyserController sac = null;
+    try{
+      // Create a serial analyser
+      sac = (SerialAnalyserController)
+          Factory.createResource("gate.creole.SerialAnalyserController",
+                                 Factory.newFeatureMap(),
+                                 Factory.newFeatureMap(),
+                                 "ANNIE_" + Gate.genSym());
+      // Load each PR as defined in gate.creole.ANNIEConstants.PR_NAMES
+      for(int i = 0; i < ANNIEConstants.PR_NAMES.length; i++){
+      ProcessingResource pr = (ProcessingResource)
+          Factory.createResource(ANNIEConstants.PR_NAMES[i], params);
+        // Add the PR to the sac
+        sac.add(pr);
+      }// End for
+
+    }catch(gate.creole.ResourceInstantiationException ex){
+      ex.printStackTrace(Err.getPrintWriter());
+    }
+    return sac;
+  }// End of LoadANNIEWithDefaults()
+
 
   /** Test suite routine for the test runner */
   public static Test suite() {
