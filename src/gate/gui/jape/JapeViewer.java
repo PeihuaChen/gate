42de5ebf9b6bb07c7bfd4aac025c9822049869b9
nirajaswani <nirajaswani@aa4978b9-1eff-4426-90b4-889a60d71948>
2006-06-18 09:33:50 +0000

    Jape Viewer refreshes itself when the PR is reinitialised

--- src/gate/gui/jape/JapeViewer.java
+++ src/gate/gui/jape/JapeViewer.java
@@ -7,92 +7,111 @@ import java.awt.*;
 
 import gate.*;
 import gate.creole.*;
+import gate.event.ProgressListener;
+import gate.util.GateRuntimeException;
 
 /**
- * <p>Title: Gate2</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2000</p>
- * <p>Company: University Of Sheffield</p>
- * @author not attributable
- * @version 1.0
+ * @author niraj
+ * @version 1.1
  */
 
+public class JapeViewer extends AbstractVisualResource implements
+		ANNIEConstants, ProgressListener {
 
-public class JapeViewer extends AbstractVisualResource implements ANNIEConstants {
+	public JapeViewer() {
+	}
 
-  public JapeViewer() {
-  }
+	// GUI components
+	/** The text display. */
+	protected JTextArea textArea;
 
-  //GUI components
-  /** The text display.*/
-  protected JTextArea textArea;
+	/** Scroller used for the text diaplay */
+	protected JScrollPane textScroll;
 
-  /** Scroller used for the text diaplay*/
-  protected JScrollPane textScroll;
+	/** The toolbar displayed on the top part of the component */
+	protected JToolBar toolbar;
 
-  /** The toolbar displayed on the top part of the component */
-  protected JToolBar toolbar;
+	/** Should this component bahave as an editor as well as an viewer */
+	private boolean editable = false;
 
-  /**Should this component bahave as an editor as well as an viewer*/
-  private boolean editable = false;
+	/** A Button for saving the contents in a Jape file */
+	private JButton saveButton;
 
-  /** A Button for saving the contents in a Jape file */
-  private JButton saveButton;
+	/** A Button for reverting contents */
+	private JButton revertButton;
 
-  /** A Button for reverting contents */
-  private JButton revertButton;
+	/** A field that holds the jape file name */
+	private URL japeFileURL;
 
-  /** A field that holds the jape file name */
-  private URL japeFileURL;
+	/** a field that holds the jape file contents */
+	private String japeFileContents;
 
-  /** a field that holds the jape file contents */
-  private String japeFileContents;
+	/** Transducer */
+	private Transducer transducer;
 
-  /** Transducer */
-  private Transducer transducer;
+	/** An Init method */
+	public Resource init() {
+		initGuiComponents();
+		japeFileContents = new String();
+		return this;
+	}
 
-  /** An Init method */
-  public Resource init() {
-    initGuiComponents();
-    japeFileContents = new String();
-    return this;
-  }
+	private void initGuiComponents() {
+		setLayout(new BorderLayout());
+		textArea = new JTextArea();
+		textArea.setEditable(editable);
+		textScroll = new JScrollPane(textArea,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		add(textScroll, BorderLayout.CENTER);
+	}
 
-  private void initGuiComponents() {
-    setLayout(new BorderLayout());
-    textArea = new JTextArea();
-    textArea.setEditable(editable);
-    textScroll = new JScrollPane(textArea,
-                                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    add(textScroll, BorderLayout.CENTER);
-  }
-
-  public void setTarget(Object target) {
-    if(!(target instanceof  Transducer)) {
-      throw new IllegalArgumentException(
-        "The GATE jape editor can only be used with a GATE jape transducer!\n" +
-        target.getClass().toString() + " is not a GATE Jape Transducer!");
-    }
-
-    this.transducer = (Transducer) target;
-    //Transducer inst = (Transducer)((Gate.getCreoleRegister().getPrInstances("gate.creole.Transducer")).get(0));
-    japeFileURL = transducer.getGrammarURL();
-    // reading japeFile
-    try {
-		if(japeFileURL != null) {
-			BufferedReader br = new BufferedReader(new InputStreamReader(japeFileURL.openStream()));
-			String content = br.readLine();
-			while(content != null) {
-				japeFileContents += content + "\n";
-				content = br.readLine();
-			}
-			textArea.setText(japeFileContents);
-			br.close();
-		} else {
-			textArea.setText("The JAPE Transducer Object was loaded from a serialised tranducer and therefore cannot show any text!");
+	public void setTarget(Object target) {
+		if (!(target instanceof Transducer)) {
+			throw new IllegalArgumentException(
+					"The GATE jape editor can only be used with a GATE jape transducer!\n"
+							+ target.getClass().toString()
+							+ " is not a GATE Jape Transducer!");
 		}
-    } catch (IOException ioe) {
-    }
-  }
+
+		this.transducer = (Transducer) target;
+		// Transducer inst =
+		// (Transducer)((Gate.getCreoleRegister().getPrInstances("gate.creole.Transducer")).get(0));
+		japeFileURL = transducer.getGrammarURL();
+		// reading japeFile
+		readJAPEFileContents();
+		((Transducer)target).addProgressListener(this);
+	}
+
+	private void readJAPEFileContents() {
+		try {
+			if (japeFileURL != null) {
+				BufferedReader br = new BufferedReader(new FileReader(new File(japeFileURL.getFile())));
+				String content = br.readLine();
+				japeFileContents = "";
+				while (content != null) {
+					japeFileContents += content + "\n";
+					content = br.readLine();
+				}
+				textArea.setEditable(true);
+				textArea.setText(japeFileContents);
+				textArea.updateUI();
+				textArea.setEditable(false);
+				br.close();
+			} else {
+				textArea
+						.setText("The JAPE Transducer Object was loaded from a serialised tranducer and therefore cannot show any text!");
+			}
+		} catch (IOException ioe) {
+			throw new GateRuntimeException(ioe);
+		}
+	}
+
+	public void processFinished() {
+		readJAPEFileContents();
+	}
+
+	public void progressChanged(int progress) {
+
+	}
 }
