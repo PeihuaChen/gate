ecf450873868813933158e9ded522112803135ca
julien_nioche <julien_nioche@aa4978b9-1eff-4426-90b4-889a60d71948>
2006-07-07 11:57:44 +0000

    Refactoring : moved the KEA and ML sources into their respective plugins. The jar files required are deleted from the main GATE lib and copied into the lib directories of the plugins. The motivation for this change is to limit the version conflict for the jars and have a cleaner separation between core code and plugins. The new learning API (coming with the next major release) will replace the existing one and will be available as a plugin.

---
+++ plugins/Machine_Learning/src/ml/maxent/TestMaxentWrapper.java
@@ -0,0 +1,144 @@
+/*
+ *  TestMaxentWrapper.java
+ *
+ *  Copyright (c) 1998-2005, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Mike Dowman, 1/4/2004
+ *
+ *  $Id: TestMaxentWrapper.java 7455 2006-06-19 11:18:44 +0000 (Mon, 19 Jun 2006) ian_roberts $
+ */
+
+package gate.creole.ml.maxent;
+
+import junit.framework.*;
+import gate.*;
+import gate.corpora.*;
+import java.io.File;
+import java.net.*;
+import gate.gui.MainFrame;
+import gate.util.Files;
+import gate.util.GateRuntimeException;
+
+public class TestMaxentWrapper extends TestCase {
+
+  private static final boolean DEBUG=false;
+
+  public TestMaxentWrapper(String name) {
+    super(name);
+  }
+
+  /** Fixture set up - does nothing */
+  public void setUp() throws Exception {
+    //make sure the right plugin is loaded
+    File pluginsHome = new File(System.getProperty(
+            GateConstants.GATE_HOME_PROPERTY_NAME), 
+            "plugins");
+    try{
+      Gate.getCreoleRegister().registerDirectories(
+              new File(pluginsHome, "Machine_Learning").toURL());
+    }catch(Exception e){
+      throw new GateRuntimeException(e);
+    }
+    
+  }
+
+  /** Fixture tear down - does nothing */
+  public void tearDown() throws Exception {
+  } // tearDown
+
+  /** Tests the MAXENT machine learning wrapper, by training it to identify
+   * lookup annotations based on the precence of lookup annotations.
+   */
+  public void testMaxentWrapper() throws Exception {
+    // Store the original standard output stream, so we can restore it later.
+    java.io.PrintStream normalOutputStream=System.out;
+
+    // Display the gui for debugging purposes.
+         if (DEBUG) {
+      MainFrame mainFrame = new MainFrame();
+      mainFrame.setVisible(true);
+    } else {
+      // We don't want the output displayed unless we are debugging, so set the
+      // standard output stream to a new one that never outputs anything.
+      System.setOut(new java.io.PrintStream(
+          new java.io.OutputStream() {
+        public void write(int b) { }
+        public void write(byte[] b, int off, int len) { }
+      }));
+    }
+
+    //get a document - take it from the gate server.
+    // tests/doc0.html is a simple html document.
+    Document doc = Factory.newDocument(
+      new URL(TestDocument.getTestServerName() + "tests/doc0.html")
+    );
+
+    // Get a tokeniser - just use all the default settings.
+    gate.creole.tokeniser.DefaultTokeniser tokeniser=
+        (gate.creole.tokeniser.DefaultTokeniser) Factory.createResource(
+        "gate.creole.tokeniser.DefaultTokeniser");
+
+    // Get a default gazetteer, again just use all the default settings
+    gate.creole.gazetteer.Gazetteer gazetteerInst =
+        (gate.creole.gazetteer.DefaultGazetteer) Factory.createResource(
+        "gate.creole.gazetteer.DefaultGazetteer");
+
+    // Create the Maxent ML Processing resource.
+    // First set up the parameters
+    FeatureMap maxentParameters = Factory.newFeatureMap();
+    maxentParameters.put("configFileURL", Files.getGateResource( 
+                                 "/gate.ac.uk/tests/TestMaxentConfigFile.xml"));
+    // Then actually make the PR
+    gate.creole.ml.MachineLearningPR maxentPR =
+        (gate.creole.ml.MachineLearningPR)
+        Factory.createResource("gate.creole.ml.MachineLearningPR",
+                               maxentParameters);
+
+    // runtime stuff - set the document to be used with the gazetteer,the
+    // tokeniser and the ML PR to doc, and run each of them in turn.
+    tokeniser.setDocument(doc);
+    tokeniser.execute();
+    gazetteerInst.setDocument(doc);
+    gazetteerInst.execute();
+    maxentPR.setDocument(doc);
+    maxentPR.execute();
+
+    // Now run the trained maxent model.
+    maxentPR.setTraining(new Boolean(false));
+    maxentPR.execute();
+
+    // Now clean up so we don't get a memory leak.
+    Factory.deleteResource(doc);
+    Factory.deleteResource(tokeniser);
+    Factory.deleteResource(maxentPR);
+    Factory.deleteResource(gazetteerInst);
+
+    // Restore the standard output stream.
+    System.setOut(normalOutputStream);
+  } // TestMaxentWrapper
+
+  /** Test suite routine for the test runner */
+  public static Test suite() {
+    return new TestSuite(TestMaxentWrapper.class);
+  } // suite
+
+  // The main class allows this class to be tested on its own, without the
+  // need to call it from another class.
+  public static void main(String[] args) {
+    try{
+      Gate.init();
+      TestMaxentWrapper testMax = new TestMaxentWrapper("");
+      testMax.setUp();
+      testMax.testMaxentWrapper();
+      testMax.tearDown();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  } // main
+
+} // TestFlexibleGazetteer
