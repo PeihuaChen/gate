264ee8acc207c1d29d3e5d46d56c5531420a9c67
nirajaswani <nirajaswani@aa4978b9-1eff-4426-90b4-889a60d71948>
2007-03-12 14:34:22 +0000

    checking in new ontology API and removed the previous ontology API

---
+++ src/gate/creole/ontology/Literal.java
@@ -0,0 +1,102 @@
+/*
+ *  Literal.java
+ *
+ *  Niraj Aswani, 09/March/07
+ *
+ *  $Id: Literal.html,v 1.0 2007/03/09 16:13:01 niraj Exp $
+ */
+package gate.creole.ontology;
+
+/**
+ * Literal represents a single value or a value with language used for
+ * annotation properties, or a value with datatype used for the datatype
+ * properties.
+ * 
+ * @author niraj
+ */
+public class Literal {
+  /**
+   * The actual value of the literal
+   */
+  private String value;
+
+  /**
+   * Specified language for the literal.
+   */
+  private String language;
+
+  /**
+   * Assigned Datatype to this instance of literal
+   */
+  private DataType dataType;
+
+  /**
+   * Constructor
+   * 
+   * @param value
+   */
+  public Literal(String value) {
+    this.value = value;
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param value
+   * @param language
+   */
+  public Literal(String value, String language) {
+    this.value = value;
+    this.language = language;
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param value
+   * @param dataType
+   * @throws InvalidValueException
+   */
+  public Literal(String value, DataType dataType) throws InvalidValueException {
+    this.value = value;
+    this.dataType = dataType;
+    // lets check if the provided value is valid for the supplied
+    // dataType
+    if(!dataType.isValidValue(this.value)) {
+      throw new InvalidValueException("The value :\"" + this.value
+              + "\" is not compatible with the dataType \""
+              + dataType.getXmlSchemaURI() + "\"");
+    }
+  }
+
+  /**
+   * Gets the assigned datatype. This may return null if user did not
+   * use the Literal(String, Datatype) constructor to instantiate the
+   * instance.
+   * 
+   * @return
+   */
+  public DataType getDataType() {
+    return dataType;
+  }
+
+  /**
+   * Returns the value associated with this instance of literal.
+   * 
+   * @return
+   */
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * Returns the language associated with this literal. This may return
+   * null if use did not use the Literal(String, String) constructor to
+   * instantiate the instance.
+   * 
+   * @return
+   */
+  public String getLanguage() {
+    return language;
+  }
+}
--- src/gate/creole/ontology/OClass.java
+++ src/gate/creole/ontology/OClass.java
@@ -1,39 +1,127 @@
 /*
- * OClass.java
+ *  OClass.java
  *
- * Copyright (c) 2002, The University of Sheffield.
+ *  Niraj Aswani, 09/March/07
  *
- * This file is part of GATE (see http://gate.ac.uk/), and is free
- * software, licenced under the GNU Library General Public License,
- * Version 2, June1991.
- *
- * A copy of this licence is included in the distribution in the file
- * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
- *
- * Kalina Bontcheva 11/2003
- *
- *  $Id$*
+ *  $Id$
  */
 package gate.creole.ontology;
 
+import java.util.ArrayList;
 import java.util.Set;
 
-public interface OClass extends TClass {
-  /** Indicates that these are disjoint classes */
-  public void setDisjointWith(OClass theClass);
-
-  /** Indicates that these classes are the same */
-  public void setSameClassAs(OClass theClass);
+/**
+ * Each OClass (Ontology Class) represents a concept/class in ontology.
+ * It provides various methods (including and not limited) to iterate
+ * through its super and sub classes in the taxonomy hierarchy.
+ * 
+ * @author niraj
+ * 
+ */
+public interface OClass extends OResource, OConstants {
+  /**
+   * Adds a sub class to this class.
+   * 
+   * @param subClass the subClass to be added.
+   */
+  public void addSubClass(OClass subClass);
 
   /**
-   * Returns a set of all classes that are disjoint with ours. Null if no such
-   * classes.
+   * Adds a super class to this class.
+   * 
+   * @param superClass the super class to be added
    */
-  public Set getDisjointClasses();
+  public void addSuperClass(OClass superClass);
 
   /**
-   * Returns a set of all classes that are the same as ours. Null if no such
-   * classes.
+   * Removes a sub class.
+   * 
+   * @param subClass the sub class to be removed
    */
-  public Set getSameClasses();
+  public void removeSubClass(OClass subClass);
+
+  /**
+   * Removes a super class.
+   * 
+   * @param superClass the super class to be removed
+   */
+  public void removeSuperClass(OClass superClass);
+
+  /**
+   * Gets the subclasses according to the desired closure.
+   * 
+   * @param closure either DIRECT_CLOSURE or TRASITIVE_CLOSURE
+   * @return the set of subclasses
+   */
+  public Set<OClass> getSubClasses(byte closure);
+
+  /**
+   * Gets the super classes according to the desired closure.
+   * 
+   * @param closure either DIRECT_CLOSURE or TRASITIVE_CLOSURE
+   * @return the set of super classes
+   */
+  public Set<OClass> getSuperClasses(byte closure);
+
+  /**
+   * Checks whether the class is a super class of the given class.
+   * 
+   * @param aClass
+   * @param closure either OntologyConstants.DIRECT_CLOSURE or
+   *          OntologyConstants.TRANSITIVE_CLOSURE
+   * @return true, if the class is a super class of the given class,
+   *         otherwise - false.
+   */
+  public boolean isSuperClassOf(OClass aClass, byte closure);
+
+  /**
+   * Checks whether the class is a sub class of the given class.
+   * 
+   * @param aClass
+   * @param closure either OntologyConstants.DIRECT_CLOSURE or
+   *          OntologyConstants.TRANSITIVE_CLOSURE
+   * @return true, if the class is a sub class of the given class,
+   *         otherwise - false.
+   */
+  public boolean isSubClassOf(OClass aClass, byte closure);
+
+  /**
+   * Checks whether this class is a top.
+   * 
+   * @return true if this is a top class, otherwise - false.
+   */
+  public boolean isTopClass();
+
+  /** Indicates that these classes are the equivalent */
+  public void setEquivalentClassAs(OClass theClass);
+
+  /**
+   * Returns a set of all classes that are equivalent as this one. Null
+   * if no such classes.
+   */
+  public Set<OClass> getEquivalentClasses();
+
+  /**
+   * Checks whether the class is equivalent as the given class.
+   * 
+   * @param aClass
+   * @return true, if the class is equivalent as the aClass, otherwise -
+   *         false.
+   */
+  public boolean isEquivalentClassAs(OClass aClass);
+
+  /**
+   * Gets the super classes, and returns them in an array list where on
+   * each index there is a collection of the super classes at distance -
+   * the index.
+   */
+  public ArrayList<Set<OClass>> getSuperClassesVSDistance();
+
+  /**
+   * Gets the sub classes, and returns them in an array list where on
+   * each index there is a collection of the sub classes at distance -
+   * the index.
+   */
+  public ArrayList<Set<OClass>> getSubClassesVsDistance();
+
 }
