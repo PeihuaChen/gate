9a45d7d04a5da18d4f243b96eead7ddfaaa43b0f
yaoyongli <yaoyongli@aa4978b9-1eff-4426-90b4-889a60d71948>
2007-03-26 14:56:33 +0000

    the source file with complete javadoc.

--- plugins/learning/src/gate/learningLightWeight/ArgOfRelation.java
+++ plugins/learning/src/gate/learningLightWeight/ArgOfRelation.java
@@ -1,17 +1,24 @@
+/*
+ *  ArgOfRelation.java
+ * 
+ *  Yaoyong Li 22/03/2007
+ *
+ *  $Id: ArgOfRelation.java, v 1.0 2007-03-22 12:58:16 +0000 yaoyong $
+ */
 package gate.learningLightWeight;
-
+/**
+ * Store the identification information and the related features for the
+ * annotation corresponding to one argument of a relation
+ */
 public class ArgOfRelation {
-  /** The annotation type that one arg. of relation refers to. */
+  /** Annotation type one argument of relation refers to. */
   String type;
-
-  /** The annotation feature that one arg. of relation refers to. */
+  /** Annotation feature one argument of relation refers to. */
   String feat;
-
+  /** Attribute features related to the argument. */
   java.util.List attributes;
-
+  /** Ngram features related to the argument. */
   java.util.List ngrams;
-
-  /** The arrays and variables for fast computations. */
+  /** Object for arrays and variables for fast computation purpose. */
   ArraysDataSetDefinition arrs;
-
 }
