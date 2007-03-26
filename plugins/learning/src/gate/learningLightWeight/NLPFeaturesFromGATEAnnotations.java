cb56dbaa1013534b04273b67815d305167f08935
yaoyongli <yaoyongli@aa4978b9-1eff-4426-90b4-889a60d71948>
2007-03-26 15:01:47 +0000

    Not used any more

--- plugins/learning/src/gate/learningLightWeight/NLPFeaturesFromGATEAnnotations.java
+++
@@ -1,7 +0,0 @@
-package gate.learningLightWeight;
-
-//obtain the NLP features from GATE annotations from each document,
-//write the NLP features into a file for each document
-public class NLPFeaturesFromGATEAnnotations {
-
-}
