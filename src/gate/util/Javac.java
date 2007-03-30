f83695f280ea8b7ab15374c49f0ea1f0f0f9bbe9
ian_roberts <ian_roberts@aa4978b9-1eff-4426-90b4-889a60d71948>
2007-03-30 13:44:49 +0000

    Changed the compiler selection logic to fall back on the default compiler type if the user-specified compiler implementation throws an exception from its constructor.  Previously it fell back on the default if the user-specified class could not be found, but not if it was found but not initialized.

--- src/gate/util/Javac.java
+++ src/gate/util/Javac.java
@@ -14,25 +14,28 @@
 package gate.util;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 import gate.Gate;
 import gate.GateConstants;
 
 /**
- * This class compiles a set of java sources using the user's preferred Java
- * compiler.  The default compiler used is the Eclipse JDT compiler, but this
- * can be overridden by the user via an option in gate.xml.
+ * This class compiles a set of java sources using the user's preferred
+ * Java compiler. The default compiler used is the Eclipse JDT compiler,
+ * but this can be overridden by the user via an option in gate.xml.
  */
 public abstract class Javac implements GateConstants {
 
   /**
-   * Compiles a set of java sources and loads the compiled classes in the gate
-   * class loader.
+   * Compiles a set of java sources and loads the compiled classes in
+   * the gate class loader.
+   * 
    * @param sources a map from fully qualified classname to java source
-   * @throws GateException in case of a compilation error or warning.
-   * In the case of warnings the compiled classes are loaded before the error is
-   * raised.
+   * @throws GateException in case of a compilation error or warning. In
+   *           the case of warnings the compiled classes are loaded
+   *           before the error is raised.
    */
   public static void loadClasses(Map sources) throws GateException {
     if(compiler == null) {
@@ -43,9 +46,9 @@ public abstract class Javac implements GateConstants {
   }
 
   /**
-   * Sets the type of compiler to be used, based on the user's configuration.
-   * The default is to use the Eclipse compiler unless the user requests
-   * otherwise.
+   * Sets the type of compiler to be used, based on the user's
+   * configuration. The default is to use the Eclipse compiler unless
+   * the user requests otherwise.
    */
   private static void setCompilerTypeFromUserConfig() throws GateException {
     if(classLoader == null) classLoader = Gate.getClassLoader();
@@ -56,74 +59,98 @@ public abstract class Javac implements GateConstants {
       compilerType = DEFAULT_COMPILER;
     }
 
-    // We try and load the compiler class first by treating the given name as a
-    // fully qualified class name.  If this fails, we prepend
-    // "gate.util.compilers." (so the user can say just "Sun" rather than
-    // "gate.util.compilers.Sun").  If that fails, we try the default value
-    // DEFAULT_COMPILER.  If that fails, we give up.
+    // We try and load the compiler class first by treating the given
+    // name as a fully qualified class name. If this fails, we prepend
+    // "gate.util.compilers." (so the user can say just "Sun" rather
+    // than "gate.util.compilers.Sun"). If that fails, we try the
+    // default value DEFAULT_COMPILER. If that fails, we give up.
+    try {
+      // first treat the compiler type as a fully qualified class name
+      compiler = createCompilerInstance(compilerType);
+    }
+    catch(GateException ge) {
+      // if it's the default compiler we've just failed to load, give up
+      // now
+      if(DEFAULT_COMPILER.equals(compilerType)) {
+        throw ge;
+      }
+
+      // we failed to find the class as a FQN, so try relative to
+      // gate.util.compilers
+      compilerType = "gate.util.compilers." + compilerType;
+      try {
+        compiler = createCompilerInstance(compilerType);
+      }
+      catch(GateException ge2) {
+        // if it's the default compiler we've just failed to load, give
+        // up now
+        if(DEFAULT_COMPILER.equals(compilerType)) {
+          throw ge2;
+        }
+
+        Err.prln("Unable to load compiler class " + compilerType
+                + ", falling back to default of " + DEFAULT_COMPILER);
+        compilerType = DEFAULT_COMPILER;
+        // last try - fall back on the default value. If this fails we
+        // just allow the failure exception to propagate up the stack
+        // from here.
+        compiler = createCompilerInstance(compilerType);
+      }
+    }
+  }
+
+  private static Javac createCompilerInstance(String compilerType)
+          throws GateException {
     Class compilerClass = null;
     try {
       // first treat the compiler type as a fully qualified class name
       compilerClass = classLoader.loadClass(compilerType, true);
     }
     catch(ClassNotFoundException cnfe) {
-      // not a problem
+      // ignore exception but leave compilerClass == null
     }
 
-    if(compilerClass == null 
-        || !Javac.class.isAssignableFrom(compilerClass)) {
-      // failed to find the class as a FQN, so try relative to
-      // gate.util.compilers
-      compilerType = "gate.util.compilers." + compilerType;
-      try {
-        compilerClass = classLoader.loadClass(compilerType, true);
-      }
-      catch(ClassNotFoundException cnfe2) {
-        // still not a problem
-      }
+    if(compilerClass == null || !Javac.class.isAssignableFrom(compilerClass)) {
+      throw new GateException("Unable to load Java compiler class "
+              + compilerType);
     }
 
-    if(compilerClass == null
-        || !Javac.class.isAssignableFrom(compilerClass)) {
-      Err.prln("Unable to load compiler class " + compilerType 
-          + ", falling back to default of " + DEFAULT_COMPILER);
-      compilerType = DEFAULT_COMPILER;
-      try {
-        compilerClass = classLoader.loadClass(compilerType, true);
-      }
-      catch(ClassNotFoundException cnfe3) {
-        // a problem
-      }
-    }
-
-    if(compilerClass == null
-        || !Javac.class.isAssignableFrom(compilerClass)) {
-      throw new GateException("Unable to load a Java compiler class");
-    }
-    
     // At this point we have successfully loaded a compiler class.
-    // Now create an instance using a no-argument constructor.
+    // Now try and create an instance using a no-argument constructor.
     try {
-      compiler = (Javac)compilerClass.newInstance();
+      Constructor<?> noArgConstructor = compilerClass.getConstructor();
+      return (Javac)noArgConstructor.newInstance();
     }
     catch(IllegalAccessException iae) {
-      Err.prln("Cannot access Java compiler class " + compilerType);
-      throw new GateException(iae);
+      throw new GateException("Cannot access Java compiler class "
+              + compilerType, iae);
     }
     catch(InstantiationException ie) {
-      Err.prln("Cannot instantiate Java compiler class " + compilerType);
-      throw new GateException(ie);
+      throw new GateException("Cannot instantiate Java compiler class "
+              + compilerType, ie);
+    }
+    catch(NoSuchMethodException nsme) {
+      throw new GateException("Java compiler class " + compilerType
+              + " does not have a no-argument constructor", nsme);
+    }
+    catch(InvocationTargetException ite) {
+      throw new GateException("Exception when constructing Java compiler " +
+              "of type " + compilerType, ite.getCause());
+    }
+    catch(ExceptionInInitializerError eiie) {
+      throw new GateException("Exception when initializing Java compiler "
+              + "class " + compilerType, eiie.getCause());
     }
   }
 
   /**
-   * Compile a set of Java sources, and load the resulting classes into the
-   * GATE class loader.
-   *
+   * Compile a set of Java sources, and load the resulting classes into
+   * the GATE class loader.
+   * 
    * @param sources a map from fully qualified classname to java source
-   * @throws GateException in case of a compilation error or warning.
-   * In the case of warnings, the compiled classes are loaded before the
-   * exception is thrown.
+   * @throws GateException in case of a compilation error or warning. In
+   *           the case of warnings, the compiled classes are loaded
+   *           before the exception is thrown.
    */
   public abstract void compile(Map sources) throws GateException;
 
@@ -136,7 +163,7 @@ public abstract class Javac implements GateConstants {
    * The GATE class loader.
    */
   private static GateClassLoader classLoader = null;
-  
+
   /**
    * The default compiler to use.
    */
