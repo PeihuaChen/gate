78d986975401a334563fa121fd9f25168eae8182
ian_roberts <ian_roberts@aa4978b9-1eff-4426-90b4-889a60d71948>
2008-08-25 22:50:05 +0000

    Corrected package comment.

--- src/gate/creole/metadata/package-info.java
+++ src/gate/creole/metadata/package-info.java
@@ -1,9 +1,8 @@
 /**
  * This package contains Java 5 annotation types that can be used
  * to annotate the classes that implement CREOLE resources as an
- * alternative to writing a creole.xml file by hand.  The annotations
- * are not read at runtime - the creole.xml file is still used but
- * it is generated automatically from the annotations rather than
- * being written by hand.
+ * alternative to writing a creole.xml file by hand.  See
+ * <a href="http://gate.ac.uk/cgi-bin/userguide/sec:creole-model:config">the
+ * user guide</a> for details.
  */
 package gate.creole.metadata;
