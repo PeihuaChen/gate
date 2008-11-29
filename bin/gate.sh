67eeaf26a4bcaaf915f317450edd061ffa592c72
johann_p <johann_p@aa4978b9-1eff-4426-90b4-889a60d71948>
2008-11-29 12:27:20 +0000

    Allow command line arguments and make the script work when invoked via a symbolic link.

--- bin/gate.sh
+++ bin/gate.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-
-GATE_BIN_DIR=`dirname $0`
-export GATE_HOME=`dirname $GATE_BIN_DIR`
+# adapted from ant starting script
+PRG="$0"
+# need this for relative symlinks
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`"/$link"
+  fi
+done
+GATE_HOME=`dirname "$PRG"`/..
+# make it fully qualified
+export GATE_HOME=`cd "$GATE_HOME" && pwd`
 cd $GATE_HOME 
-
-exec bin/ant run
+exec bin/ant run "$@"
