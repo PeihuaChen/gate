db9af498d57ccb6d196c18b218c41379eb6026f2
angus_roberts <angus_roberts@aa4978b9-1eff-4426-90b4-889a60d71948>
2009-07-21 16:17:39 +0000

    Added all options to creole, removed use of xml config file, added a few comments

--- plugins/OpenCalais/src/gate/opencalais/OpenCalais.java
+++ plugins/OpenCalais/src/gate/opencalais/OpenCalais.java
@@ -1,3 +1,17 @@
+/*
+ *  OpenCalais.java
+ *
+ *  Copyright (c) 2009, The University of Sheffield.
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ */
+
+
+
 package gate.opencalais;
 
 import java.io.InputStreamReader;
@@ -10,9 +24,6 @@ import java.net.URISyntaxException;
 import java.io.UnsupportedEncodingException;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.io.FileReader;
 import java.io.Reader;
 import java.io.Serializable;
 
@@ -31,12 +42,28 @@ import gate.creole.ExecutionException;
 import gate.util.InvalidOffsetException;
 import gate.corpora.DocumentContentImpl;
 
+
 /**
- * A semantic annotator using OpenCalais
+ * <p>A semantic annotator using OpenCalais. OpenCalais is called
+ * using its REST interface, via a http POST.</p>
+ *
+ * <p>This GATE Processing resource was written by participants at
+ * FIG09 - the GATE Summer School 2009. A small portion of the code
+ * was inspired by James Leigh, "Extracting meaning from text with
+ * OpenCalias R3" at http://www.devx.com/semantic/Article/39550/1954
+ * </p>
  */
 public class OpenCalais extends AbstractLanguageAnalyser implements
                                                     ProcessingResource,
                                                     Serializable {
+
+  /**
+   * The OpenCalais configuraiton xml
+   * 
+   */
+  private String paramsXMLString;
+
+
   /**
    * The URL of the OpenCalais REST service
    * 
@@ -51,21 +78,8 @@ public class OpenCalais extends AbstractLanguageAnalyser implements
     return openCalaisURL;
   }
 
-  /**
-   * The URL of the OpenCalais configuraiton file
-   * 
-   */
-  private URL paramsXMLURL;
- 
-  private String paramsXMLString;
 
-  public void setParamsXMLURL(URL newValue) {
-    paramsXMLURL = newValue;
-  }
 
-  public URL getParamsXMLURL() {
-    return paramsXMLURL;
-  }
 
   /**
    * The license key for OpenCalais
@@ -83,6 +97,129 @@ public class OpenCalais extends AbstractLanguageAnalyser implements
 
 
   /**
+   * The reltagBaseURL: Base URL to be put in rel-tag microformats
+   * 
+   */
+  private URL reltagBaseURL;
+
+  public void setReltagBaseURL(URL newValue) {
+    reltagBaseURL = newValue;
+  }
+
+  public URL getReltagBaseURL() {
+    return reltagBaseURL;
+  }
+
+
+  /**
+   * The calculateRelevanceScore: Indicates whether the extracted metadata will include relevance score for each unique entity
+   * 
+   */
+  private Boolean calculateRelevanceScore;
+
+  public void setCalculateRelevanceScore(Boolean newValue) {
+    calculateRelevanceScore = newValue;
+  }
+
+  public Boolean getCalculateRelevanceScore() {
+    return calculateRelevanceScore;
+  }
+
+
+  /**
+   * The enableMetadataType: Indicates whether output will include Generic Relation extractions (RDF) and/or SocialTags 
+   *
+   */
+  private gate.opencalais.MetadataType enableMetadataType;
+
+  public void setEnableMetadataType(gate.opencalais.MetadataType newValue) {
+    enableMetadataType = newValue;
+  }
+
+  public gate.opencalais.MetadataType getEnableMetadataType() {
+    return enableMetadataType;
+  }
+
+
+  /**
+   * The docRDFaccessible: Indicates whether entire XML/RDF document is saved in the Calais Linked Data repository
+   * 
+   */
+  private Boolean docRDFaccessible;
+
+  public void setDocRDFaccessible(Boolean newValue) {
+    docRDFaccessible = newValue;
+  }
+
+  public Boolean getDocRDFaccessible() {
+    return docRDFaccessible;
+  }
+
+
+  /**
+   * The allowDistribution: Indicates whether the extracted metadata can be distributed
+   * 
+   */
+  private Boolean allowDistribution;
+
+  public void setAllowDistribution(Boolean newValue) {
+    allowDistribution = newValue;
+  }
+
+  public Boolean getAllowDistribution() {
+    return allowDistribution;
+  }
+
+
+  /**
+   * The allowSearch: Indicates whether future searches can be performed on the extracted metadata
+   * 
+   */
+  private Boolean allowSearch;
+
+  public void setAllowSearch(Boolean newValue) {
+    allowSearch = newValue;
+  }
+
+  public Boolean getAllowSearch() {
+    return allowSearch;
+  }
+
+
+  /**
+   * The externalID: User-generated ID for the submission
+   * 
+   */
+  private String externalID;
+
+  public void setExternalID(String newValue) {
+    externalID = newValue;
+  }
+
+  public String getExternalID() {
+    return externalID;
+  }
+
+
+  /**
+   * The submitter: Identifier for the content submitter
+   * 
+   */
+  private String submitter;
+
+  public void setSubmitter(String newValue) {
+    submitter = newValue;
+  }
+
+  public String getSubmitter() {
+    return submitter;
+  }
+
+
+
+
+
+  /**
    * Create the semantic annotator
    */
   public Resource init() throws ResourceInstantiationException {
@@ -93,29 +230,37 @@ public class OpenCalais extends AbstractLanguageAnalyser implements
     if(licenseID == null || licenseID.equals("")) { throw new ResourceInstantiationException(
             "OpenCalais license ID must be specified"); }
 
-    // Read the params file
-    try {
-      File paramsFile = new File(paramsXMLURL.toURI());
-      BufferedReader bufReader = new BufferedReader(new FileReader(paramsFile));
-      String line = bufReader.readLine();
-      StringBuffer strBuf = new StringBuffer();
-      while(line != null) {
-	strBuf.append(line);
-        line = bufReader.readLine();
-      }
-
-      paramsXMLString = strBuf.toString();
-
-    } catch(FileNotFoundException fnfe) {   
-      throw new ResourceInstantiationException(
-              "paramsXML file" + paramsXMLURL  + "does not exist");
-    } catch(IOException ioe) {
-      throw new ResourceInstantiationException(
-              "Problem reading paramsXML file" + paramsXMLURL);
-    } catch(URISyntaxException use) {
-      throw new ResourceInstantiationException(
-              "Problem reading params file URI: " + paramsXMLURL);
+    // Construct the params xml
+    paramsXMLString  = "<c:params xmlns:c=\"http://s.opencalais.com/1/pred/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"> "
+                       + "<c:processingDirectives c:contentType=\"text/txt\" c:outputFormat=\"xml/rdf\"> "
+                       + "</c:processingDirectives> ";
+    // include parameters that have at least default values
+    paramsXMLString += "<c:userDirectives " 
+                       + "c:calculateRelevanceScore=\"" + calculateRelevanceScore
+                       + "\" c:docRDFaccessible=\"" + docRDFaccessible   
+                       + "\" c:allowDistribution=\"" + allowDistribution
+                       + "\" c:allowSearch=\"" + allowSearch;
+    // for all other parameters, check if they have been set, before including
+    if (reltagBaseURL != null)
+      paramsXMLString += "\" c:reltagBaseURL=\"" + reltagBaseURL;
+    
+    if (enableMetadataType != null) {
+      if (enableMetadataType.equals("GenericRelations") || enableMetadataType.equals("SocialTags"))
+        paramsXMLString += "\" c:enableMetadataType=\"" + enableMetadataType;
+      if (enableMetadataType.equals("both"))
+        paramsXMLString += "\" c:enableMetadataType=\"GenericRelations,SocialTags\"";
     }
+
+    if (externalID != null)
+      paramsXMLString += "\" c:externalID=\"" + externalID;
+    
+    if (submitter != null)
+      paramsXMLString += "\" c:submitter=\"" + submitter;
+    
+    paramsXMLString += "\"> </c:userDirectives>"
+                       + " <c:externalMetadata>"
+                       + " </c:externalMetadata>"
+                       + " </c:params>";
 
     return this;
   }
@@ -133,7 +278,7 @@ public class OpenCalais extends AbstractLanguageAnalyser implements
        results = new BufferedReader(post(docText));
     }catch (IOException ioe) {
       throw new ExecutionException(
-              "Problem reading params file URI: " + paramsXMLURL);
+              "Problem talking to OpenCalais service: " + openCalaisURL);
     }
 
 
@@ -154,24 +299,25 @@ public class OpenCalais extends AbstractLanguageAnalyser implements
     
   }
   
+
   /**
-   * Post a request to OpenCalais
+   * Post a request to the OpenCalais REST interface.
    */
-    private Reader post(String text) throws IOException, UnsupportedEncodingException {
-	StringBuilder sb = new StringBuilder(text.length() + 1024);
-	sb.append("licenseID=").append(encode(licenseID));
-	sb.append("&content=").append(encode(text));
-	sb.append("&paramsXML=").append(encode(paramsXMLString));
-	URLConnection connection = openCalaisURL.openConnection();
-	connection.addRequestProperty("Content-Type",
-			"application/x-www-form-urlencoded");
-	connection.addRequestProperty("Content-Length", Integer.toString(sb.length()));
-	connection.setDoOutput(true);
-	OutputStream out = connection.getOutputStream();
-	OutputStreamWriter writer = new OutputStreamWriter(out);
-	writer.write(sb.toString());
-	writer.flush();
-	return new InputStreamReader(connection.getInputStream());
+  private Reader post(String text) throws IOException, UnsupportedEncodingException {
+    StringBuilder sb = new StringBuilder(text.length() + 1024);
+    sb.append("licenseID=").append(encode(licenseID));
+    sb.append("&content=").append(encode(text));
+    sb.append("&paramsXML=").append(encode(paramsXMLString));
+    URLConnection connection = openCalaisURL.openConnection();
+    connection.addRequestProperty("Content-Type",
+      "application/x-www-form-urlencoded");
+    connection.addRequestProperty("Content-Length", Integer.toString(sb.length()));
+    connection.setDoOutput(true);
+    OutputStream out = connection.getOutputStream();
+    OutputStreamWriter writer = new OutputStreamWriter(out);
+    writer.write(sb.toString());
+    writer.flush();
+    return new InputStreamReader(connection.getInputStream());
   }
 
 
