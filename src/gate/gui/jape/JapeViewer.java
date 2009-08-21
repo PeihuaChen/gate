7304481235705a3ac9022ea9a37f9954746333c9
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2009-08-21 09:57:06 +0000

    more improvements to the JAPE viewer so that now if you load a multiphase grammar you can select and view each phase. The combo box and reset button combination is horrid, should probably replace with a tre viewer of some form

--- src/gate/gui/jape/JapeViewer.java
+++ src/gate/gui/jape/JapeViewer.java
@@ -9,21 +9,29 @@ import gate.jape.parser.ParseCpslConstants;
 import gate.jape.parser.ParseCpslTokenManager;
 import gate.jape.parser.SimpleCharStream;
 import gate.jape.parser.Token;
+import gate.util.Files;
 import gate.util.GateRuntimeException;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.JToolBar;
@@ -56,20 +64,18 @@ public class JapeViewer extends AbstractVisualResource implements
   /** Should this component behave as an editor as well as an viewer */
   private boolean editable = false;
 
-  /** A Button for saving the contents in a Jape file */
-  private JButton saveButton;
-
-  /** A Button for reverting contents */
-  private JButton revertButton;
-
   /** A field that holds the jape file name */
   private URL japeFileURL;
 
   /** a field that holds the jape file contents */
   private String japeFileContents;
+  
+  private boolean updating = false;
 
   /** Transducer */
   private Transducer transducer;
+  
+  private JComboBox cboPhases;
 
   // should probably be static
 
@@ -90,7 +96,43 @@ public class JapeViewer extends AbstractVisualResource implements
             new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                     JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     add(textScroll, BorderLayout.CENTER);
+    
+    toolbar = new JToolBar();
+    toolbar = new JToolBar(JToolBar.HORIZONTAL);
+    toolbar.setFloatable(false);
+    
+    JButton btnReload = new JButton("Reset");
+    btnReload.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        japeFileURL = transducer.getGrammarURL();
+        readJAPEFileContents();
+      }
+    });
+    toolbar.add(btnReload);
+    
+    cboPhases = new JComboBox();
+    cboPhases.setPrototypeDisplayValue("The name of a phase");
+    cboPhases.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent ie) {
+        if (updating) return;
+        if (ie.getStateChange() != ItemEvent.SELECTED) return;
+        
+        try {
+          japeFileURL = new URL(transducer.getGrammarURL(),ie.getItem()+".jape");
+          System.out.println(japeFileURL);
+          readJAPEFileContents();
+        }
+        catch (MalformedURLException mue) {
+          mue.printStackTrace();
+        }
+      }
+    });
+    toolbar.addSeparator();
+    toolbar.add(new JLabel("Phase:"));
+    toolbar.add(cboPhases);
 
+    add(toolbar, BorderLayout.NORTH);
+    
     // if we want to set the jape to be monospaced then do this...
     /*
      * MutableAttributeSet attrs = textArea.getInputAttributes();
@@ -156,6 +198,7 @@ public class JapeViewer extends AbstractVisualResource implements
   }
 
   private void readJAPEFileContents() {
+    updating = true;
     try {
       if(japeFileURL != null) {
         Reader japeReader = null;
@@ -180,6 +223,12 @@ public class JapeViewer extends AbstractVisualResource implements
         textArea.setEditable(false);
         br.close();
 
+        cboPhases.removeAllItems();
+        System.out.println(cboPhases.getItemCount());
+        String mainName = Files.fileFromURL(japeFileURL).getName();
+        mainName = mainName.substring(0,mainName.lastIndexOf("."));
+        cboPhases.addItem("");
+        
         ParseCpslTokenManager tokenManager =
                 new ParseCpslTokenManager(new SimpleCharStream(
                         new StringReader(japeFileContents)));
@@ -222,6 +271,10 @@ public class JapeViewer extends AbstractVisualResource implements
             int end = lineOffsets.get(t.endLine - 1) + t.endColumn - 1;
             doc.setCharacterAttributes(start, end - start + 1, style, true);
           }
+          
+          if (t.kind == ParseCpslConstants.path) {
+            cboPhases.addItem(t.toString());
+          }
         }
       } else {
         textArea
@@ -230,9 +283,11 @@ public class JapeViewer extends AbstractVisualResource implements
     } catch(IOException ioe) {
       throw new GateRuntimeException(ioe);
     }
+    updating = false;
   }
 
   public void processFinished() {
+    japeFileURL = transducer.getGrammarURL();
     readJAPEFileContents();
   }
 
