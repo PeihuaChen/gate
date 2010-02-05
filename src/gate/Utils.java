b5c7030088ac75580c7f4dabf9464ff4882741a8
johann_p <johann_p@aa4978b9-1eff-4426-90b4-889a60d71948>
2010-02-05 14:22:22 +0000

    Utility class for methods that provide useful shortcuts to often-used tasks. For now contains length() for Annotations and Documents and getContentForAnntotion() and getStringForAnnotation() to get the content/text that is covered by an annotation in a document.

---
+++ src/gate/Utils.java
@@ -0,0 +1,121 @@
+/*
+ *  Utils.java
+ *
+ *  Copyright (c) 1995-2010, The University of Sheffield. See the file
+ *  COPYRIGHT.txt in the software or at http://gate.ac.uk/gate/COPYRIGHT.txt
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *  Johann Petrak, 2010-02-05
+ *
+ *  $Id: Main.java 12006 2009-12-01 17:24:28Z thomas_heitz $
+ */
+
+package gate;
+
+import gate.util.GateRuntimeException;
+
+/**
+ * Various utility methods to make often-needed tasks more easy and
+ * using up less code.
+ * @author Johann Petrak
+ */
+public class Utils {
+  /**
+   * Return the length of the document content covered by an Annotation as an
+   * int -- if the content is too long for an int, the method will throw
+   * a GateRuntimeException. Use getLengthLong(SimpleAnnotation ann) if
+   * this situation could occur.
+   * @param ann the annotation for which to determine the length
+   * @return the length of the document content covered by this annotation.
+   */
+  public static int length(SimpleAnnotation ann) {
+    long len = lengthLong(ann);
+    if (len > java.lang.Integer.MAX_VALUE) {
+      throw new GateRuntimeException(
+              "Length of annotation too big to be returned as an int: "+len);
+    } else {
+      return (int)len;
+    }
+  }
+
+  /**
+   * Return the length of the document content covered by an Annotation as a
+   * long.
+   * @param ann the annotation for which to determine the length
+   * @return the length of the document content covered by this annotation.
+   */
+  public static long lengthLong(SimpleAnnotation ann) {
+    return ann.getEndNode().getOffset() -
+       ann.getStartNode().getOffset();
+  }
+
+  /**
+   * Return the length of the document as an
+   * int -- if the content is too long for an int, the method will throw a
+   * GateRuntimeException. Use getLengthLong(Document doc) if
+   * this situation could occur.
+   * @param doc the document for which to determine the length
+   * @return the length of the document content.
+   */
+  public static int length(Document doc) {
+    long len = doc.getContent().size();
+    if (len > java.lang.Integer.MAX_VALUE) {
+      throw new GateRuntimeException(
+              "Length of document too big to be returned as an int: "+len);
+    } else {
+      return (int)len;
+    }
+  }
+
+  /**
+   * Return the length of the document as a long.
+   * @param doc the document for which to determine the length
+   * @return the length of the document content.
+   */
+  public static long lengthLong(Document doc) {
+    return doc.getContent().size();
+  }
+
+  /**
+   * Return the DocumentContent corresponding to the annotation.
+   * <p>
+   * Note: the DocumentContent object returned will also contain the
+   * original content which can be accessed using the getOriginalContent()
+   * method.
+   * @param doc the document from which to extract the content
+   * @param ann the annotation for which to return the content.
+   * @return a DocumentContent representing the content spanned by the annotation.
+   */
+  public static DocumentContent getContentForAnnotation(
+          SimpleDocument doc, SimpleAnnotation ann) {
+    try {
+      return doc.getContent().getContent(
+              ann.getStartNode().getOffset(),
+              ann.getEndNode().getOffset());
+    } catch(gate.util.InvalidOffsetException ex) {
+      throw new GateRuntimeException(ex.getMessage());
+    }
+  }
+
+  /**
+   * Return the document text as a String corresponding to the annotation.
+   * @param doc the document from which to extract the document text
+   * @param ann the annotation for which to return the text.
+   * @return a String representing the text content spanned by the annotation.
+   */
+  public static String getStringForAnnotation(
+          Document doc, SimpleAnnotation ann) {
+    try {
+      return doc.getContent().getContent(
+              ann.getStartNode().getOffset(),
+              ann.getEndNode().getOffset()).toString();
+    } catch(gate.util.InvalidOffsetException ex) {
+      throw new GateRuntimeException(ex.getMessage());
+    }
+  }
+
+}
