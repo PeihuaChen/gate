7dceafc270a9f91beedf61fb559077bd6e10ddd9
ian_roberts <ian_roberts@aa4978b9-1eff-4426-90b4-889a60d71948>
2010-02-25 14:09:38 +0000

    Clarifying the interface javadoc as well.

--- src/gate/annotation/AnnotationSetImpl.java
+++ src/gate/annotation/AnnotationSetImpl.java
@@ -592,7 +592,7 @@ public class AnnotationSetImpl extends AbstractSet<Annotation> implements
    * Select annotations contained within an interval, i.e.
    * those annotations whose start position is
    * >= <code>startOffset</code> and &lt; <code>endOffset</code>
-   * and whose end position is &lt= <code>endOffset</code>.
+   * and whose end position is &lt;= <code>endOffset</code>.
    */
   public AnnotationSet getContained(Long startOffset, Long endOffset) {
     // the result will include all the annotations that either:
