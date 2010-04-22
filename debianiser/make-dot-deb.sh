7a0dd1365878b5c7a57800c92b1096d63888675e
hcunningham <hcunningham@aa4978b9-1eff-4426-90b4-889a60d71948>
2010-04-22 21:35:55 +0000

    added first draft of a .deb creator...

---
+++ debianiser/make-dot-deb.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# make-dot-deb.sh -- hamish -- apr-22-2010
+
+# prevent cd-related errors
+unset CDPATH
+
+# some constants
+VERSION=`cat ../build/version.txt`-`cat ../build/build.txt`-0ubuntu1
+DEBFILE=gate-developer-${VERSION}_all.deb
+DEBELS=deb-elements
+SUMS=control/md5sums
+STARTSCRIPT=data/usr/bin/gate-developer
+CHANGELOG=usr/share/doc/gate-developer/changelog.Debian
+
+# dump the previous one if it exists
+[ -f ${DEBFILE} ] && rm ${DEBFILE}
+
+# the startup script
+sed 's,GATE_HOME=`dirname.*$,GATE_HOME=/usr/share/gate-developer,' \
+  ../bin/gate.sh >${STARTSCRIPT}
+chmod 755 ${STARTSCRIPT}
+
+# the changelog
+cd data
+[ -f ${CHANGELOG} ] && gzip ${CHANGELOG}
+
+# the control file
+cd ../control
+sed 's,^Version:.*$,Version: '${VERSION}',' control >$$
+mv $$ control
+
+# the checksums
+>../${SUMS}
+for f in `find . -type f |grep -v '\.svn' |sed 's,^\./,,'`
+do
+  md5sum $f >> ../${SUMS}
+done
+cd ..
+
+# the control and data .tgzs and the .deb
+cd control && fakeroot tar czf ../${DEBELS}/control.tar.gz *
+cd ../data && fakeroot tar czf ../${DEBELS}/data.tar.gz *
+cd ../${DEBELS} && ar -cr ../${DEBFILE} control.tar.gz data.tar.gz debian-binary
+cd ..
+
+# all done
+echo; echo we seem to have a new .deb in ${DEBFILE}; echo
