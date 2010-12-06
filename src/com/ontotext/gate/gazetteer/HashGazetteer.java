4fbfcd59578f1f250ee0ac873ec41ba1212d8c7d
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2010-12-06 14:02:22 +0000

    Moved from a binary only copy of the Ontotext HashGazetteer to having the source in the main tree -- thanks to Ontotext for allowing decompilation to recover the source. There are still improvements to be made to deal with some weird method names etc. that came from the decompilation process, and some obvious optimizations to the original source code.

---
+++ src/com/ontotext/gate/gazetteer/HashGazetteer.java
@@ -0,0 +1,412 @@
+/**
+ * (c) Copyright Ontotext Lab, Sirma Group Corp 2004
+ */
+
+package com.ontotext.gate.gazetteer;
+
+import gate.AnnotationSet;
+import gate.Factory;
+import gate.FeatureMap;
+import gate.Resource;
+import gate.creole.ExecutionException;
+import gate.creole.ResourceInstantiationException;
+import gate.creole.gazetteer.AbstractGazetteer;
+import gate.creole.gazetteer.GazetteerException;
+import gate.creole.gazetteer.GazetteerList;
+import gate.creole.gazetteer.GazetteerNode;
+import gate.creole.gazetteer.LinearDefinition;
+import gate.creole.gazetteer.LinearNode;
+import gate.creole.gazetteer.Lookup;
+import gate.creole.gazetteer.MappingNode;
+import gate.util.InvalidOffsetException;
+import gate.util.LuckyException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class HashGazetteer extends AbstractGazetteer {
+  private static final long serialVersionUID = -4603155688378104052L;
+
+  @SuppressWarnings("unchecked")
+  public HashGazetteer() {
+    categoryList = null;
+    mapsList = new HashMap[1000];
+    mapsListSize = 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  public Resource init() throws ResourceInstantiationException {
+    if(listsURL == null)
+      throw new ResourceInstantiationException(
+              "No URL provided for gazetteer creation!");
+    try {
+      definition = new LinearDefinition();
+      definition.setURL(listsURL);
+      definition.load();
+      int i = definition.size();
+      listsByNode = definition.loadLists();
+      mapsListSize = mapsList.length;
+      categoryList = new ArrayList<Lookup>(i + 1);
+      Iterator<LinearNode> iterator = definition.iterator();
+      int j = 0;
+      LinearNode linearnode;
+      for(; iterator.hasNext(); readList(linearnode, true)) {
+        linearnode = (LinearNode)iterator.next();
+        fireStatusChanged("Reading " + linearnode.toString());
+        fireProgressChanged((++j * 100) / i);
+      }
+
+      fireProcessFinished();
+    }
+    catch(Exception exception) {
+      throw new ResourceInstantiationException(exception);
+    }
+    return this;
+  }
+
+  public void execute() throws ExecutionException {
+    if(document == null) throw new ExecutionException("Document is null!");
+    if(annotationSetName == null || annotationSetName.equals(""))
+      annotationSet = document.getAnnotations();
+    else annotationSet = document.getAnnotations(annotationSetName);
+    String s = document.getContent().toString() + " ";
+    int i = s.length();
+    int j = 0;
+    int k = 0;
+
+    StringBuffer stringbuffer = new StringBuffer();
+    boolean flag4 = false;
+    boolean flag6 = false;
+    boolean flag8 = false;
+
+    boolean flag11 = false;
+
+    String s3 = "";
+    int i1 = 0;
+    int j1 = 0;
+
+    for(int l1 = 0; l1 < i; l1++) {
+      char c = s.charAt(l1);
+      boolean flag2 = Character.isWhitespace(c);
+      if(flag2 && stringbuffer.length() == 0) {
+        j++;
+        flag8 = flag6 = flag4 = flag11 = false;
+        continue;
+      }
+      if(flag2 && flag4 && stringbuffer.length() == 1) {
+        j += 2;
+        flag8 = flag6 = flag4 = flag11 = false;
+        stringbuffer.delete(0, stringbuffer.length());
+        continue;
+      }
+      boolean flag7 = Character.isLetter(c);
+      boolean flag5 = Character.isDigit(c);
+      boolean flag3 = !flag2 && !flag7 && !flag5;
+      boolean flag10 = Character.isLowerCase(c);
+      if(k <= j && (flag2 || flag3 || flag11 && !flag10 || !flag8 && flag7))
+        k = l1;
+      boolean flag13 = flag8 && (flag5 || flag3 || flag2) || flag8 && flag7
+              && flag11 && !flag10 || flag6 && (flag7 || flag3 || flag2)
+              || flag4;
+      if(l1 == i - 1) flag13 = true;
+      if(flag13) {
+        boolean flag16 = !flag3 && !flag5;
+        if(l1 == i - 1) flag16 = true;
+        String s2 = normalizeWhitespace(stringbuffer.toString());
+        int k1 = s2.length();
+        flag16 &= k1 - j1 > 1;
+        j1 = k1;
+        if(i1 != j || !s2.equals(s3)) {
+          int l = s2.length();
+          if(l > 0) {
+            boolean flag14 = annotate(s2, j, l1, l, false);
+            if(flag14) {
+              s3 = s2;
+              i1 = j;
+            }
+            if(!flag14 && flag16 || i - 1 == l1) {
+              if(k <= j) k = l1;
+              j = k;
+              l1 = k - 1;
+              stringbuffer.delete(0, stringbuffer.length());
+              continue;
+            }
+          }
+        }
+      }
+      stringbuffer.append(c);
+      flag6 = flag5;
+      flag8 = flag7;
+      flag4 = flag3;
+    }
+
+    fireProcessFinished();
+    fireStatusChanged("Hash Gazetteer processing finished!");
+  }
+
+  public boolean add(String s, Lookup lookup1) {
+    if(!super.caseSensitive.booleanValue()) {
+      String s1 = s.toUpperCase();
+      if(!s1.equals(s)) add(s1, lookup1);
+    }
+    String s2 = removeTrailingSymbols(s);
+    if(!s2.equals(s)) add(s2, lookup1);
+    String s3 = s + " ";
+
+    List<Lookup> arraylist = null;
+    int j = 0;
+    s3.trim();
+    j = s3.length();
+
+    boolean prevIsLetter = false;
+    boolean prevIsDigit = false;
+    boolean prevIsLowercase = false;
+
+    String s4 = "";
+    Map<String, List<Lookup>> hashmap = null;
+    for(int k = 0; k < j; k++) {
+      char c = s3.charAt(k);
+      boolean currIsWhitespace = Character.isWhitespace(c);
+      boolean currIsDigit = Character.isDigit(c);
+      boolean currIsLetter = Character.isLetter(c);
+      boolean currIsSymbol = !currIsWhitespace && !currIsDigit && !currIsLetter;
+      boolean currIsLowercase = Character.isLowerCase(c);
+      boolean flag18 = prevIsLetter
+              && (currIsDigit || currIsSymbol || currIsWhitespace)
+              || prevIsLetter && currIsLetter && prevIsLowercase
+              && !currIsLowercase || prevIsDigit
+              && (currIsLetter || currIsSymbol || currIsWhitespace);
+      if(k + 1 == j) flag18 = true;
+      if(flag18) {
+        s4 = normalizeWhitespace(s3.substring(0, k));
+        int i = s4.length();
+        if(mapsList[i] == null) {
+          hashmap = new HashMap<String, List<Lookup>>();
+          mapsList[i] = hashmap;
+        }
+        else {
+          hashmap = (Map<String, List<Lookup>>)mapsList[i];
+        }
+        if(!hashmap.containsKey(s4)) hashmap.put(s4, null);
+      }
+      prevIsDigit = currIsDigit;
+      prevIsLetter = currIsLetter;
+
+      prevIsLowercase = currIsLowercase;
+
+    }
+
+    arraylist = hashmap.get(s4);
+    if(null == arraylist) {
+      arraylist = new ArrayList<Lookup>(1);
+      arraylist.add(lookup1);
+    }
+    else if(!arraylist.contains(lookup1)) arraylist.add(lookup1);
+    hashmap.put(s4, arraylist);
+    return true;
+  }
+
+  public Set<Lookup> lookup(String s) {
+    Set<Lookup> set = null;
+    String s1 = normalizeWhitespace(s);
+    int i = s1.length();
+    if(mapsListSize < i) return set;
+    Map<String, List<Lookup>> hashmap = (HashMap<String, List<Lookup>>)mapsList[i];
+    if(hashmap == null) {
+      return set;
+    }
+    else {
+      Set<Lookup> hashset = new HashSet<Lookup>(hashmap.get(s1));
+      return hashset;
+    }
+  }
+
+  private boolean annotate(String s, int i, int j, int k, boolean flag) {
+    boolean flag1 = false;
+    if(k >= mapsListSize) return false;
+    Map<String, List<Lookup>> hashmap = mapsList[k];
+    if(hashmap == null) return false;
+    if(hashmap.containsKey(s)) {
+      List<Lookup> arraylist = hashmap.get(s);
+      flag1 = true;
+      if(null != arraylist) {
+        for(Iterator<Lookup> iterator = arraylist.iterator(); iterator
+                .hasNext();) {
+          Lookup lookup1 = (Lookup)iterator.next();
+          FeatureMap featuremap = Factory.newFeatureMap();
+          featuremap.put("majorType", lookup1.majorType);
+          if(null != lookup1.oClass && null != lookup1.ontology) {
+            featuremap.put("class", lookup1.oClass);
+            featuremap.put("ontology", lookup1.ontology);
+          }
+          if(null != lookup1.minorType) {
+            featuremap.put("minorType", lookup1.minorType);
+            if(null != lookup1.languages)
+              featuremap.put("language", lookup1.languages);
+          }
+          try {
+            annotationSet.add(new Long(i), new Long(j), "Lookup", featuremap);
+          }
+          catch(InvalidOffsetException invalidoffsetexception) {
+            throw new LuckyException(invalidoffsetexception.toString());
+          }
+        }
+
+      }
+    }
+    return flag1;
+  }
+
+  public boolean remove(String s) {
+    boolean flag = false;
+    String s1 = a(s, true);
+    int i = s1.length();
+    if(i > mapsListSize) return false;
+    Map<String, List<Lookup>> hashmap = mapsList[i];
+    if(hashmap == null) return false;
+    if(hashmap.containsKey(s1)) {
+      hashmap.remove(s1);
+      flag = true;
+    }
+    return flag;
+  }
+
+  private String removeTrailingSymbols(String s) {
+    for(int i = s.length() - 1; i >= 0; i--) {
+      char c = s.charAt(i);
+      if(!Character.isLetter(c) && !Character.isDigit(c)
+              && !Character.isWhitespace(c))
+        s = s.substring(0, i);
+      else return s;
+    }
+
+    return s;
+  }
+
+  private String normalizeWhitespace(String s) {
+    StringBuffer stringbuffer = new StringBuffer();
+    s = s.trim();
+    char ac[] = s.toCharArray();
+    int i = s.length();
+    boolean flag1 = false;
+    for(int j = 0; j < i; j++) {
+      char c = ac[j];
+      boolean flag = Character.isWhitespace(c);
+      if(flag && !flag1)
+        stringbuffer.append(' ');
+      else if(!flag) stringbuffer.append(c);
+      flag1 = flag;
+    }
+
+    return stringbuffer.toString();
+  }
+
+  private String a(String s, boolean flag) {
+    StringBuffer stringbuffer = new StringBuffer();
+    boolean flag1 = true;
+    s = s.trim();
+    char ac[] = s.toCharArray();
+    int i = s.length();
+    if(i <= 1) return s;
+    char c = ac[0];
+    stringbuffer.append(c);
+    boolean flag2 = true;
+    boolean prevIsLetter = Character.isLetter(c);
+    boolean prevNotLetterOrDigit = !Character.isLetterOrDigit(c);
+
+    boolean flag10 = true;
+    char c2 = 'p';
+
+    for(int j = 1; j < i; j++) {
+      char c1 = ac[j];
+      boolean flag9 = !Character.isLetterOrDigit(c1);
+      boolean flag12 = Character.isWhitespace(c1);
+      boolean flag3 = Character.isLetter(c1);
+      boolean flag6 = Character.isDigit(c1);
+      if(j > 0 && flag2) {
+        if(prevNotLetterOrDigit && flag12) continue;
+        flag2 = prevIsLetter && flag9 || prevNotLetterOrDigit && flag3;
+        if(flag9) {
+          if(c2 == 'p') c2 = c1;
+          flag2 = flag10 = c2 == c1;
+        }
+        if(j > 2 && !flag2 && stringbuffer.length() > 0) {
+          char c3 = stringbuffer.charAt(stringbuffer.length() - 1);
+          stringbuffer.deleteCharAt(stringbuffer.length() - 1);
+          stringbuffer.append(Character.toLowerCase(c3));
+        }
+      }
+      if(flag3 || flag6) {
+        if(flag && flag3) flag1 &= Character.isUpperCase(c1);
+        if(!flag10) c1 = Character.toLowerCase(c1);
+        stringbuffer.append(c1);
+      }
+      else if(!flag2) flag10 = false;
+      prevIsLetter = flag3;
+      prevNotLetterOrDigit = flag9;
+    }
+
+    String s1 = stringbuffer.toString();
+    if(flag && flag1) s1 = s1.toUpperCase();
+    return s1;
+  }
+
+  void readList(LinearNode linearnode, boolean flag) throws GazetteerException {
+    if(null == linearnode)
+      throw new GazetteerException(" LinearNode node is null ");
+    String s = linearnode.getList();
+    String majorType = linearnode.getMajorType();
+    String minorType = linearnode.getMinorType();
+    String language = linearnode.getLanguage();
+    GazetteerList gazetteerlist = (GazetteerList)listsByNode.get(linearnode);
+    if(null == gazetteerlist)
+      throw new GazetteerException("gazetteer list not found by node");
+    List<Lookup> arraylist = new ArrayList<Lookup>(1);
+    Lookup lookup1 = new Lookup(s, majorType, minorType, language);
+    if(null != mappingDefinition) {
+      MappingNode mappingnode = mappingDefinition.getNodeByList(s);
+      if(null != mappingnode) {
+        lookup1.oClass = mappingnode.getClassID();
+        lookup1.ontology = mappingnode.getOntologyID();
+      }
+    }
+    lookup1.list = s;
+    arraylist.add(lookup1);
+    categoryList.add(lookup1);
+    @SuppressWarnings("unchecked")
+    Iterator<GazetteerNode> iterator = gazetteerlist.iterator();
+    String s6 = null;
+
+    //TODO is s5 and the else clause needed?
+    String s5;
+    if(flag)
+      for(; iterator.hasNext(); add(s6, lookup1)) {
+        String s4 = iterator.next().toString();
+        s4.trim();
+        int i = s4.length();
+        for(int j = 0; j < i; j++) {
+          if(j + 1 != i && !Character.isWhitespace(s4.charAt(j))) continue;
+          if(j + 1 == i) j = i;
+          s6 = s4.substring(0, j).trim();
+        }
+
+      }
+    else while(iterator.hasNext())
+      s5 = iterator.next().toString();
+  }
+
+  private ArrayList<Lookup> categoryList;
+
+  private Map<LinearNode, GazetteerList> listsByNode;
+
+  private Map<String, List<Lookup>> mapsList[];
+
+  private int mapsListSize;
+
+  private AnnotationSet annotationSet;
+}
