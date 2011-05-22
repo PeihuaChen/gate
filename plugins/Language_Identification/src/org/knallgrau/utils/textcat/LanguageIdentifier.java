cc7d3d586341982ccf35e3e79f7d17219a68ead8
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-05-22 16:41:37 +0000

    upgraded the TextCat library and improved the PR to allow the set of languages to be customized via a config file -- by default the same languages as previously are specified in the config file

--- plugins/Language_Identification/src/org/knallgrau/utils/textcat/LanguageIdentifier.java
+++ plugins/Language_Identification/src/org/knallgrau/utils/textcat/LanguageIdentifier.java
@@ -1,110 +1,118 @@
 package org.knallgrau.utils.textcat;
 
-import gate.*;
-import gate.creole.*;
-import gate.creole.metadata.*;
-import org.knallgrau.utils.textcat.TextCategorizer;
+import gate.Annotation;
+import gate.AnnotationSet;
+import gate.Utils;
+import gate.creole.ExecutionException;
+import gate.creole.ResourceInstantiationException;
+import gate.creole.metadata.CreoleParameter;
+import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.Optional;
+import gate.creole.metadata.RunTime;
+import gate.util.Files;
 
+import java.net.URL;
 
-@CreoleResource(name = "TextCat PR", 
-        comment = "Recognizes the document language using TextCat. Possible" +
-        "languages: german, english, french, spanish, italian, swedish, polish, dutch, " +
-        "norwegian, finnish, albanian, slovakian, slovenian, danish, hungarian.")		
-public class LanguageIdentifier 
-  extends gate.creole.AbstractLanguageAnalyser 
-  implements ControllerAwarePR     {
+import at.knallgrau.textcat.TextCategorizer;
+
+@CreoleResource(name = "TextCat Language Identification", comment = "Recognizes the document language using TextCat", icon = "paw-print.png")
+public class LanguageIdentifier extends gate.creole.AbstractLanguageAnalyser {
 
   private static final long serialVersionUID = 5831213212185693826L;
+
   @SuppressWarnings("unused")
-  private static final String __SVNID = "$Id$";
-  
-  private TextCategorizer guesser; 
-  
+  private static final String __SVNID =
+          "$Id$";
+
+  private TextCategorizer guesser;
+
+  private String languageFeatureName;
+
+  private String annotationType;
+
+  private String annotationSetName;
+
+  private URL configURL;
+
   public LanguageIdentifier init() throws ResourceInstantiationException {
+    try {
+      if(configURL != null) {
+        guesser =
+                new TextCategorizer(Files.fileFromURL(configURL)
+                        .getAbsolutePath());
+      } else {
+        guesser = new TextCategorizer();
+      }
+    } catch(Exception e) {
+      throw new ResourceInstantiationException(
+              "unable to load TextCat config file", e);
+    }
+
     return this;
   }
-  
-  
-  public void controllerExecutionStarted(Controller c)
-    throws ExecutionException {
-    guesser = new TextCategorizer();
+
+  /**
+   * Based on the document content, recognizes the language and adds a document
+   * feature.
+   */
+  public void execute() throws ExecutionException {
+    if(document == null || document.getFeatures() == null) return;
+
+    if(annotationType == null || annotationType.trim().equals("")) {
+      /*
+       * Default situation: classify the whole document and save the result as a
+       * document feature.
+       */
+      String text = document.getContent().toString();
+      String category = guesser.categorize(text);
+      document.getFeatures().put(languageFeatureName, category);
+    }
+
+    else {
+      /*
+       * New option: classify the text underlying each annotation (specified by
+       * AS and type) and save the result as an annotation feature.
+       */
+      AnnotationSet annotations =
+              document.getAnnotations(annotationSetName).get(annotationType);
+      for(Annotation annotation : annotations) {
+        String text = Utils.stringFor(document, annotation);
+        String category = guesser.categorize(text);
+        annotation.getFeatures().put(languageFeatureName, category);
+      }
+    }
+
   }
 
+  public void reInit() throws ResourceInstantiationException {
+    init();
+  }
 
-	/**
-	 * Based on the document content, recognizes the language and adds a document feature.
-	 */
-	public void execute() throws ExecutionException {
-		if(document == null || document.getFeatures() == null)
-			return;
-		
-//		String language = (String) document.getFeatures().get(languageFeatureName);
-//		if(language != null && language.length() > 0)
-//			return;
-		
-		if(languageFeatureName == null || "".equals(languageFeatureName))
-			languageFeatureName = "language";
-
-		/* Default situation: classify the whole document and save the
-		 * result as a document feature.		 */
-		if ( (annotationType == null) || (annotationType.length() == 0) )  {
-		  String text = document.getContent().toString();
-		  String category = guesser.categorize(text);
-		  document.getFeatures().put(languageFeatureName, category);
-		}
-		
-		/* New option: classify the text underlying each annotation
-		 * (specified by AS and type) and save the result as
-		 * an annotation feature.		 */
-		else {
-		  AnnotationSet annotations = document.getAnnotations(annotationSetName).get(annotationType);
-		  for (Annotation annotation : annotations) {
-		    String text = Utils.stringFor(document, annotation);
-		    String category = guesser.categorize(text);
-		    annotation.getFeatures().put(languageFeatureName, category);
-		  }
-		}
-		
-	}
-	
-	
-	public void reInit() throws ResourceInstantiationException { }
-	
-	
-	/*  CREOLE PARAMETERS */
-	
-	@RunTime
-	@Optional
-	@CreoleParameter(comment = "name of document or annotation features for the language identified",
-	        defaultValue = "LANGUAGE")
-	public void setLanguageFeatureName(String languageFeatureName) {
-		this.languageFeatureName = languageFeatureName;
-	}
+  @RunTime
+  @Optional
+  @CreoleParameter(comment = "name of document or annotation features for the language identified", defaultValue = "lang")
+  public void setLanguageFeatureName(String languageFeatureName) {
+    this.languageFeatureName = languageFeatureName;
+  }
 
   public String getLanguageFeatureName() {
     return languageFeatureName;
   }
 
-  private String languageFeatureName;
-  
-	
-	@RunTime
-	@Optional
-	@CreoleParameter(comment = "type of annotations to classify; leave blank for whole-document classification",
-	        defaultValue = "")
-	public void setAnnotationType(String atype) {
-	  this.annotationType = atype;
-	}
-	
-	public String getAnnotationType() {
-	  return this.annotationType;
-	}
-	
-	private String annotationType;
-	
-	@RunTime
-	@Optional
-	@CreoleParameter(comment = " annotation set used for input/output (ignored for whole-document classification)")
+  @RunTime
+  @Optional
+  @CreoleParameter(comment = "type of annotations to classify; leave blank for whole-document classification")
+  public void setAnnotationType(String atype) {
+    this.annotationType = atype;
+  }
+
+  public String getAnnotationType() {
+    return this.annotationType;
+  }
+
+  @RunTime
+  @Optional
+  @CreoleParameter(comment = " annotation set used for input/output (ignored for whole-document classification)")
   public void setAnnotationSetName(String inputASName) {
     this.annotationSetName = inputASName;
   }
@@ -113,21 +121,12 @@ public class LanguageIdentifier
     return annotationSetName;
   }
 
-  private String annotationSetName;
-
-	
-	/*  MISC. INHERITED STUFF */
-	
-  public void controllerExecutionAborted(Controller c, Throwable t)
-          throws ExecutionException {
-    // NOTHING
+  @CreoleParameter(defaultValue = "resources/default-names.conf")
+  public void setConfigURL(URL configURL) {
+    this.configURL = configURL;
   }
 
-  public void controllerExecutionFinished(Controller c)
-          throws ExecutionException {
-    // NOTHING
+  public URL getConfigURL() {
+    return configURL;
   }
-
-
-} // class LanguageIdentifier
-
+}
