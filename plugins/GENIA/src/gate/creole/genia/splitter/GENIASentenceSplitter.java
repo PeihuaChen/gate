457086395c06171034eb1dbb5b9c155ad7c49f39
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-06-01 17:32:00 +0000

    copyright license info

--- plugins/GENIA/src/gate/creole/genia/splitter/GENIASentenceSplitter.java
+++ plugins/GENIA/src/gate/creole/genia/splitter/GENIASentenceSplitter.java
@@ -1,3 +1,15 @@
+/*
+ * GENIASentenceSplitter
+ * 
+ * Copyright (c) 2011, The University of Sheffield.
+ * 
+ * This file is part of GATE (see http://gate.ac.uk/), and is free software,
+ * licenced under the GNU Library General Public License, Version 3, June 2007
+ * (in the distribution as file licence.html, and also available at
+ * http://gate.ac.uk/gate/licence.html).
+ * 
+ * Mark A. Greenwood, 01/06/2011
+ */
 package gate.creole.genia.splitter;
 
 import gate.AnnotationSet;
