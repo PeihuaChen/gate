f4c2474de3808568d4b576d2b680de6e4539b0e1
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-06-01 17:30:48 +0000

    a new GENIA plugin which includes a sentence splitter PR, together with the tagger framework we can now host the GENIA pipeline within GATE, and this plugin also contains a xgapp doing just that

---
+++ plugins/GENIA/src/gate/creole/genia/splitter/GENIASentenceSplitter.java
@@ -0,0 +1,111 @@
+package gate.creole.genia.splitter;
+
+import gate.AnnotationSet;
+import gate.Factory;
+import gate.creole.AbstractLanguageAnalyser;
+import gate.creole.ExecutionException;
+import gate.creole.metadata.CreoleParameter;
+import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.Optional;
+import gate.creole.metadata.RunTime;
+import gate.util.Files;
+import gate.util.ProcessManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.net.URL;
+
+@CreoleResource(name = "GENIA Sentence Splitter", icon = "sentence-splitter.png")
+public class GENIASentenceSplitter extends AbstractLanguageAnalyser {
+
+  private boolean debug = false;
+
+  private String annotationSetName;
+
+  private URL splitterBinary;
+
+  private ProcessManager manager = new ProcessManager();
+
+  public Boolean getDebug() {
+    return debug;
+  }
+
+  @RunTime
+  @CreoleParameter(defaultValue = "false")
+  public void setDebug(Boolean debug) {
+    this.debug = debug;
+  }
+
+  public URL getSplitterBinary() {
+    return splitterBinary;
+  }
+
+  @RunTime
+  @CreoleParameter()
+  public void setSplitterBinary(URL splitterBinary) {
+    this.splitterBinary = splitterBinary;
+  }
+
+  public String getAnnotationSetName() {
+    return annotationSetName;
+  }
+
+  @RunTime
+  @Optional
+  @CreoleParameter()
+  public void setAnnotationSetName(String annotationSetName) {
+    this.annotationSetName = annotationSetName;
+  }
+
+  public void execute() throws ExecutionException {
+    AnnotationSet annotationSet = document.getAnnotations(annotationSetName);
+
+    File splitter = Files.fileFromURL(splitterBinary);
+
+    String docContent =
+            document.getContent().toString().replace((char)160, ' ');
+
+    try {
+      File tmpIn = File.createTempFile("GENIA", ".txt");
+      File tmpOut = File.createTempFile("GENIA", ".txt");
+
+      FileOutputStream fos = new FileOutputStream(tmpIn);
+      fos.write(docContent.getBytes("utf8"));
+      fos.close();
+
+      String[] args =
+              new String[]{splitter.getAbsolutePath(), tmpIn.getAbsolutePath(),
+                  tmpOut.getAbsolutePath()};
+
+      manager.runProcess(args, splitter.getParentFile(), (debug ? System.out : null), (debug ? System.err : null));
+
+      int end = 0;
+
+      BufferedReader in = new BufferedReader(new FileReader(tmpOut));
+      String sentence = in.readLine();
+      while(sentence != null) {
+
+        sentence = sentence.trim();
+
+        int start = docContent.indexOf(sentence, end);
+
+        end = start + sentence.length();
+
+        if(end > start && sentence.length() > 0) {
+          annotationSet.add((long)start, (long)end, "Sentence",
+                  Factory.newFeatureMap());
+        }
+
+        sentence = in.readLine();
+      }
+      
+      if (!debug && !tmpIn.delete()) tmpIn.deleteOnExit();
+      if (!debug && !tmpOut.delete()) tmpOut.deleteOnExit();
+
+    } catch(Exception ioe) {
+      throw new ExecutionException("An error occured running the splitter", ioe);
+    }
+  }
+}
