9a5a8f4607f83e9ca9333215562ac42d652dcae1
johann_p <johann_p@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-08-23 15:15:22 +0000

    Make our representation of Literals conform more closely to the standards: a literal can be plain with or without a language tag or it can be typed with a datatype, but it cannot be both. Also, no specific language tag is associated with a literal unless explicitly specified. Deprecate the methods that make use of java.util.Locale and provide a constructor and a getter for directly using a language tag instead. Improve the method for converting a literal to Turtle language representation. Improve the JavaDoc.

--- src/gate/creole/ontology/Literal.java
+++ src/gate/creole/ontology/Literal.java
@@ -10,63 +10,110 @@ package gate.creole.ontology;
 import java.util.Locale;
 
 /**
- * Literal represents a single value or a value with language used for
- * annotation properties, or a value with datatype used for the datatype
- * properties.
+ * The class Literal represents a literal value as defined in section 6.5 of 
+ * http://www.w3.org/TR/rdf-concepts . 
+ * A literal value consists of a lexical form which is represented
+ * as a unicode string and in addition optionally either a language 
+ * tag or a datatype.
  * 
- * @author Niraj Aswani
+ * @author Niraj Aswani, Johann Petrak
  */
+
+// NOTE: (Johann Petrak) This may need to get extended in the future to 
+// also deal with the rdf:PlainLiteral datatype and the different
+// way of how literals of that type are represented.
+// See http://www.w3.org/TR/rdf-plain-literal/
+
 public class Literal {
   /**
-   * The actual value of the literal
+   * The actual value (the lexical form) of the literal, represented 
+   * as a unicode string.
    */
   private String value;
 
   /**
-   * Specified language for the literal.
+   * The language tag for the literal if it is a plain literal. If there is
+   * a language tag, there cannot be a data type.
    */
   private Locale language;
 
   /**
-   * Assigned Datatype to this instance of literal
+   * The data type of the literal if it is a typed literal. If there is 
+   * a data type, there cannot be a language tag. 
+   * Data types are represented as gate.creole.ontology.DataType objects
+   * but the only information that is used internally is the datatype 
+   * URI String (as returned by DataType.getXmlSchemaURIString())
    */
   private DataType dataType;
 
   /**
-   * Constructor
+   * Create a plain literal (i.e an untyped literal) without a language tag
+   * (the language is null).
+   * A plain literal cannot have a data type (the datatype will always be null).
    * 
-   * @param value
+   * @param val
    */
   public Literal(String value) {
     this.value = value;
-    this.language = OConstants.ENGLISH;
-    this.dataType = DataType.getStringDataType();
+    // removed by JP: a plain literal does not have a language tag nor
+    // a datatype.
+    //this.language = OConstants.ENGLISH;
+    //this.dataType = DataType.getStringDataType();
+    this.language = null;
+    this.dataType = null;
   }
 
   /**
-   * Constructor
+   * Create a plain literal (i.e. an untyped literal) with a language
+   * tag that corresponds to the string returned by language.getLanguage().
+   * Any information stored in the Java Locale object other than the
+   * language code is ignored and discarded when stored in the ontology.
+   * A plain literal cannot have a data type (the datatype will alwats be null).
    * 
-   * @param value
+   * @param value 
    * @param language
+   * @deprecated Use the constructor Literal(String value, String languagetag) 
+   * instead
    */
+  @Deprecated
   public Literal(String value, Locale language) {
     this.value = value;
     this.language = language;
-    this.dataType = DataType.getStringDataType(); 
+    // removed by JP: a plain literal with a language tag must
+    // not have a datatype
+    // this.dataType = DataType.getStringDataType(); 
+    this.dataType = null;
   }
 
   /**
-   * Constructor
+   * Create a plain literal associated with a language tag. A language tag
+   * should be an official two-character lowercase language code.
    * 
    * @param value
+   * @param languagetag 
+   */
+  public Literal(String value, String languagetag) {
+    this.value = value;
+    this.language = new Locale(languagetag);
+    this.dataType = null;
+  }
+  
+  
+  /**
+   * Create a typed literal. A typed literal is associated with a datatype
+   * and cannot have a language tag (the language will always be null).
+   * 
+   * @param value 
    * @param dataType
    * @throws InvalidValueException
    */
   public Literal(String value, DataType dataType) throws InvalidValueException {
     this.value = value;
-    this.language = OConstants.ENGLISH;
+    // removed by JP: a typed literal must not have a language tag
+    // this.language = OConstants.ENGLISH;
+    this.language = null;
     this.dataType = dataType;
-    // lets check if the provided value is valid for the supplied
+    // lets check if the provided val is valid for the supplied
     // dataType
     if(!dataType.isValidValue(this.value)) {
       throw new InvalidValueException("The value :\"" + this.value
@@ -76,9 +123,8 @@ public class Literal {
   }
 
   /**
-   * Gets the assigned datatype. This may return null if user did not
-   * use the Literal(String, Datatype) constructor to instantiate the
-   * instance.
+   * Gets the datatype of the literal. This will return null for all plain
+   * literals (i.e. literals that are not associated with a data type).
    * 
    * @return
    */
@@ -87,7 +133,7 @@ public class Literal {
   }
 
   /**
-   * Returns the value associated with this instance of literal.
+   * Returns the value (lexical form) associated with this instance of literal.
    * 
    * @return
    */
@@ -96,44 +142,89 @@ public class Literal {
   }
 
   /**
-   * Returns the language associated with this literal. This may return
-   * null if use did not use the Literal(String, String) constructor to
-   * instantiate the instance.
+   * Returns the language associated with this literal represented as a 
+   * Locale object. Note that the only useful information in the Locale
+   * object is the language code which can be retrieved with the 
+   * getLanguage() method of the Locale object.
    * 
    * @return
+   * @deprecated Use the method getLanguageTag() instead.
    */
+  @Deprecated
   public Locale getLanguage() {
     return language;
   }
 
+  /**
+   * The language tag of the literal as a two-character lowercase language code
+   * or null if no language tag
+   * is associated. This always returns null for typed literals (literals
+   * associated with a data type).
+   * 
+   * @return the language tag as String
+   */
+  public String getLanguageTag() {
+    if(language == null) {
+      return null;
+    } else {
+      return language.getLanguage();
+    }
+  }
+  
+  
+  /**
+   * This method always returns the string representation of the 
+   * literal value in the same way as getValue().
+   * For a representation that also includes the language tag or 
+   * data type, use toTurtle().
+   * 
+   * @return The value of the literal as String, identical to getValue()
+   */
+  @Override
   public String toString() {
     return value;
   }
 
 
+  /**
+   * The representation of the literal in Turtle language
+   * (see http://www.w3.org/TeamSubmission/turtle/)
+   * Note that datatype URIs for typed literals 
+   * will always be represented as full URIs
+   * and not use the xsd namespace.
+   * 
+   * @return A String that contains the representation of the literal 
+   * in Turtle language 
+   */
   public String toTurtle() {
-    // TODO: do the escaping correctly!
-    String value = this.value;
-    value = value.replace("\"", "\\\"");
-    value = "\""+value+"\"";
-    if(dataType.isStringDataType()) {
+    String val = this.value;
+    val = val.replace("\"", "\\\"");
+    val = "\""+val+"\"";
+    if(dataType == null) {
       if(language != null) {
-        value = value+"@"+language;
-      } else {
-        value = value+"^^<" + dataType.getXmlSchemaURIString() + ">";
-      }
+        val = val+"@"+language;
+      } 
     } else {
-      value = value+"^^<" + dataType.getXmlSchemaURIString() + ">";
+      val = val+"^^<" + dataType.getXmlSchemaURIString() + ">";
     }
-    return value;
+    return val;
   }
 
   @Override
   public int hashCode() {
-    return value.hashCode();
+    int hash = 17 + value.hashCode();
+    hash = 37*hash + language.hashCode();
+    hash = 37*hash + dataType.hashCode();
+    return hash;
   }
 
   @Override
+  /**
+   * Two literals are considered equal if the strings representing their
+   * values are equal and if they have identical language tags or 
+   * identical data types or both do not have either a language tag or
+   * a data type.
+   */
   public boolean equals(Object obj) {
     if (obj == null) {
       return false;
