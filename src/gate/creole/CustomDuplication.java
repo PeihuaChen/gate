e8029a1db5d085fccc8b6c741edd5bc17cbf7515
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-01-11 07:03:42 +0000

    the javadoc warning makes not sense, but it doesn't generate a link so lets change the comment to avoid needing the link in the first place

--- src/gate/creole/CustomDuplication.java
+++ src/gate/creole/CustomDuplication.java
@@ -21,10 +21,8 @@ import gate.Factory.DuplicationContext;
  * Interface which should be implemented by any Resource type which cannot be
  * duplicated in the standard way (see {@link Factory#duplicate(Resource)
  * Factory.duplicate}). If a Resource class requires custom duplication logic it
- * should implement this interface and provide an implementation of the
- * {@link CustomDuplication#duplicate(DuplicationContext) duplicate} method to
- * create a new resource instance that has the same behaviour as
- * <code>this</code>.
+ * should implement this interface and provide a method to create a new resource
+ * instance that has the same behaviour as <code>this</code>.
  * 
  * @author ian
  */
