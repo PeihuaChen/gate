9dfb1ff1ad615adac4b423dc35c81a6993946151
adamfunk <adamfunk@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-02-17 16:00:21 +0000

    Create Token and SpaceToken annotations with length & string features. Add kind (chunk|token) and column features to annotations generated from tags.

--- src/gate/corpora/ConllDocumentFormat.java
+++ src/gate/corpora/ConllDocumentFormat.java
@@ -37,6 +37,10 @@ public class ConllDocumentFormat extends TextualDocumentFormat {
   
   private static final long serialVersionUID = 5756433194230855515L;
   
+  public static final String ANNOTATION_COLUMN_FEATURE = "column";
+  public static final String ANNOTATION_KIND_FEATURE = "kind";
+  
+  
   /** Debug flag */
   private static final boolean DEBUG = false;
 
@@ -61,8 +65,19 @@ public class ConllDocumentFormat extends TextualDocumentFormat {
     // by "O" or end of sentence.
     Map<String, Annotandum> inProgress = new HashMap<String, Annotandum>();
 
-    // TODO: close "FOO" annotation when we run out of "I-FOO" tags, rather
-    // than waiting for the end of the sentence or the next "B-FOO".
+    /* Note: I-Foo handling currently has a weak spot.
+     * 
+     * this    B-Foo
+     * is      B-Bar
+     * strange I-Foo
+     * 
+     * will result in a Foo annotation spanning "this is strange", because
+     * the I-Foo extends the existing B-Foo.  If the sentence is cut off 
+     * before hitting another I-Foo, however, the Foo annotation will not
+     * have been extended.  But this situation will not occur in carefully
+     * edited input.  
+     */
+
     long oldEnd = 0L;
     long start = 0L;
     long end = 0L;
@@ -82,31 +97,34 @@ public class ConllDocumentFormat extends TextualDocumentFormat {
       
       else {
         String token = items[0];
-        // TODO: handle spacing better  
+        // We've agreed to put the space after every token.
         newContent.append(token);
         end = newContent.length();
         newContent.append(' ');
         
+        // Create Token and following SpaceToken annotation.
+        annotanda.add(Annotandum.makeToken(start, end, token));
+        annotanda.add(Annotandum.makeSpaceToken(end));
 
-        for (int i=1 ; i < items.length ; i++) {
+        for (int column=1 ; column < items.length ; column++) {
           // O means close all annotations in progress
-          if (items[i].equals("O")) {
+          if (items[column].equals("O")) {
             finishAllTags(inProgress, annotanda, oldEnd);
           }
           
           // "B-FOO": start a new "FOO" annotation
           // after closing any "FOO" already in progress
-          else if ( (items[i].length() > 2) &&
-              items[i].startsWith("B-") ) {
-            String type = items[i].substring(2);
+          else if ( (items[column].length() > 2) &&
+              items[column].startsWith("B-") ) {
+            String type = items[column].substring(2);
             finishTag(type, inProgress, annotanda, oldEnd);
-            inProgress.put(type, new Annotandum(type, start, end));
+            inProgress.put(type, new Annotandum(type, start, end, column, true));
           }
           
           // "I-FOO": extend current "FOO" annotation
-          else if ( (items[i].length() > 2) &&
-              items[i].startsWith("I-") ) {
-            String type = items[i].substring(2);
+          else if ( (items[column].length() > 2) &&
+              items[column].startsWith("I-") ) {
+            String type = items[column].substring(2);
             
             if (inProgress.containsKey(type)) {
               // good I-FOO, so update the end offset
@@ -114,13 +132,13 @@ public class ConllDocumentFormat extends TextualDocumentFormat {
             }
             else {
               // bad data, containing I-FOO without a B-FOO, so treat as if B-FOO
-              inProgress.put(type, new Annotandum(type, start, end));
+              inProgress.put(type, new Annotandum(type, start, end, column, true));
             }
           }
           
           // "FOO": treat as single-token annotation (such as POS tag)
           else { 
-            Annotandum tag = new Annotandum(items[1], start, end);
+            Annotandum tag = new Annotandum(items[1], start, end, column, false);
             annotanda.add(tag);
           }
         }
@@ -217,9 +235,13 @@ class Annotandum {
     this.features = Factory.newFeatureMap();
   }
   
-  protected Annotandum(String type, Long startOffset, Long endOffset, String tag) {
+  /* Note that chunkiness is determined by the tag structure.  A "B-Foo"
+   * that spans only one token is chunky.  Tags outside the B/I/O system
+   * get the kind==token feature; tags in the system get kind==chunky.   */
+  protected Annotandum(String type, Long startOffset, Long endOffset, int column, boolean chunky) {
     this.features = Factory.newFeatureMap();
-    this.features.put(ANNIEConstants.TOKEN_CATEGORY_FEATURE_NAME, tag);
+    this.features.put(ConllDocumentFormat.ANNOTATION_COLUMN_FEATURE, column);
+    this.features.put(ConllDocumentFormat.ANNOTATION_KIND_FEATURE, chunky ? "chunk" : "token");
     this.startOffset = startOffset;
     this.endOffset = endOffset;
     this.type = type;
@@ -232,6 +254,21 @@ class Annotandum {
     this.features = features;
   }
 
+  protected static Annotandum makeToken(long start, long end, String string) {
+    int length = (int) (end - start);
+    FeatureMap features = Factory.newFeatureMap();
+    features.put(ANNIEConstants.TOKEN_LENGTH_FEATURE_NAME, length);
+    features.put(ANNIEConstants.TOKEN_STRING_FEATURE_NAME, string);
+    return new Annotandum("Token", start, end, features);
+  }
+  
+  protected static Annotandum makeSpaceToken(long start) {
+    long end = start + 1L;
+    FeatureMap features = Factory.newFeatureMap();
+    features.put(ANNIEConstants.TOKEN_LENGTH_FEATURE_NAME, 1);
+    features.put(ANNIEConstants.TOKEN_STRING_FEATURE_NAME, " ");
+    return new Annotandum("SpaceToken", start, end, features);
+  }
   
   
 }
