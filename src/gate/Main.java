8c4b1cd1be1e3638e16f42957b7aac76ca3aaf6c
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-03-23 10:54:28 +0000

    cleaned up the imports

--- src/gate/Main.java
+++ src/gate/Main.java
@@ -37,7 +37,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.management.ThreadInfo;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
