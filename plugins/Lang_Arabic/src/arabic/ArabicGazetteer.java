6aa42e66a27968b84fca3f67fad49174bb173d2e
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-10-29 18:43:19 +0000

    switched to using annotations to define the PRs so all the params are inherited from the original versions

--- plugins/Lang_Arabic/src/arabic/ArabicGazetteer.java
+++ plugins/Lang_Arabic/src/arabic/ArabicGazetteer.java
@@ -1,5 +1,16 @@
 package arabic;
-import gate.creole.gazetteer.DefaultGazetteer;
 
+import java.net.URL;
+
+import gate.creole.gazetteer.DefaultGazetteer;
+import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.CreoleParameter;
+
+@CreoleResource(name="Arabic Gazetteer")
 public class ArabicGazetteer extends DefaultGazetteer {
+
+	@CreoleParameter(defaultValue="resources/gazetteer/lists.def")
+	public void setListsURL(URL url) {
+		super.setListsURL(url);
+	}
 }
--- plugins/Lang_Arabic/src/arabic/ArabicInferedGazetteer.java
+++ plugins/Lang_Arabic/src/arabic/ArabicInferedGazetteer.java
@@ -1,5 +1,15 @@
 package arabic;
-import gate.creole.gazetteer.DefaultGazetteer;
+import java.net.URL;
 
+import gate.creole.gazetteer.DefaultGazetteer;
+import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.CreoleParameter;
+
+@CreoleResource(name="Arabic Infered Gazetteer")
 public class ArabicInferedGazetteer extends DefaultGazetteer {
+
+	@CreoleParameter(defaultValue="resources/inferred-gazetteer/lists.def")
+	public void setListsURL(URL url) {
+		super.setListsURL(url);
+	}
 }
