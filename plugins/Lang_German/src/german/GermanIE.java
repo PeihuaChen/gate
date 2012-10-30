f4ab37284bcc2309446022590e8624c3c7264d4d
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-10-30 18:35:29 +0000

    simplified the packaged app to use the newer style of specifiying the params which is much simpler and requires less boilerplate code

--- plugins/Lang_German/src/german/GermanIE.java
+++ plugins/Lang_German/src/german/GermanIE.java
@@ -2,24 +2,16 @@ package german;
 
 import gate.creole.PackagedController;
 import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
 
 import java.net.URL;
 import java.util.List;
 
-@CreoleResource(name = "German IE System", autoinstances = @AutoInstance)
+@CreoleResource(name = "German IE System", autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/german.gapp"),
+	@AutoInstanceParam(name="menu", value="German")}))
 public class GermanIE extends PackagedController {
 
-  @Override
-  @CreoleParameter(defaultValue = "resources/german.gapp")
-  public void setPipelineURL(URL url) {
-    this.url = url;
-  }
-
-  @Override
-  @CreoleParameter(defaultValue = "German")
-  public void setMenu(List<String> menu) {
-    super.setMenu(menu);
-  }
 }
