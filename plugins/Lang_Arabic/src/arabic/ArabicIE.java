339da48a81f8f4c0eff9470523e5e569d4d6583f
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-10-30 18:17:40 +0000

    simplified the packaged app to use the newer style of specifiying the params which is much simpler and requires less boilerplate code

--- plugins/Lang_Arabic/src/arabic/ArabicIE.java
+++ plugins/Lang_Arabic/src/arabic/ArabicIE.java
@@ -2,24 +2,16 @@ package arabic;
 
 import gate.creole.PackagedController;
 import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
 
 import java.net.URL;
 import java.util.List;
 
-@CreoleResource(name = "Arabic IE System", autoinstances = @AutoInstance)
+@CreoleResource(name = "Arabic IE System", autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/arabic.gapp"),
+	@AutoInstanceParam(name="menu", value="Arabic")}))
 public class ArabicIE extends PackagedController {
 
-  @Override
-  @CreoleParameter(defaultValue = "resources/arabic.gapp")
-  public void setPipelineURL(URL url) {
-    this.url = url;
-  }
-
-  @Override
-  @CreoleParameter(defaultValue = "Arabic")
-  public void setMenu(List<String> menu) {
-    super.setMenu(menu);
-  }
 }
