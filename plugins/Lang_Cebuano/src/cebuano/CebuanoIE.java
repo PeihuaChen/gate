334240a7a62404f24d65f7066ec94a7279803055
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-10-30 18:12:54 +0000

    simplified the packaged app to use the newer style of specifiying the params which is much simpler and requires less boilerplate code

--- plugins/Lang_Cebuano/src/cebuano/CebuanoIE.java
+++ plugins/Lang_Cebuano/src/cebuano/CebuanoIE.java
@@ -2,24 +2,16 @@ package cebuano;
 
 import gate.creole.PackagedController;
 import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
 
 import java.net.URL;
 import java.util.List;
 
-@CreoleResource(name = "Cebuano IE System", autoinstances = @AutoInstance)
+@CreoleResource(name = "Cebuano IE System", autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/cebuano.gapp"),
+	@AutoInstanceParam(name="menu", value="Cebuano")}))
 public class CebuanoIE extends PackagedController {
 
-  @Override
-  @CreoleParameter(defaultValue = "resources/cebuano.gapp")
-  public void setPipelineURL(URL url) {
-    this.url = url;
-  }
-
-  @Override
-  @CreoleParameter(defaultValue = "Cebuano")
-  public void setMenu(List<String> menu) {
-    super.setMenu(menu);
-  }
 }
