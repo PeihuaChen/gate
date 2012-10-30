f13cd32effc7fc9df3d85c6166945c76c3244454
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-10-30 18:38:45 +0000

    simplified the packaged app to use the newer style of specifiying the params which is much simpler and requires less boilerplate code

--- plugins/Lang_Romanian/src/romanian/RomanianIE.java
+++ plugins/Lang_Romanian/src/romanian/RomanianIE.java
@@ -2,24 +2,16 @@ package romanian;
 
 import gate.creole.PackagedController;
 import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
 
 import java.net.URL;
 import java.util.List;
 
-@CreoleResource(name = "Romanian IE System", autoinstances = @AutoInstance)
+@CreoleResource(name = "Romanian IE System", autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/romanian.gapp"),
+	@AutoInstanceParam(name="menu", value="Romanian")}))
 public class RomanianIE extends PackagedController {
 
-  @Override
-  @CreoleParameter(defaultValue = "resources/romanian.gapp")
-  public void setPipelineURL(URL url) {
-    this.url = url;
-  }
-
-  @Override
-  @CreoleParameter(defaultValue = "Romanian")
-  public void setMenu(List<String> menu) {
-    super.setMenu(menu);
-  }
 }
