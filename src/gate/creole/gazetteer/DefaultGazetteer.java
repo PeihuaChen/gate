dccd52dd368903c6bbb7cc71573a6fb759d72dac
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2012-11-05 17:42:31 +0000

    remove a rogue @RunTime that stops the feature separator param from working

--- src/gate/creole/gazetteer/DefaultGazetteer.java
+++ src/gate/creole/gazetteer/DefaultGazetteer.java
@@ -601,7 +601,6 @@ public class DefaultGazetteer extends AbstractGazetteer
   /**
    * @param gazetteerFeatureSeparator the gazetteerFeatureSeparator to set
    */
-  @RunTime
   @Optional
   @CreoleParameter(comment="The character used to separate features for entries in gazetteer lists. Accepts strings like &quot;\t&quot; and will unescape it to the relevant character. If not specified, this gazetteer does not support extra features.")
   public void setGazetteerFeatureSeparator(String gazetteerFeatureSeparator) {
