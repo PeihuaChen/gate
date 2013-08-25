63b5275b1e7a8e6c80f1f7dc48581f3dc4910153
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2013-08-25 11:56:50 +0000

    integrated another of the apps into the GUI

---
+++ plugins/Lang_Russian/src/com/ontotext/russie/apps/RussIEOrtho.java
@@ -0,0 +1,13 @@
+package com.ontotext.russie.apps;
+
+import gate.creole.PackagedController;
+import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
+import gate.creole.metadata.CreoleResource;
+
+@CreoleResource(name = "RussIE + OrthoMatcher", autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/RussIE_ortho.xgapp"),
+	@AutoInstanceParam(name="menu", value="Russian")}))
+public class RussIEOrtho extends PackagedController {
+
+}
