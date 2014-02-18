a73eb6d8fcdbf1a3f43988eef8275b1493073068
adamfunk <adamfunk@aa4978b9-1eff-4426-90b4-889a60d71948>
2014-02-18 16:52:41 +0000

    PMI example should be easier to follow

--- plugins/TermRaider/resources/groovy/CleanPMICandidates.groovy
+++ plugins/TermRaider/resources/groovy/CleanPMICandidates.groovy
@@ -6,9 +6,7 @@
 // candidates.
 
 Set<String> termTypes = new HashSet<String>();
-termTypes.add("Entity");
-termTypes.add("Verb");
-termTypes.add("VG");
+termTypes.add("MultiWord");
 
 Set<String> exclusionTypes = new HashSet<String>();
 exclusionTypes.add("Person");
@@ -29,15 +27,4 @@ for (Annotation candidate : candidates) {
     inputAS.add(candidate.getStartNode(), candidate.getEndNode(), newType, newf);
     inputAS.remove(candidate);  
   }
-  
-  // delete single-token VG annotations
-  else if ( candidate.getType().equals("VG") &&
-            (gate.Utils.getContainedAnnotations(inputAS, candidate, "Token").size() < 2) ) {
-    FeatureMap newf = Factory.newFeatureMap();
-    newf.putAll(candidate.getFeatures());
-    String newType = "deleted_VG_" + candidate.getType();
-    inputAS.add(candidate.getStartNode(), candidate.getEndNode(), newType, newf);
-    inputAS.remove(candidate);  
-  }
-  
 }
