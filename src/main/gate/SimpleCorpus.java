b3f715dae6cd6b6f49d1a06b5732520d5a12dacb
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2014-02-26 14:45:12 +0000

    removed some deprecation suppressing annotations, that had nothing to supress

--- src/main/gate/SimpleCorpus.java
+++ src/main/gate/SimpleCorpus.java
@@ -27,7 +27,6 @@ import java.util.List;
 /**
  * Corpora are lists of Document. TIPSTER equivalent: Collection.
  */
-@SuppressWarnings("deprecation")
 public interface SimpleCorpus extends LanguageResource, List<Document>, NameBearer {
 
   public static final String CORPUS_NAME_PARAMETER_NAME = "name";
