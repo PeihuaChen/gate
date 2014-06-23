425071cbbcd21fa6ea4ad9aec853df0121d6cb81
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2014-06-23 12:42:11 +0000

    added the license headers

--- plugins/Format_DataSift/src/gate/corpora/datasift/DataSift.java
+++ plugins/Format_DataSift/src/gate/corpora/datasift/DataSift.java
@@ -1,3 +1,15 @@
+/*
+ *  Copyright (c) 1995-2014, The University of Sheffield. See the file
+ *  COPYRIGHT.txt in the software or at http://gate.ac.uk/gate/COPYRIGHT.txt
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *  
+ *  Mark A. Greenwood, 23/06/2014
+ */
+
 package gate.corpora.datasift;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
