945e9cc3d921d20074ce90b984c23c1a3354463e
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2014-07-17 07:36:53 +0000

    some code comments and a little refactoring of the gui side of the export code

--- src/main/gate/DocumentExporter.java
+++ src/main/gate/DocumentExporter.java
@@ -20,39 +20,71 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 
 import javax.swing.filechooser.FileFilter;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * Adds support for exporting documents from GATE. Subclasses of
+ * DocumentExporter now how to export in a given format. These exporters
+ * are loaded as Creole resources and made easily available within the
+ * GUI, but can also be easily accessed from within the API.
+ */
 public abstract class DocumentExporter extends AbstractResource {
 
   private static final long serialVersionUID = -4810523902750051704L;
 
   protected String fileType, defaultExtension;
+
   protected FileFilter filter;
-  
+
+  /**
+   * Creates a new exporter instance for a given file type with default
+   * extension.
+   * 
+   * @param fileType this is the human readable file type name that will
+   *          appear on the menu
+   * @param defaultExtension the default file extension for this type
+   */
   public DocumentExporter(String fileType, String defaultExtension) {
     this.fileType = fileType;
     this.defaultExtension = defaultExtension;
-    filter = new ExtensionFileFilter(fileType+" Files (*."+defaultExtension+")",defaultExtension);    
   }
-  
+
+  /**
+   * The name of the file type exported
+   * 
+   * @return the name of the file type exported
+   */
   public String getFileType() {
     return fileType;
   }
-  
+
+  /**
+   * The default extension added to files saved in this format
+   * 
+   * @return the default extension added to files saved in this format
+   */
   public String getDefaultExtension() {
     return defaultExtension;
   }
-  
+
+  /**
+   * A filter used in the file chooser to restrict the view to files of
+   * this type. The default implementation just uses the default
+   * extension as the filter constraint
+   */
   public FileFilter getFileFilter() {
+    if(filter == null)
+      filter =
+              new ExtensionFileFilter(fileType + " Files (*."
+                      + defaultExtension + ")", defaultExtension);
     return filter;
   }
-    
+
   /**
-   * Equivalent to {@link #export(Document,File, Map)} with an empty map
+   * Equivalent to {@link #export(Document,File,FeatureMap)} with an empty map
    * of options.
    */
   public void export(Document doc, File file) throws IOException {
@@ -60,7 +92,7 @@ public abstract class DocumentExporter extends AbstractResource {
   }
 
   /**
-   * Equivalent to {@link #export(Document,OutputStream, Map)} using a
+   * Equivalent to {@link #export(Document,OutputStream,FeatureMap)} using a
    * FileOutputStream instance constructed from the File param.
    */
   public void export(Document doc, File file, FeatureMap options)
@@ -90,5 +122,6 @@ public abstract class DocumentExporter extends AbstractResource {
    * @param out the OutputStream to export the document to
    * @param options DocumentExporter specific options
    */
-  public abstract void export(Document doc, OutputStream out, FeatureMap options) throws IOException;
+  public abstract void export(Document doc, OutputStream out, FeatureMap options)
+          throws IOException;
 }
--- src/main/gate/gui/NameBearerHandle.java
+++ src/main/gate/gui/NameBearerHandle.java
@@ -289,7 +289,7 @@ public class NameBearerHandle implements Handle, StatusListener,
       }
       if(target instanceof gate.TextualDocument) {
         staticPopupItems.add(null);
-        staticPopupItems.add(new DocumentExportDialog.SaveAsMenu((Document)target, sListenerProxy));
+        staticPopupItems.add(new DocumentExportMenu((Document)target, sListenerProxy));
       }
       else if(target instanceof Corpus) {
         corpusFiller = new CorpusFillerComponent();
@@ -300,7 +300,7 @@ public class NameBearerHandle implements Handle, StatusListener,
                 new PopulateCorpusFromSingleConcatenatedFileAction(),
                 sListenerProxy));
         staticPopupItems.add(null);
-        staticPopupItems.add(new DocumentExportDialog.SaveAsMenu((Corpus)target, sListenerProxy));
+        staticPopupItems.add(new DocumentExportMenu((Corpus)target, sListenerProxy));
         // staticPopupItems.add(new XJMenuItem(new
         // SaveCorpusAsXmlAction(true), sListenerProxy));
         
