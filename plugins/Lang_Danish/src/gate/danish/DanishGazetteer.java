b4d79afe5360fb1af9c8ff36f923bb0d1a25fc31
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2016-02-04 10:01:11 +0000

    added serial version IDs to make the compiler happier

--- plugins/Lang_Danish/src/gate/danish/DanishGazetteer.java
+++ plugins/Lang_Danish/src/gate/danish/DanishGazetteer.java
@@ -5,4 +5,6 @@ import gate.creole.gazetteer.DefaultGazetteer;
 
 @CreoleResource(name = "Danish Gazetteer", comment = "Person, Location and Organisation gazetteers for Danish")
 public class DanishGazetteer extends DefaultGazetteer {
+
+  private static final long serialVersionUID = -1639930602486322792L;
 }
--- plugins/Lang_Danish/src/gate/danish/DanishTokeniser.java
+++ plugins/Lang_Danish/src/gate/danish/DanishTokeniser.java
@@ -6,5 +6,5 @@ import gate.creole.tokeniser.DefaultTokeniser;
 @CreoleResource(name = "Danish Tokeniser", comment = "PAROLE tokeniser, for Danish")
 public class DanishTokeniser extends DefaultTokeniser {
 
-
+  private static final long serialVersionUID = 150304758902960407L;
 }
