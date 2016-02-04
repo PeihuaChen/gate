0404e5c25407a03b2ddf7dae5834cfc14f879e6a
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2016-02-04 10:19:21 +0000

    copyright headers and fixed the tokenizer so that the PR uses the right jape is created independently from the xgapp -- also simplified the xgapp so the tokenizer uses the default values rather than specifiying them so if they are changed in the future the xgapp automagically picks them up

--- plugins/Lang_Danish/src/gate/danish/DanishGazetteer.java
+++ plugins/Lang_Danish/src/gate/danish/DanishGazetteer.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 1995-2016, The University of Sheffield. See the file
+ * COPYRIGHT.txt in the software or at http://gate.ac.uk/gate/COPYRIGHT.txt
+ * 
+ * This file is part of GATE (see http://gate.ac.uk/), and is free software,
+ * licenced under the GNU Library General Public License, Version 2, June 1991
+ * (in the distribution as file licence.html, and also available at
+ * http://gate.ac.uk/gate/licence.html).
+ */
+
 package danish;
 
 import gate.creole.metadata.CreoleResource;
