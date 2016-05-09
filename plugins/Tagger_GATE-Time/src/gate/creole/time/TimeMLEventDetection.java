5fcca1e229546dd4788320706eaed522927d7aab
markagreenwood <markagreenwood@aa4978b9-1eff-4426-90b4-889a60d71948>
2016-05-09 13:16:11 +0000

    switch the license headers to GPL to match HeidelTime before we get bitten again

--- plugins/Tagger_GATE-Time/src/gate/creole/time/TimeMLEventDetection.java
+++ plugins/Tagger_GATE-Time/src/gate/creole/time/TimeMLEventDetection.java
@@ -3,12 +3,18 @@
  *
  * Copyright (c) 2016, The University of Sheffield.
  *
- * This file is part of GATE (see http://gate.ac.uk/), and is free
- * software, licenced under the GNU Library General Public License,
- * Version 3, 29 June 2007.
- *
- * A copy of this licence is included in the distribution in the file
- * licence.html, and is also available at http://gate.ac.uk/gate/licence.html.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Mark A. Greenwood, 09/05/2016
  */
